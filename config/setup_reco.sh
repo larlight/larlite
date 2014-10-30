f392804b600ce7abca80d72e2f16c05e19c0cab8
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-10-30 07:15:17 -0500

    set up script for (incomplete) RecoTool repo --kazu

---
+++ config/setup_reco.sh
@@ -0,0 +1,8 @@
+
+export USER_MODULE="RecoTool"
+# Find the location of this script:
+me="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Find the directory one above.
+export LARLITE_BASEDIR="$( cd "$( dirname "$me" )" && pwd )"
+
+source $LARLITE_BASEDIR/config/setup.sh
