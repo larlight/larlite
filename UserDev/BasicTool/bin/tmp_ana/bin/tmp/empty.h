2a66e9c77159b029d9fbed36b7bf2c234b9ffdd0
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-10-30 07:14:53 -0500

    Add a basic tool repo inside larlite --kazu

---
+++ UserDev/BasicTool/bin/tmp_ana/bin/tmp/empty.h
@@ -0,0 +1,38 @@
+/**
+ * \file Empty_Class_Name.h
+ *
+ * \ingroup Working_Package
+ * 
+ * \brief Class def header for a class Empty_Class_Name
+ *
+ * @author SHELL_USER_NAME
+ */
+
+/** \addtogroup Working_Package
+
+    @{*/
+#ifndef LARLITE_EMPTY_CLASS_NAME_H
+#define LARLITE_EMPTY_CLASS_NAME_H
+
+#include <iostream>
+
+/**
+   \class Empty_Class_Name
+   User defined class Empty_Class_Name ... these comments are used to generate
+   doxygen documentation!
+ */
+class Empty_Class_Name{
+
+public:
+
+  /// Default constructor
+  Empty_Class_Name(){};
+
+  /// Default destructor
+  virtual ~Empty_Class_Name(){};
+
+};
+
+#endif
+/** @} */ // end of doxygen group 
+
