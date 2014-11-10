692164a197c9617ec3b7c274051fb8b4285c390d
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-11-10 16:02:09 -0600

    MCTrack updated --kazu

--- core/DataFormat/mctrack.cxx
+++ core/DataFormat/mctrack.cxx
@@ -11,9 +11,16 @@ namespace larlite {
     std::vector<mcstep>::clear();
     data_base::clear_data();
 
+    fOrigin  = simb::kUnknown;
+    fProcess = "";
     fPDGCode         = data::kINVALID_INT;
-    fCreationProcess = "";
-    fAncestorPDGCode = data::kINVALID_INT;
+    fG4TrackID       = data::kINVALID_UINT;
+
+    fMotherPDGCode   = data::kINVALID_INT;
+    fMotherG4TrackID = data::kINVALID_UINT;
+
+    fAncestorPDGCode   = data::kINVALID_INT;
+    fAncestorG4TrackID = data::kINVALID_UINT;
 
     TLorentzVector invalid(data::kINVALID_DOUBLE,
 			   data::kINVALID_DOUBLE,
@@ -24,8 +31,17 @@ namespace larlite {
     fG4Start.SetMomentum(invalid);
     fG4End.SetPosition(invalid);
     fG4End.SetMomentum(invalid);
+
+    fMotherG4Start.SetPosition(invalid);
+    fMotherG4Start.SetMomentum(invalid);
+    fMotherG4End.SetPosition(invalid);
+    fMotherG4End.SetMomentum(invalid);
+
     fAncestorG4Start.SetPosition(invalid);
     fAncestorG4Start.SetMomentum(invalid);
+    fAncestorG4End.SetPosition(invalid);
+    fAncestorG4End.SetMomentum(invalid);
+
   }
 
 }
