797281d262c30dba5c3fe8f64fcb0765245b0f40
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-11-14 04:28:39 -0600

    handle non-vector type class --kazu

--- bin/eventdump.py
+++ bin/eventdump.py
@@ -26,7 +26,8 @@ if not mgr.is_open():
     print
     sys.exit(1)
 
-FORMAT="\033[95mProduct\033[00m \"%10s \" (enum=%2d) ...\033[95m made by\033[00m %20s ...\033[95m count :\033[00m %d "
+FORMAT_VEC="\033[95mProduct\033[00m \"%10s \" (enum=%2d) ...\033[95m made by\033[00m %20s ...\033[95m count :\033[00m %d "
+FORMAT="\033[95mProduct\033[00m \"%10s \" (enum=%2d) ...\033[95m made by\033[00m %20s ...\033[95m count :\033[00m 1 "
 
 ctr=0;
 while mgr.next_event():
@@ -38,7 +39,9 @@ while mgr.next_event():
         product_id  = product_id_v.at(x)
         product_ptr = mgr.get_data(product_id.first,product_id.second)
         if product_ptr :
-            print FORMAT % (mgr.product_name(product_id.first),product_id.first,product_id.second,product_ptr.size())
-
+            try:
+                print FORMAT_VEC % (mgr.product_name(product_id.first),product_id.first,product_id.second,product_ptr.size())
+            except AttributeError:
+                print FORMAT % (mgr.product_name(product_id.first),product_id.first,product_id.second)
     print
     ctr+=1
