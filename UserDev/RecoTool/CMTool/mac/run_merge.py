c6301327a8c26342107de942c00a4d32f03de1fb
coreyjadams <corey.adams@yale.edu>
2014-11-17 12:19:36 -0600

    dsfkjhsdf :

--- UserDev/RecoTool/CMTool/mac/run_merge.py
+++ UserDev/RecoTool/CMTool/mac/run_merge.py
@@ -1,21 +1,23 @@
 import ROOT,sys
 ROOT.gSystem.Load("libClusterRecoUtil")
 from ROOT import *
-from ROOT import larlight as fmwk
+from ROOT import larlite as fmwk
 
 mgr = fmwk.ana_processor()
 
 mgr.add_input_file(sys.argv[1])
 
-mgr.set_input_rootdir("scanner")
+# mgr.set_input_rootdir("scanner")
 
 mgr.set_output_file("out.root")
 
-mgr.set_io_mode(fmwk.storage_manager.BOTH)
+mgr.set_io_mode(fmwk.storage_manager.kBOTH)
 
 mgr.set_ana_output_file("")
 
 proc = fmwk.ClusterMerger()
+
+proc.SetClusterProducer()
 
 proc.SaveOutputCluster(True)
 
