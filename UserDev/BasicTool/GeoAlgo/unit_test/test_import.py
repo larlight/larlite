fca249f8fd79292c6709af0671e48c2355293e7e
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-12-03 15:14:19 -0600

    unit test example --kazu

---
+++ UserDev/BasicTool/GeoAlgo/unit_test/test_import.py
@@ -0,0 +1,16 @@
+from test_msg import debug, info, error, warning
+
+def test_import():
+
+    try:
+        debug('Attempting to import geoalgo')
+        from ROOT import geoalgo
+    except Exception:
+        error('Import geoalgo unit test failed.')
+        return 1
+    info('geoalgo import succeeded.')
+    return 0
+
+if __name__ == '__main__':
+    test_Import()
+
---
+++ UserDev/BasicTool/GeoAlgo/unit_test/unit_test.py
@@ -0,0 +1,26 @@
+import sys
+import test_msg
+from test_msg import debug, info, error, warning
+
+print
+print '\033[93mExecuting a unit test for geoalgo Package\033[00m'
+print 
+
+# message level: [0,1,2,3] = [debug,info,warning,error]
+MSG_LEVEL=1
+
+test_msg.test_msg.level = MSG_LEVEL
+
+#
+# Test import
+#
+from test_import import test_import
+if test_import(): sys.exit(1)
+
+#
+# Test Vector
+#
+from test_vector import test_vector
+if test_vector(): sys.exit(1)
+
+
