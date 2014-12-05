747227e375d0dedf56ee211aea4b14a4937ec182
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-12-05 09:12:23 -0600

    fix --kazu

--- UserDev/BasicTool/GeoAlgo/python/trial.py
+++ UserDev/BasicTool/GeoAlgo/python/trial.py
@@ -1,8 +1,21 @@
-from ROOT import geoalgo
-alg=geoalgo.DistanceAlgo()
-hline1  = geoalgo.HalfLine(3,3,3,-1,-2,-3)
-hline2  = geoalgo.HalfLine(2,4,-1,1,0.5,4)
-p1      = geoalgo.Vector(1,1,1)
-p2      = geoalgo.Vector(2,2,2)
-print alg.SqDist(hline1, hline2, p1, p2)
+from geoalgo import *
 
+k=GeoViewer()
+
+pt=geoalgo.Vector(3)
+trj=geoalgo.Trajectory()
+
+for x in xrange(10):
+    for y in xrange(pt.size()):
+        if x<5 or not y:
+            pt[y] = x
+        elif y>=1:
+            pt[y] = 5
+    print 'Adding trajectory point:',x,' ... ',pt[0],pt[1],pt[2]
+    trj += pt
+
+box=geoalgo.AABox(0,0,0,7,7,7)
+
+k.add(box)
+k.add(trj)
+k.show()
