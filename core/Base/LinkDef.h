a68b075e9a3afb4df82a12f931cc9873d09bd2c5
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-12-06 07:55:21 -0600

    add byte code count --kazu

--- core/Base/LinkDef.h
+++ core/Base/LinkDef.h
@@ -9,15 +9,15 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ namespace larlite;
-#pragma link C++ namespace larlite::simb;
-#pragma link C++ namespace larlite::anab;
-#pragma link C++ namespace larlite::msg;
-#pragma link C++ namespace larlite::data;
-#pragma link C++ namespace larlite::geo;
+#pragma link C++ namespace larlite+;
+#pragma link C++ namespace larlite::simb+;
+#pragma link C++ namespace larlite::anab+;
+#pragma link C++ namespace larlite::msg+;
+#pragma link C++ namespace larlite::data+;
+#pragma link C++ namespace larlite::geo+;
 
-#pragma link C++ enum larlite::geo::SigType_t;
-#pragma link C++ enum larlite::geo::View_t;
+#pragma link C++ enum larlite::geo::SigType_t+;
+#pragma link C++ enum larlite::geo::View_t+;
 
 #pragma link C++ class std::vector<larlite::geo::SigType_t>+;
 #pragma link C++ class std::vector<larlite::geo::View_t>+;
