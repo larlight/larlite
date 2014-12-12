efae6d699c75fbf1bebd7cba7027593010cff69c
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-12-12 09:49:13 -0600

    update the version --kazu

--- config/setup_mrb_lar.sh
+++ config/setup_mrb_lar.sh
@@ -39,7 +39,7 @@ source /grid/fermiapp/products/uboone/setup_uboone.sh
 # Set nightly build LArSoft
 #setup uboonecode nightly -q e4:prof
 #setup larsoft nightly -q e5:prof
-setup larsoft v03_03_00 -q e6:prof
+setup larsoft v03_04_05 -q e6:prof
 
 # Set jobsub available (generic, not specific to uboone)
 #setup jobsub_tools
