3afd9fe86590c67d46e2a214bec5f74777937204
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-01-01 14:23:42 -0600

    fix an issue of env set @ gpvm --kazu

--- config/python/clean_env.py
+++ config/python/clean_env.py
@@ -1,23 +1,25 @@
 import os,sys
 cmd = ''
-if not len(sys.argv) == 2 or not 'LARLITE_BASEDIR' in os.environ:
+if not len(sys.argv) == 2 or not 'LARLITE_BASEDIR' in os.environ or not sys.argv[1] in os.environ:
     pass
 else:
     lite = os.environ['LARLITE_BASEDIR']
     e = sys.argv[1]
-    if not e in os.environ: print ''
     v = os.environ[e]
     start = v.find(lite)
-    if start < 0: pass
-    end = v.find(':',start)
-    
-    replace = ''
-    if end >=0:
-        replace = v[start:end+1]
-    else:
-        replace = v[start:len(v)]
+    if start >= 0:
+        end = v.find(':',start)
+
+        replace = ''
+        if end >=0:
+            replace = v[start:end+1]
+        else:
+            replace = v[start:len(v)]
+        print
+        print v
+        print
         
-    cmd = 'export %s=%s' % (e,v.replace(replace,''))
-    while cmd.find('::') >= 0:
-        cmd = cmd.replace('::',':')
+        cmd = 'export %s=%s' % (e,v.replace(replace,''))
+        while cmd.find('::') >= 0:
+            cmd = cmd.replace('::',':')
 print cmd
