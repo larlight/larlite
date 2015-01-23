143971d4a8d26fc07c2747a222b7593154ae143e
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-01-23 06:25:50 -0600

    Space for an application --kazu

---
+++ UserDev/SelectionTool/ERTool/App/lib/gen_rootmap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+temp=make_rootmap.$$.temp
+
+rootlibmap() {
+ ROOTMAP=$1
+ SOFILE=$2
+ LINKDEF=$3
+ shift 3
+ DEPS=$*
+ if [[ -e $SOFILE && -e $LINKDEF ]]; then
+     rlibmap -f -o $ROOTMAP -l $SOFILE -d $DEPS -c $LINKDEF 2>> $temp
+     rm -f $temp
+ fi
+}
+
+######################################################
+# ERTool_App
+rootlibmap libSelectionTool_ERApp.rootmap libSelectionTool_ERApp.so \
+    $LARLITE_USERDEVDIR/SelectionTool/ERTool/App/LinkDef.h \
+    libBasicTool_GeoAlgo.so \
+    libRooFit.so \
+    libEG.so
