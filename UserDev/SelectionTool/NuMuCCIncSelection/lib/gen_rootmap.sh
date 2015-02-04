c8a2f61d883775c561473e57abbd907bac7b9543
Joseph Zennamo <jzennamo@uchicago.edu>
2015-02-04 14:54:30 -0600

    Adding nu_mu CC Inclusive filter

---
+++ UserDev/SelectionTool/NuMuCCIncSelection/lib/gen_rootmap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+temp=make_rootmap.$$.temp
+
+rootlibmap() {
+ ROOTMAP=$1
+ SOFILE=$2
+ LINKDEF=$3
+ shift 3
+ DEPS=$*
+ if [[ -e $SOFILE && -e $LINKDEF ]]; then
+     rlibmap -f -o $ROOTMAP -l $SOFILE -d $DEPS -c $LINKDEF 2>> $temp
+     rm -f $temp
+ fi
+}
+
+######################################################
+# NuMuCCIncSelection
+rootlibmap libSelectionTool_NuMuCCIncSelection.rootmap libSelectionTool_NuMuCCIncSelection.so $LARLITE_USERDEVDIR/SelectionTool/NuMuCCIncSelection/LinkDef.h \
+    libLArLite_Analysis.so \
+
