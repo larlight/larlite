e2e53fdcdf3d532ccf35aa48541f5cc830d7606f
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-02-23 14:39:01 -0600

    Allowing TimeOverlap to make a pair w/ 2 clusters. The feature is OFF by default, use RequireThreePlanes(false) to enable --kazu

--- UserDev/RecoTool/CMTool/CMTAlgMatch/CFAlgoTimeOverlap.h
+++ UserDev/RecoTool/CMTool/CMTAlgMatch/CFAlgoTimeOverlap.h
@@ -36,29 +36,33 @@ namespace cmtool {
 		and compares across planes to form matches. 
     */
     virtual float Float(const std::vector<const cluster::ClusterParamsAlg*> &clusters);
-
-	void SetStartTimeCut(float start_time) { _start_time_cut = start_time ; } 
-
-	void SetRatioCut(float ratio) { _time_ratio_cut = ratio ; } 
+    
+    void SetStartTimeCut(float start_time) { _start_time_cut = start_time ; } 
+    
+    void SetRatioCut(float ratio) { _time_ratio_cut = ratio ; } 
 
  
     //Order the theta, phi, hits per plane to make cuts convenient
-    virtual void SetMaxMiddleMin(const double first, const double second, const double third, double &most, double &middle, double &least) ;
+    /*
+    virtual void SetMaxMiddleMin(const double first, const double second, const double third, 
+				 double &most, double &middle, double &least) ;
+    */
+    void SetDebug(bool debug) { _debug = debug ; }
+    
+    void SetVerbose(bool verbose) { _verbose = verbose ; }
 
-	void SetDebug(bool debug) { _debug = debug ; }
-
-	void SetVerbose(bool verbose) { _verbose = verbose ; }
+    void RequireThreePlanes(bool doit) { _require_3planes = doit; }
 
     virtual void Report();
     
     virtual void Reset();
 
-	protected:
-		float _time_ratio_cut ;
-		float _start_time_cut ;
-		bool _debug ;
-		bool _verbose ;
-
+  protected:
+    float _time_ratio_cut ;
+    float _start_time_cut ;
+    bool _debug ;
+    bool _verbose ;
+    bool _require_3planes;
   };
 }
 #endif
