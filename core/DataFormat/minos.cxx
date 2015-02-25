df6587b254df453b2b89eec57bea37d7cbdb3ebe
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-02-25 10:05:36 -0600

    minor minor --kazu

--- core/DataFormat/minos.cxx
+++ core/DataFormat/minos.cxx
@@ -2,12 +2,12 @@
 
 namespace larlite{
   minos::minos() : data_base(data::kMinos)
-		   ftrkstpX(1,-999.0),
-		   ftrkstpY(1,-999.0),
-		   ftrkstpZ(1,-999.0),
-		   ftrkstpU(1,-999.0),
-		   ftrkstpV(1,-999.0),
-		   fmatched(-1)
+		 , ftrkstpX(1,-999.0)
+		 , ftrkstpY(1,-999.0)
+		 , ftrkstpZ(1,-999.0)
+		 , ftrkstpU(1,-999.0)
+		 , ftrkstpV(1,-999.0)
+		 , fmatched(-1)
   {}
   
   minos::~minos()
