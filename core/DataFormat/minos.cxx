452187961fe9e5a3ecb08a2181e8a163c7ae2881
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-02-25 10:00:14 -0600

    minor forgotten changes --kazu

--- core/DataFormat/minos.cxx
+++ core/DataFormat/minos.cxx
@@ -1,19 +1,15 @@
 #include "minos.h"
 
 namespace larlite{
-   minos::minos():
-      ftrkstpX(1,-999.0),
-      ftrkstpY(1,-999.0),
-      ftrkstpZ(1,-999.0),
-      ftrkstpU(1,-999.0),
-      ftrkstpV(1,-999.0),
-      fmatched(-1)
-   {
-   }
-
-   minos::~minos()
-   {
-   }
-
-
+  minos::minos() : data_base(data::kMinos)
+		   ftrkstpX(1,-999.0),
+		   ftrkstpY(1,-999.0),
+		   ftrkstpZ(1,-999.0),
+		   ftrkstpU(1,-999.0),
+		   ftrkstpV(1,-999.0),
+		   fmatched(-1)
+  {}
+  
+  minos::~minos()
+  {}
 }
