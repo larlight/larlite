63957f6ed0b0be6c0815eb243e86f0cd95611239
Corey Adams <corey.adams@yale.edu>
2015-03-04 15:27:14 -0500

    Fixing typo in LarUtilManager.

--- core/LArUtil/LArUtilManager.cxx
+++ core/LArUtil/LArUtilManager.cxx
@@ -26,7 +26,7 @@ namespace larutil {
     larlite::Message::send(larlite::msg::kNORMAL,__FUNCTION__,"Reconfiguring Geometry");
     Geometry* geom = (Geometry*)(Geometry::GetME(false));
     geom->SetFileName(Form("%s/LArUtil/dat/%s",
-			   getenv("LARLITE_CORE_DIR"),
+			   getenv("LARLITE_COREDIR"),
 			   kUTIL_DATA_FILENAME[LArUtilConfig::Detector()].c_str()));
     geom->SetTreeName(kTREENAME_GEOMETRY);
     status = status && geom->LoadData(true);
@@ -35,7 +35,7 @@ namespace larutil {
     larlite::Message::send(larlite::msg::kNORMAL,__FUNCTION__,"Reconfiguring LArProperties");
     LArProperties* larp = (LArProperties*)(LArProperties::GetME(false));
     larp->SetFileName(Form("%s/LArUtil/dat/%s",
-			   getenv("LARLITE_CORE_DIR"),
+			   getenv("LARLITE_COREDIR"),
 			   kUTIL_DATA_FILENAME[LArUtilConfig::Detector()].c_str()));
     larp->SetTreeName(kTREENAME_LARPROPERTIES);
     status = status && larp->LoadData(true);
@@ -44,7 +44,7 @@ namespace larutil {
     larlite::Message::send(larlite::msg::kNORMAL,__FUNCTION__,"Reconfiguring DetectorProperties");
     DetectorProperties* detp = (DetectorProperties*)(DetectorProperties::GetME(false));
     detp->SetFileName(Form("%s/LArUtil/dat/%s",
-			   getenv("LARLITE_CORE_DIR"),
+			   getenv("LARLITE_COREDIR"),
 			   kUTIL_DATA_FILENAME[LArUtilConfig::Detector()].c_str()));
     detp->SetTreeName(kTREENAME_DETECTORPROPERTIES);
     status = status && detp->LoadData(true);
