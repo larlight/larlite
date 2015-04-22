c423220291de852e1b596bdd2d5b79f1a7018629
David Kaleko <kaleko@nevis.columbia.edu>
2015-04-22 15:22:06 -0500

    damnit particle physics is hard

--- UserDev/SelectionTool/ERTool/Base/Particle.cxx
+++ UserDev/SelectionTool/ERTool/Base/Particle.cxx
@@ -39,7 +39,7 @@ namespace ertool {
   { return sqrt(_mom.SqLength() + _mass * _mass ); }
 
   double Particle::KineticEnergy() const
-  { return sqrt(pow(this->Energy(),2) - pow(this->Mass(),2)); }
+  { return (this->Energy() - this->Mass()); }
 
   double Particle::DaughterEnergySum() const
   { 
