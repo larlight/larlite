579b8af1ce6d4c08472300dbadb86da7d4aeb7a9
ryan <grossora.neutrino@gmail.com>
2015-05-22 12:21:29 -0500

    Script to compare reco 1pi0 to mc1pi0 -ryan

---
+++ UserDev/LArLiteApp/Pi0Ana/mac/Single_Pi0_back_Selection.py
@@ -0,0 +1,103 @@
+import sys, os
+from ROOT import gSystem
+gSystem.Load("libLArLite_Analysis")
+from ROOT import larlite as fmwk
+fmwk.ana_processor
+gSystem.Load("libLArLiteApp_Pi0Ana")
+from ROOT import ertool
+from ROOT import geoalgo
+
+
+if len(sys.argv) < 2:
+    msg  = '\n'
+    msg += "Usage 1: %s $INPUT_ROOT_FILE\n" % sys.argv[0]
+    msg += '\n'
+    sys.stderr.write(msg)
+    sys.exit(1)
+
+# open file on which to write values
+#myfile = open("misid_vs_posRes.txt","a")
+#posres = float(sys.argv[-1])
+#print "Position resolution set to: ", posres
+#myfile.write(str(posres)+"\t")
+
+
+# Create ana_processor instance
+my_proc = fmwk.ana_processor()
+my_proc.enable_filter(True)
+
+# Create algorithm
+my_algo = ertool.AlgoPi0()
+my_algo.setVerbose(False)
+my_algo.setMinShrEnergy(10)
+my_algo.setMaxShrEnergy(1000)
+my_algo.setIPMax(10)
+my_algo.setMinFitMass(50)
+my_algo.setMaxFitMass(200)
+my_algo.setAngleMax(3.14)
+# Create filter
+
+# Create analysis unit
+my_anaunit = fmwk.Pi0Reco()#ExampleERSelection()
+my_proc = fmwk.ana_processor()
+my_proc.enable_filter(True)
+
+# Set up ana for 1pi0 selection
+my_ana = ertool.ERAna1PI0()
+
+
+# First lets make a filter that looks for a certain events
+pi0_topo = fmwk.singlepi0();
+# 0 == inclusive 1== 1pi0&&nopi+/-
+pi0_topo.SetTopology(1);
+# false == ntsignal true == signal
+pi0_topo.SignalTopology(False);
+# 0 == CC 1 == NC 
+pi0_topo.SetCCNC(1);
+
+
+my_anaunit._mgr.SetAna(my_ana)
+my_anaunit._mgr.SetAlgo(my_algo)
+my_anaunit._mgr._training_mode =False
+
+
+# Set input root file
+for x in xrange(len(sys.argv)-1):
+    my_proc.add_input_file(sys.argv[x+1])
+
+# Specify IO mode
+my_proc.set_io_mode(fmwk.storage_manager.kREAD)
+
+# Specify output root file name
+my_proc.set_ana_output_file("singlepi0_back.root")
+
+# here set E-cut for Helper & Ana modules
+my_anaunit._mgr._mc_for_ana = True
+
+# ***************  Set Producers  ****************
+# First Argument: True = MC, False = Reco
+#my_anaunit.SetShowerProducer(True,"mcreco");
+#my_anaunit.SetShowerProducer(False,"newdefaultreco");
+#my_anaunit.SetShowerProducer(False,"pandoraNuShower");
+#my_anaunit.SetShowerProducer(False,"mergeall");
+#my_anaunit.SetTrackProducer(True,"mcreco");
+#my_anaunit.SetTrackProducer(False,"stitchkalmanhit");
+#my_anaunit.SetVtxProducer(True,"generator");
+my_anaunit.SetShowerProducer(False,"showerrecofuzzy");
+my_anaunit.SetTrackProducer(False,"");
+my_anaunit.SetVtxProducer(False,"");
+# ************************************************
+
+my_proc.add_process(pi0_topo)
+my_proc.add_process(my_anaunit)
+
+my_proc.run()
+
+# done!
+print
+print "Finished running ana_processor event loop!"
+print
+
+#my_algo.StoreParams()
+sys.exit(0)
+
--- UserDev/LArLiteApp/Pi0Ana/singlepi0.cxx
+++ UserDev/LArLiteApp/Pi0Ana/singlepi0.cxx
@@ -10,8 +10,6 @@ namespace larlite {
         _total_events = 0 ;
         _passed_events = 0;
 	
-	//SetTopology(0);
-
     return true;
   }
   
@@ -22,36 +20,38 @@ namespace larlite {
                 print(larlite::msg::kERROR,__FUNCTION__,Form("Did not find specified data product, mctruth!"));
                 return false;}// if no mctruth
         auto mcpart = mctruth->at(0).GetParticles();
+        auto mcnu = mctruth->at(0).GetNeutrino();
 
         _total_events++;
 
         size_t totalpi0 =0;
         size_t cmeson =0;
 
+	// If it is ccnc 
+	if(_ccnc != mcnu.CCNC()) return false;
+
+	//loop over all particles to look topology 1 pi0 and no pi+/-
+	for(auto const& p : mcpart){
+		if(p.PdgCode()==111 && p.StatusCode()==1) totalpi0+=1;
+		if(abs(p.PdgCode())==211 && p.StatusCode()==1) cmeson+=1;
+		   }// loop over mcparts
+
 	// This will return a bool for signal events
-if(_Sig){
-        //loop over all particles to look topology 1 pi0 and no pi+/-
-        for(auto const& p : mcpart){
-                 if(p.PdgCode()==111 && p.StatusCode()==1) totalpi0+=1;
-                 if(abs(p.PdgCode())==211 && p.StatusCode()==1) cmeson+=1;
-                                   }// loop over mcparts
-	// Fill in the topo you want to look at 
-        if(_Topo == 0){if(totalpi0!=1) return false;}
-	if(_Topo ==1 ){if(totalpi0!=1 || cmeson!=0) return false;}
-        _passed_events++;
-    return true;
-	}
+	if(_Sig){
+		// Fill in the topo you want to look at 
+		if(_Topo == 0){if(totalpi0!=1) return false;}
+		if(_Topo ==1 ){if(totalpi0!=1 || cmeson!=0) return false;}
+		_passed_events++;
+	    return true;
+		}
 
 	// Repeat with negatives if !_Sig
-        for(auto const& p : mcpart){
-                 if(p.PdgCode()==111 && p.StatusCode()==1) totalpi0+=1;
-                 if(abs(p.PdgCode())==211 && p.StatusCode()==1) cmeson+=1;
-                                   }// loop over mcparts
-        if(_Topo == 0){if(totalpi0!=1) return true;}
-	if(_Topo ==1 ){if(totalpi0!=1 || cmeson!=0) return true;}
-        _passed_events++;
-    return false;
+		if(_Topo == 0){if(totalpi0!=1) return true;}
+		if(_Topo ==1 ){if(totalpi0!=1 || cmeson!=0) return true;}
+		_passed_events++;
+	    return false;
 
+		
 
 
 
@@ -63,10 +63,12 @@ if(_Sig){
 
   bool singlepi0::finalize() {
 
-       std::cout<<" Summary of Generator Events : "<<std::endl;
+       std::cout<<"\n =====================\n Summary of Generator Events \n ====================="<<std::endl;
         std::cout<<" Total Events generated  " << _total_events<<std::endl;
-        std::cout<<" Total Single pi0 Topology"<<_Topo<<" Has this many events: " << _passed_events<<std::endl;
+	std::cout<<" We are looking at ccnc = "<<_ccnc<<std::endl;
+        std::cout<<" Total Single pi0 Topology :"<<_Topo<<":  Has this many events: " << _passed_events<<std::endl;
         std::cout<<" Percent of sample " << (double) _passed_events/_total_events<<std::endl;
+       std::cout<<"\n =====================\n End  of Generator Events \n ========================"<<std::endl;
 
   
     return true;
