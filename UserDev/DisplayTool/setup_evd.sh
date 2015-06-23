a35f7c2ff5817aa619e8fa10824c4be3a56ed0ce
Corey Adams <corey.adams@yale.edu>
2015-06-23 13:48:07 -0400

    Adding updated setup script

--- UserDev/DisplayTool/setup_evd.sh
+++ UserDev/DisplayTool/setup_evd.sh
@@ -17,6 +17,43 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 # This section extends the path and python path to run from anywhere
 
-export PATH=$DIR/python:$PATH
-export PYTHONPATH=$DIR/python/:$PYTHONPATH
+
+
+# This section verifies that python dependences are setup 
+
+PYTHONPATH_backup=$PYTHONPATH
+PATH_backup=$PATH
+
+# Test argparse
+if ! $(python -c "import argparse" &> /dev/null); then 
+  echo "Warning: can not use evd due to missing package argparse"
+  PYTHONPATH=$PYTHONPATH_backup
+  PATH=$PATH_backup
+  return
+fi
+
+# Test numpy
+if ! $(python -c "import numpy" &> /dev/null); then 
+  echo "Warning: can not use evd due to missing package numpy"
+  PYTHONPATH=$PYTHONPATH_backup
+  PATH=$PATH_backup
+  return
+fi
+
+# Test pyqt4
+if ! $(python -c "import PyQt4" &> /dev/null); then 
+  echo "Warning: can not use evd due to missing package PyQt4"
+  PYTHONPATH=$PYTHONPATH_backup
+  PATH=$PATH_backup
+  return
+fi
+
+if [[ ! ":$PATH:" == *":$DIR/python:"* ]]; then
+  export PATH=$DIR/python:$PATH
+fi
+
+if [[ ! ":$PYTHONPATH:" == *":$DIR/python:"* ]]; then
+  export PYTHONPATH=$DIR/python/:$PYTHONPATH
+fi
+
 
