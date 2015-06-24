c4cc1e817c7f40f91aac16c382d8179ba425dca5
Ariana Hackenburg <ariana.hackenburg@yale.edu>
2015-06-24 13:33:13 -0500

    Some changes to primary pi0 algo

--- UserDev/SelectionTool/ERTool/Algo/ERAlgoPrimaryPi0.cxx
+++ UserDev/SelectionTool/ERTool/Algo/ERAlgoPrimaryPi0.cxx
@@ -8,7 +8,6 @@ namespace ertool {
   ERAlgoPrimaryPi0::ERAlgoPrimaryPi0(const std::string& name) : AlgoBase(name)
   { 
     SetVerbose(true);
-//    SetMinDist();
     }
 
   void ERAlgoPrimaryPi0::Reset()
@@ -23,7 +22,7 @@ namespace ertool {
   bool ERAlgoPrimaryPi0::Reconstruct(const EventData &data, ParticleGraph& graph)
   {
 
-    //std::cout<<"\n\n\n NEW EVENT! "<<std::endl ;
+    std::cout<<"\n\n\n NEW EVENT! "<<std::endl ;
 
     if(graph.GetParticleNodes(RecoType_t::kShower).size() < 2) return true;	
 
@@ -35,6 +34,16 @@ namespace ertool {
     auto datacpy = data ;
     auto const& primaryTrks = graph.GetPrimaryNodes(RecoType_t::kTrack);
     bool primaryPi0 = true ; 
+
+//    auto mc = MCParticleGraph() ;
+//
+//    for ( auto const & m : mc.GetParticleArray()){
+//	std::cout<<"MCParts : "<< m.PdgCode()<<std::endl ;
+//    
+//	}
+
+    std::cout<<"graph diagram : "<<graph.Diagram() <<std::endl;
+
 
     //First, look only at events with pi0s
     for ( auto const & p : graph.GetParticleArray()){
@@ -48,8 +57,10 @@ namespace ertool {
 	    //	4) non-prim pi0 
 
 	    //If there are no primary tracks, it's a NC pi0
-	    if ( primaryTrks.size() == 0 )
-		graph.SetPrimary(pi0.ID());
+	    if ( primaryTrks.size() == 0 ){
+//		graph.SetPrimary(pi0.ID());
+		std::cout<<"ERAlgoPriamary NC pi0 "<<std::endl ;
+		}
 
 	    int nPrimTracks = primaryTrks.size() ;
 
@@ -57,7 +68,11 @@ namespace ertool {
 	    //Loop over primary tracks and compare vertices with primary pi0 
 	    for ( auto const& primID : graph.GetPrimaryNodes(RecoType_t::kTrack) ){
 
+//		auto const& thatID = graph.GetParticle(primID).RecoID();
+//		auto thatTrack  = data.Track(thatID) ;
 
+    	//	double IP = _findRel.FindClosestApproach(thisShower,thatTrack,vtx);
+//	//	std::cout<<"\nPrimary track pdgcode : " <<graph.GetParticle(primID).PdgCode() <<std::endl ;
 
 		auto trkParticle = graph.GetParticle(primID) ;
 		double _vtxDist = pow( pow(pi0.Vertex()[0] - trkParticle.Vertex()[0],2) 
@@ -69,19 +84,23 @@ namespace ertool {
 				            + pow(pi0.Vertex()[1] - thisTrack.at(thisTrack.size()-1)[1],2)
 				            + pow(pi0.Vertex()[2] - thisTrack.at(thisTrack.size()-1)[2],2) ,0.5) ;
 
-	//	std::cout<<"Min dist is: "<<_minDistVtx<<", and vtxDist is: "<<_vtxDist<<std::endl;
-	//	std::cout<<"pi0 vertex: "<<pi0.Vertex()[0]<<", "<<pi0.Vertex()[1]<<", "<<pi0.Vertex()[2]<<std::endl ;
-	//	std::cout<<"Track vertex: "<<trkParticle.Vertex()[0]<<", "<<trkParticle.Vertex()[1]<<", "<<trkParticle.Vertex()[2]<<std::endl ;
+//		std::cout<<"Min dist is: "<<_vtxDist <<std::endl ;//_pi0vtxToTrkEnd<<", and vtxDist is: "<<_vtxDist<<std::endl;
+//		std::cout<<"Min dist set at: "<<_minDistVtx<<std::endl; 
+//		std::cout<<"pi0 vertex: "<<pi0.Vertex()[0]<<", "<<pi0.Vertex()[1]<<", "<<pi0.Vertex()[2]<<std::endl ;
+//		std::cout<<"track vtx: "<<trkParticle.Vertex()[0]<<", "<<trkParticle.Vertex()[1]<<", "<<trkParticle.Vertex()[2]<<std::endl ;
+	//	std::cout<<"Track end: "<<thisTrack.at(thisTrack.size()-1)[0]
+	//				<<", "<<thisTrack.at(thisTrack.size()-1)[1]
+	//				<<", "<<thisTrack.at(thisTrack.size()-1)[2]<<std::endl ;
     	                
     	        if( _vtxDist < _minDistVtx){
 
-		    graph.SetPrimary(pi0.ID());
+//		    graph.SetPrimary(pi0.ID());
 
     	            //If pi0 is close enough to a track's vertex, 
     	            //make those guys siblings
     	            trkParticle.SetParticleInfo( trkParticle.PdgCode(), trkParticle.Mass(),
-					   trkParticle.Vertex() , trkParticle.Momentum());
-		    //std::cout<<"Make these guys sibs: "<<pi0.ID()<<", "<<primID <<std::endl; 
+					         trkParticle.Vertex() , trkParticle.Momentum());
+		    std::cout<<"Make these guys sibs: "<<pi0.ID()<<", "<<primID <<std::endl; 
     	            graph.SetSiblings(pi0.ID(),primID);
 		   }
 
@@ -102,14 +121,31 @@ namespace ertool {
 
 	    	}//Loop over primary tracks
 
+	    if (pi0.Primary() ) {
+
+		auto pi0ID = pi0.RecoID() ;
+	    	auto pi0Shr = data.Shower ( pi0ID ) ;
+
+		for (auto const & s : graph.GetParticleNodes(RecoType_t::kShower) ) {
+		
+		    auto thisID = graph.GetParticle(s).RecoID() ;
+	    	    auto thisShr = data.Shower ( thisID ) ;
+		    //Does thisShr come from pi0? 
+		    auto related = _primary_alg.From ( thisShr, pi0Shr ) ;
+
+		    if ( related ) 
+			graph.SetParentage(pi0.ID(),thisID) ;
+			
+		    }
+		}
+
 	//	std::cout<<"Finished looping over tracks"<<std::endl ;
 		//If prim tracks variable = 0, at least one primary track had "relation" to pi0.
 		//Otherwise, this pi0 walks alone.  
-		if(nPrimTracks == 0) 
-		    graph.SetPrimary(pi0.ID());
+	//	if(nPrimTracks == 0) 
+	//	    graph.SetPrimary(pi0.ID());
 		    
 	    }//if pi0
-//	    std::cout<<"Exiting if(pi0)..."<<std::endl ;
 	}//Loop over particle array
 
 
