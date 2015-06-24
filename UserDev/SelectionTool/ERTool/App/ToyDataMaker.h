1a821c06447738f91a689c5dc0d3ddfa0161aa6a
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-06-24 10:36:22 -0500

    as I go --kazu

--- UserDev/SelectionTool/ERTool/App/ToyDataMaker.h
+++ UserDev/SelectionTool/ERTool/App/ToyDataMaker.h
@@ -36,37 +36,65 @@ namespace ertool {
     /// Track maker
     void MakeTrack(double x,  double y,  double z,
 		   double px, double py, double pz,
-		   int pdg_code);
+		   int pdg_code)
+    {}
     
     /// Shower adder
     void MakeShower(double x, double y, double z,
 		    double px, double py, double pz,
-		    int pdg_code);
+		    int pdg_code)
+    {}
+
+    /// Vtx sigma
+    void SetVertexSigma(const double s)
+    { _vtx_sigma = s; }
 
     /// Trajectory parameter
-    void SetTrajectoryParams(const double step_size=5,
-			     const double step_sigma=1,
-			     const double angle_sigma=5,
-			     const double dedx=2);
+    void SetTrajectoryParams(const double step_size   = 5, // cm
+			     const double step_sigma  = 1, // cm
+			     const double angle_sigma = 5, // degree
+			     const double dedx = 2 // MeV/cm 
+			     )
+    {
+      _track_step_size  = step_size;
+      _track_step_sigma = step_sigma;
+      _track_step_angle_sigma = angle_sigma;
+      _track_step_dedx = dedx;
+    }
 
     /// Shower parameter
-    void SetShowerParams(const double length,
-			 const double dedx=2);
+    void SetShowerParams(const double length, // cm
+			 const double dedx=2  // MeV/cm
+			 )
+    {
+      _shower_length = length;
+      _shower_dedx   = dedx;
+    }
 
     /// Clear added elements
-    void Clear();
+    void Clear()
+    { _shower_v.clear(); _track_v.clear(); }
 
   private:
 
     std::vector< ertool::Shower > _shower_v;
     std::vector< ertool::Track  > _track_v;
+
+    /// vtx point smearing
     double _vtx_sigma;
-    double _track_step_size;
-    double _track_step_sigma;
-    double _track_step_angle_sigma;
-    double _track_step_dedx;
+
+    //
+    // Track params
+    //
+    double _track_step_size;        ///< step size of a trajectory in cm 
+    double _track_step_sigma;       ///< that for sigma 
+    double _track_step_angle_sigma; ///< angle obviously in degree
+    double _track_step_dedx;        ///< guess your unit
+
+    // Shower params
+    double _shower_length;
     double _shower_dedx;
-    
+
   };
 }
 #endif
