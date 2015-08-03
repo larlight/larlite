0d4b5669f7ffe8eb2026bc0517aae2ee0f5c804b
Ryan <grossora.neutrino@gmail.com>
2015-08-03 12:21:44 -0500

    fixing the Linkdef after cleaning -r

--- UserDev/LArLiteApp/Pi0Ana/LinkDef.h
+++ UserDev/LArLiteApp/Pi0Ana/LinkDef.h
@@ -9,12 +9,7 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-//#pragma link C++ class larlite::Pi0Quality+;
-#pragma link C++ class larlite::Pi0Reco+;
-//#pragma link C++ class larlite::Pi0ContainmentFilter+;
-//#pragma link C++ class ertool::ERAnaPi0Selection+;
 #pragma link C++ class larlite::singlepi0+;
-#pragma link C++ class ertool::ERAna1PI0+;
 #pragma link C++ class ertool::ERAnaPi0All+;
 #pragma link C++ class larlite::effpi0+;
 //ADD_NEW_CLASS ... do not change this line
