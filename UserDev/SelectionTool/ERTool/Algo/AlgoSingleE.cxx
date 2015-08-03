a4583fbffb007fc238c64f5468092d6c96718178
David <devish.c@gmail.com>
2015-08-03 12:06:53 -0500

    removing some commented out and unused code -- David Caratelli

--- UserDev/SelectionTool/ERTool/Algo/AlgoSingleE.cxx
+++ UserDev/SelectionTool/ERTool/Algo/AlgoSingleE.cxx
@@ -213,30 +213,6 @@ namespace ertool {
 	}
       }
 
-      /*
-      // Try and remove any shower that is on top of a track
-      // this could be due to a track mis-reconstructed as a 
-      // shower.
-
-      for (auto const& t : graph.GetParticleNodes(RecoType_t::kTrack)){
-	
-	auto const& track = datacpy.Track(graph.GetParticle(t).RecoID());
-	
-	// check if track and shower start points are close
-	if (thisShower.Start().Dist(track[0]) > 1 )
-	  continue;
-	// if their start point is very close:
-	// calculate a stupid track direction
-	size_t nsteps = track.size();
-	::geoalgo::Vector_t trackDir = track[int(nsteps/2.)]-track[0];
-	trackDir.Normalize();
-	// get dot product
-	double dot = thisShower.Dir()*trackDir;
-	if (dot > 0.9)
-	  single = false;
-      }
-      */
-
       // if still single (and no sister track) look at
       // dEdx to determine if e-like
       if (single && !_hassister){
