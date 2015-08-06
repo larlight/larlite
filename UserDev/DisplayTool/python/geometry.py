fe0deea1b6b73375d8101192c19167e921a123ec
Corey Adams <corey.adams@yale.edu>
2015-08-06 18:32:02 -0500

    Changing default color scheme for induction planes.

--- UserDev/DisplayTool/python/geometry.py
+++ UserDev/DisplayTool/python/geometry.py
@@ -65,7 +65,7 @@ class microboone(geometry):
     # and then call the base class __init__
     larutil.LArUtilManager.Reconfigure(fmwk.geo.kMicroBooNE)
     super(microboone, self).__init__()
-    self._levels = [ (-50,350),(-50,350),(-10,100) ]
+    self._levels = [ (-50,200),(-50,200),(-10,100) ]
     self._pedestals = [2000,2000,440]
     self._name = "uboone"
 
