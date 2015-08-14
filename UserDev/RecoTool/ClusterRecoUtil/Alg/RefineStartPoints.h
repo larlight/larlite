99d81ebdb6f4cd1d474dee18054030610980c021
Corey Adams <corey.adams@yale.edu>
2015-08-14 12:20:44 -0500

    Adding RefineStartPoints.  No real changes, just isolated and fixes to compile.

---
+++ UserDev/RecoTool/ClusterRecoUtil/Alg/RefineStartPoints.h
@@ -0,0 +1,66 @@
+/**
+ * \file RefineStartPoints.h
+ *
+ * \ingroup ClusterRecoUtil
+ * 
+ * \brief Class def header for a class RefineStartPoints
+ *
+ * @author cadams
+ */
+
+/** \addtogroup ClusterRecoUtil
+
+    @{*/
+#ifndef REFINESTARTPOINTS_H
+#define REFINESTARTPOINTS_H
+
+#include <iostream>
+#include <string>
+#include "ClusterParams.h"
+#include "ParamsAlgBase.h"
+
+namespace cluster {
+
+
+  /**
+     \class RefineStartPoints
+     User defined class RefineStartPoints ... these comments are used to generate
+     doxygen documentation!
+   */
+  class RefineStartPoints : public ParamsAlgBase {
+
+  public:
+
+    /// Default constructor
+    RefineStartPoints(){_name = "RefineStartPoints";}
+
+    /// Default destructor
+    ~RefineStartPoints(){}
+
+
+    /**
+     * Calculates the following variables:
+     * length
+     * width
+     * hit_density_1D
+     * hit_density_2D
+     * direction
+     * start_point
+     * end_point
+     * @param override [description]
+     */
+    void do_params_fill(cluster_params &, bool verbose = false);
+
+  private:
+
+    // The following variables are used to select the local hit list
+    float linearDistance;
+    float orthogDistance;
+
+  };
+
+} // cluster
+
+#endif
+/** @} */ // end of doxygen group 
+
