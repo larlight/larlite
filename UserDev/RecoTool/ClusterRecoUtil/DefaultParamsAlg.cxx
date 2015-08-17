02ee6b3a18c4a5b7d406bff21eef6a8b16709a6d
Corey Adams <corey.adams@yale.edu>
2015-08-17 14:45:32 -0500

    Fixing unimplemented destructor.  turning algs off by default until vetted.

--- UserDev/RecoTool/ClusterRecoUtil/DefaultParamsAlg.cxx
+++ UserDev/RecoTool/ClusterRecoUtil/DefaultParamsAlg.cxx
@@ -12,18 +12,20 @@ namespace cluster {
 
     // In the constructor, create, initialize and attach a set of algs
     GetAverages        * averages        = new GetAverages();
-    FillPolygon        * polygon         = new FillPolygon();
-    GetRoughStartPoint * roughStartPoint = new GetRoughStartPoint();
-    RefineDirection    * direction       = new RefineDirection();
-    RefineStartPoints  * startPoint      = new RefineStartPoints();
-    FindShoweringPoint * showeringpoint  = new FindShoweringPoint();
+    // FillPolygon        * polygon         = new FillPolygon();
+    // GetRoughStartPoint * roughStartPoint = new GetRoughStartPoint();
+    // RefineDirection    * direction       = new RefineDirection();
+    // RefineStartPoints  * startPoint      = new RefineStartPoints();
+    // FindShoweringPoint * showeringpoint  = new FindShoweringPoint();
 
     attachAlg(averages);
-    attachAlg(polygon);
-    attachAlg(roughStartPoint);
-    attachAlg(direction);
-    attachAlg(startPoint);
-    attachAlg(showeringpoint);
+    // attachAlg(polygon);
+    // attachAlg(roughStartPoint);
+    // attachAlg(direction);
+    // attachAlg(startPoint);
+    // attachAlg(showeringpoint);
+    SetDebug(true);
+    SetVerbose(false);
   }
 
 } // cluster
