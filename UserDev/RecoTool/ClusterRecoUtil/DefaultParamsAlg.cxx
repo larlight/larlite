26fd1e3f09fda9d5bea2987feff0818a4a365c6c
Corey Adams <corey.adams@yale.edu>
2015-08-17 11:31:01 -0500

    Forgot DefaultParamsAlg
    Sorry.

---
+++ UserDev/RecoTool/ClusterRecoUtil/DefaultParamsAlg.cxx
@@ -0,0 +1,31 @@
+#ifndef DEFAULTPARAMSALG_CXX
+#define DEFAULTPARAMSALG_CXX
+
+#include "DefaultParamsAlg.h"
+
+
+
+
+namespace cluster {
+
+  DefaultParamsAlg::DefaultParamsAlg(){
+
+    // In the constructor, create, initialize and attach a set of algs
+    GetAverages        * averages        = new GetAverages();
+    FillPolygon        * polygon         = new FillPolygon();
+    GetRoughStartPoint * roughStartPoint = new GetRoughStartPoint();
+    RefineDirection    * direction       = new RefineDirection();
+    RefineStartPoints  * startPoint      = new RefineStartPoints();
+    FindShoweringPoint * showeringpoint  = new FindShoweringPoint();
+
+    attachAlg(averages);
+    attachAlg(polygon);
+    attachAlg(roughStartPoint);
+    attachAlg(direction);
+    attachAlg(startPoint);
+    attachAlg(showeringpoint);
+  }
+
+} // cluster
+
+#endif
---
+++ UserDev/RecoTool/ClusterRecoUtil/DefaultParamsAlg.h
@@ -0,0 +1,52 @@
+/**
+ * \file DefaultParamsAlg.h
+ *
+ * \ingroup ClusterRecoUtil
+ * 
+ * \brief Class def header for a class DefaultParamsAlg
+ *
+ * @author cadams
+ */
+
+/** \addtogroup ClusterRecoUtil
+
+    @{*/
+#ifndef DEFAULTPARAMSALG_H
+#define DEFAULTPARAMSALG_H
+
+#include <iostream>
+#include "ClusterParamsAlg.h"
+#include "Alg/GetAverages.h"
+#include "Alg/FillPolygon.h"
+#include "Alg/GetRoughStartPoint.h"
+#include "Alg/RefineDirection.h"
+#include "Alg/RefineStartPoints.h"
+#include "Alg/FindShoweringPoint.h"
+
+namespace cluster {
+
+
+  /**
+     \class DefaultParamsAlg
+     User defined class DefaultParamsAlg ... these comments are used to generate
+     doxygen documentation!
+   */
+  class DefaultParamsAlg : public ClusterParamsAlg {
+
+  public:
+
+    /// Default constructor
+    DefaultParamsAlg();
+
+    /// Default destructor
+    ~DefaultParamsAlg();
+
+  };
+
+
+} // cluster
+
+
+#endif
+/** @} */ // end of doxygen group 
+
