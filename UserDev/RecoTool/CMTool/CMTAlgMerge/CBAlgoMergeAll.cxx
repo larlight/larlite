f6a83b2125858c126ac6d6da1b39c3291955f15f
Ariana Hackenburg <ariana.hackenburg@yale.edu>
2015-08-27 14:52:26 -0400

    ...forgot 2 files --ahack

--- UserDev/RecoTool/CMTool/CMTAlgMerge/CBAlgoMergeAll.cxx
+++ UserDev/RecoTool/CMTool/CMTAlgMerge/CBAlgoMergeAll.cxx
@@ -10,6 +10,8 @@ namespace cmtool {
   //----------------------------------------
   {
 
+  SetHits(6);
+
   }
 
   //--------------------------------------------------------
@@ -17,7 +19,9 @@ namespace cmtool {
                             const ::cluster::cluster_params &cluster2)
   //--------------------------------------------------------
   {
-    if(cluster1.hit_vector.size() && cluster2.hit_vector.size()) return true;
+    //if(cluster1.hit_vector.size() && cluster2.hit_vector.size()) return true;
+    //Make merge "all" a bit more flexible. Set _hits to 0 for true merge all
+    if (cluster1.N_Hits > _hits && cluster2.N_Hits > _hits) return true;
     else return false;
   }
   
