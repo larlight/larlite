730cdc10e190693310684760c8b85ea84081bbfa
Corey Adams <corey.adams@yale.edu>
2015-09-09 14:21:13 -0500

    Adding updates to axis 3d.  Reorganized things, soon it will be possible to veto a plane and flag it as bad.

--- UserDev/RecoTool/ShowerReco3D/ModularAlgo/Axis3DModule.cxx
+++ UserDev/RecoTool/ShowerReco3D/ModularAlgo/Axis3DModule.cxx
@@ -101,8 +101,9 @@ void Axis3DModule::do_reconstruction(const ShowerClusterSet_t & inputShowers, Sh
   // maintain a list of the good seeds and the errors of the seed vectors too:
   std::vector<TVector3> seedVectors;
   std::vector<float>    errorVector;
-  std::vector<bool>     convergeStatus;
+  std::vector<Status>   convergeStatus;
   std::vector<int>      convergeNumber;
+  int                   n_converged(0);
 
   float errorCutoff = 0.5;
 
@@ -127,97 +128,20 @@ void Axis3DModule::do_reconstruction(const ShowerClusterSet_t & inputShowers, Sh
       //             << " was rejected\n";
       // }
     }
-    errorCutoff += 0.1;
+    errorCutoff += 0.5;
   }
 
   // Now the list of seed vectors is reduced to a few candidate vectors that are close
 
-  // For each seed vector, compare the error of it vs. it's neighbors.
-  // If the error at a neighbor is better, move to that.
-
-
-  // Some variables needed for this loop:
-  bool converged = false;
-  int n_converged = 0;
-  int current_iteration = 0;
-
-  float thetaRangeStart = 0.05;
-  float thetaRangeMax  = M_PI / 8;
-  float thetaRangeMin  = 0.0005;
-
-  float nStepsStart = 8;
-  float nStepsMax = 4 * nStepsStart;
-
-  float alpha = 0.85;
-
   int vecIndex = 0;
   for (auto & vec : seedVectors) {
-    float thetaRange = thetaRangeStart;
-    float nSteps = nStepsStart;
-    converged = false;
-    current_iteration = 0;
-    while ( ! converged ) {
-      std::vector<TVector3> neighborVectors;
-      // Make the list of the neighbor vectors:
-      generateNearbyVectors(vec, thetaRange, nSteps, neighborVectors);
-      // Find out which neighbor vector is the best:
-      int bestNeighborIndex = findBestVector(neighborVectors, slopeByPlane, planes);
-      // Compare the error of the best neighbor to the error of the vec:
-      float error = getErrorOfProjection(neighborVectors[bestNeighborIndex], slopeByPlane, planes);
-
-
-
-      // Now we have the best neighbor vector.
-      // If things improved, just make the new vector the seed and iterate.
-      //
-      // If things didn't improve, make thetaRange and nSteps bigger and try again.
-      //
-      // If things didn't improve and thetaRange or nSteps are maxed out, bail.
-
-      if (error < errorVector.at(vecIndex)) {
-        vec = neighborVectors[bestNeighborIndex];
-        errorVector.at(vecIndex) = error;
-        // If there was improvement, make sure that nSteps is
-        // set to its min value:
-        nSteps = nStepsStart;
-        // For theta range, since that is the convergence speed, set it to
-        // the max of it's min value and alpha*currentValue:
-        thetaRange = fmin(thetaRangeStart, alpha * thetaRange);
-        if (thetaRange < thetaRangeMin) thetaRange = thetaRangeMin;
-      }
-      // Else things didn't improve
-      else {
-        // This step didn't help. Try to increase the granularity first:
-        if (nSteps < nStepsMax) {
-          nSteps += nStepsStart;
-        }
-        // If that fails, perhaps need to look at smaller angles:
-        else if (thetaRange > thetaRangeMin) {
-          thetaRange *= alpha;
-        }
-        else {
-          // Both nSteps and theta range are maxed out.  Bail on this point.
-          // std::cout << "bailing because both maxed out.\n";
-          break;
-        }
-      }
-      // Check if we've reached the desired error:
-      if (error < fTargetError) {
-        vec = neighborVectors[bestNeighborIndex];
-        errorVector.at(vecIndex) = error;
-        converged = true;
-      }
-
-      current_iteration ++;
-      if (current_iteration > fMaxIterations) {
-        converged = false;
-        break;
-      }
-    }
-    // Keep track of whether or not the previous point converged
-    convergeStatus.push_back(converged);
-    convergeNumber.push_back(current_iteration);
-    if (converged) n_converged ++;
+    Status exitStatus = kNStatus;
+    int n_iterations = 0;
+    errorVector.at(vecIndex) = optimzeVector(vec, exitStatus, n_iterations, slopeByPlane, planes);
+    convergeStatus.push_back(exitStatus);
+    convergeNumber.push_back(n_iterations);
+    if (exitStatus == kNormal)
+      n_converged++;
     vecIndex ++;
   }
 
@@ -228,20 +152,33 @@ void Axis3DModule::do_reconstruction(const ShowerClusterSet_t & inputShowers, Sh
   // Print out the final vectors and their errors:
   vecIndex = 0;
   for (auto & vec : seedVectors) {
-    if (n_converged == 0 || convergeStatus.at(vecIndex) ) {
+    // Print out info about this vector:
+    std::cout << "Error: " << errorVector.at(vecIndex)
+              << "\tVec: (" << vec.X() << ", "
+              << vec.Y() << ", "
+              << vec.Z() << ")"
+              << "\tStatus: ";
+    if (convergeStatus.at(vecIndex) == kNormal)
+      std::cout << "normal";
+    if (convergeStatus.at(vecIndex) == kIterMaxOut)
+      std::cout << "maxout";
+    if (convergeStatus.at(vecIndex) == kNotOptFit)
+      std::cout << "nonopt";
+    std::cout << "\tN: " << convergeNumber.at(vecIndex);
+    std::cout << std::endl;
+    for (unsigned int p = 0; p< planes.size();p++){
+      float slope = geomHelper -> Slope_3Dto2D(vec, planes[p]);
+      std::cout << "\t"<< p <<" - \tgoal:\t" << slopeByPlane.at(p) << "\tact:\t" << slope << "\n";     
+    }
+
+
+
+    if ( convergeStatus.at(vecIndex) == kNormal ) {
       if (errorVector.at(vecIndex) < bestError) {
         bestIndex = vecIndex;
         bestError = errorVector.at(vecIndex);
       }
     }
-    // std::cout << "Seed vector " <<  vecIndex << ": ("
-    //           << vec.X() << ", "
-    //           << vec.Y() << ", "
-    //           << vec.Z() << ")., error: "
-    //           << errorVector.at(vecIndex) << " did ";
-    // if (! convergeStatus.at(vecIndex)) std::cout << "not ";
-    // std::cout << "converge after " << convergeNumber.at(vecIndex) << " iterations."
-    //           << std::endl;
     vecIndex ++;
   }
 
@@ -250,89 +187,121 @@ void Axis3DModule::do_reconstruction(const ShowerClusterSet_t & inputShowers, Sh
   if (bestIndex != -1)
     resultShower.fDCosStart = seedVectors.at(bestIndex);
 
-  // while ( ! converged ) {
-  //   // Get a set of seed vectors:
-
-  //   int min_index = findBestVector(seedVectors, slopeByPlane, planes);
-  //   prevDir = direction;
-  //   direction = seedVectors.at(min_index);
-
-  //   // Print out an update:
-  //   float error = getErrorOfProjection(direction, slopeByPlane, planes);
-  //   std::cout << "Current vector: ("
-  //             << direction.X() << ", "
-  //             << direction.Y() << ", "
-  //             << direction.Z() << ")., error: "
-  //             << error
-  //             << ", thetaRange: " << thetaRange
-  //             << ", steps: " << nSteps_current
-  //             << std::endl;
-
-  //   ///TODO:
-  //   // Keep track of all vectors with better error than last iteration, or best N
-  //   // Which ever is lower.
-  //   // Then, make the ranges around those vectors based on some angle
-  //   // Continue keeping track of multiple vectors
-  //   // so that it doesn't get stuck in a local maximum
-
-
-  //   // Only reduce the range of theta if the error is getting good:
-  //   if (error < 1 ) {
-  //     // Narrow down if we're getting better
-  //     if (error < prevError) {
-  //       // Use the taylor series for arccosine
-  //       thetaRange = 1.2 * (PI / 2 - prevDir.Dot(direction) - pow(prevDir.Dot(direction), 3) / 6);
-  //       std::cout << "Theta range is " << thetaRange << std::endl;
-  //       nSteps_current = fNStepsMin;
-  //     }
-  //     // Go finer if it's good but not getting better
-  //     else
-  //       nSteps_current += fNStepsMin + 1;
-  //   }
-  //   // Otherwise, go for finer sampling to find a good spot:
-  //   else {
-  //     nSteps_current += 2 * fNStepsMin + 1;
-  //   }
-
-  //   prevError = error;
-
-  //   // Compare the 3D vector and the *known* 3D vector:
-
-
-
-  //   error /= inputShowers.size();
-
-  //   if (error < fTargetError) converged = true;
-  //   current_iteration ++;
-  //   if (current_iteration > fMaxIterations) break;
-  // }
-
   resultShower.fXYZStart = pointOnAxis;
 
 
-  // std::cout << "FINAL RESULT: \n";
+  std::cout << "FINAL RESULT: \n";
 
-  // std::cout
-  // // << "True vector: ("
-  // // << knownAxis.X() << ", "
-  // // << knownAxis.Y() << ", "
-  // // << knownAxis.Z() << "), "
-  //     << "Calculated vector: ("
-  //     << resultShower.fDCosStart.X() << ", "
-  //     << resultShower.fDCosStart.Y() << ", "
-  //     << resultShower.fDCosStart.Z() << ").\n";
-  // // << " Difference is " << acos(knownAxis.Dot(resultShower.fDCosStart)) << " radians.\n";
+  std::cout
+  // << "True vector: ("
+  // << knownAxis.X() << ", "
+  // << knownAxis.Y() << ", "
+  // << knownAxis.Z() << "), "
+      << "Calculated vector: ("
+      << resultShower.fDCosStart.X() << ", "
+      << resultShower.fDCosStart.Y() << ", "
+      << resultShower.fDCosStart.Z() << ").\n";
+  // << " Difference is " << acos(knownAxis.Dot(resultShower.fDCosStart)) << " radians.\n";
 
-  // for (unsigned int i = 0; i < inputShowers.size(); i++) {
-  //   larutil::PxPoint start2D, dir2D;
-  //   geomHelper -> Line_3Dto2D(pointOnAxis, resultShower.fDCosStart, planes[i], start2D, dir2D);
-  //   std::cout << "\tIn plane " << planes[i] << ", the projection is "
-  //             << (dir2D.t / dir2D.w) << ", the target is "
-  //             << slopeByPlane[i] << ".\n";
-  // }
+  for (unsigned int i = 0; i < inputShowers.size(); i++) {
+    larutil::PxPoint start2D, dir2D;
+    geomHelper -> Line_3Dto2D(pointOnAxis, resultShower.fDCosStart, planes[i], start2D, dir2D);
+    std::cout << "\tIn plane " << planes[i] << ", the projection is "
+              << (dir2D.t / dir2D.w) << ", the target is "
+              << slopeByPlane[i] << ".\n";
+  }
 
 
   return;
+
+}
+
+float Axis3DModule::optimzeVector(TVector3 & inputVector,
+                                  Status & exitStatus,
+                                  int & n_iterations,
+                                  const std::vector<float> & slopeByPlane,
+                                  const std::vector<int> & planes )
+{
+
+  // Some variables needed for this loop:
+  int n_converged = 0;
+  n_iterations = 0;
+
+
+  float thetaRangeStart = 0.05;
+  float thetaRangeMax  = M_PI / 8;
+  float thetaRangeMin  = 0.0005;
+
+  float nStepsStart = 8;
+  float nStepsMax = 4 * nStepsStart;
+
+  float alpha = 0.85;
+
+  float current_error = getErrorOfProjection(inputVector, slopeByPlane, planes);
+
+  // For each seed vector, compare the error of it vs. it's neighbors.
+  // If the error at a neighbor is better, move to that.
+
+  float thetaRange = thetaRangeStart;
+  float nSteps = nStepsStart;
+
+
+  while ( n_iterations < fMaxIterations ) {
+    std::vector<TVector3> neighborVectors;
+    // Make the list of the neighbor vectors:
+    generateNearbyVectors(inputVector, thetaRange, nSteps, neighborVectors);
+    // Find out which neighbor vector is the best:
+    int bestNeighborIndex = findBestVector(neighborVectors, slopeByPlane, planes);
+    // Compare the error of the best neighbor to the error of the inputVector:
+    float error = getErrorOfProjection(neighborVectors[bestNeighborIndex], slopeByPlane, planes);
+
+    // Now we have the best neighbor vector.
+    // If things improved, just make the new vector the seed and iterate.
+    //
+    // If things didn't improve, make thetaRange and nSteps bigger and try again.
+    //
+    // If things didn't improve and thetaRange or nSteps are maxed out, bail.
+
+    if (error < current_error) {
+      inputVector = neighborVectors[bestNeighborIndex];
+      current_error = error;
+      // If there was improvement, make sure that nSteps is
+      // set to its min value:
+      nSteps = nStepsStart;
+      // For theta range, since that is the convergence speed, set it to
+      // the max of it's min value and alpha*currentValue:
+      thetaRange = fmin(thetaRangeStart, alpha * thetaRange);
+      if (thetaRange < thetaRangeMin) thetaRange = thetaRangeMin;
+    }
+    // Else things didn't improve
+    else {
+      // This step didn't help. Try to increase the granularity first:
+      if (nSteps < nStepsMax) {
+        nSteps += nStepsStart;
+      }
+      // If that fails, perhaps need to look at smaller angles:
+      else if (thetaRange > thetaRangeMin) {
+        thetaRange *= alpha;
+      }
+      else {
+        // Both nSteps and theta range are maxed out.  Bail on this point.
+        // std::cout << "bailing because both maxed out.\n";
+        exitStatus = kNotOptFit;
+        return current_error;
+      }
+    }
+    // Check if we've reached the desired current_error:
+    if (current_error < fTargetError) {
+      exitStatus = kNormal;
+      return current_error;
+    }
+
+    n_iterations ++;
+  }
+
+  // The only way to make it out of that loop without returning is n_iterations > fMaxIterations
+  exitStatus = kIterMaxOut;
+  return current_error;
 
 }
 
