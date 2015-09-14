9d746815150f8c8e8b94e34a94d1e885eac73ecc
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-09-14 14:44:31 -0500

    kazu's selfish mods --kazu

--- UserDev/LArLiteApp/CosmicRejection/mac/cosmic_tag.py
+++ UserDev/LArLiteApp/CosmicRejection/mac/cosmic_tag.py
@@ -10,7 +10,6 @@ if len(sys.argv) < 2:
 from ROOT import gSystem
 from ROOT import larlite as fmwk
 from ROOT import ertool
-from seltool.ccsingleeDef import GetCCSingleEInstance
 #ertool.Manager
 
 # Create ana_processor instance
@@ -25,13 +24,6 @@ pid_algo.setVerbose(False)
 primary_algo = ertool.ERAlgoCRPrimary()
 
 secondary_algo = ertool.ERAlgoCRSecondary()
-secondary_algo.setMinDistTrkTrk   (10)
-secondary_algo.setMinDistComOrigin(5 ) 
-secondary_algo.setMinDistTrkDef   (20)
-secondary_algo.usePerpDistTrkTrk  (True) 
-secondary_algo.setMinDistTrkShr   (50) 
-secondary_algo.usePerpDistTrkShr  (False) 
-
 
 # Set input root file
 for x in xrange(len(sys.argv)-1):
@@ -43,10 +35,6 @@ my_proc.set_io_mode(fmwk.storage_manager.kREAD)
 # Specify output root file name
 my_proc.set_ana_output_file("singleE_selection.root")
 
-# here set E-cut for Helper & Ana modules
-#This cut is applied in helper... ertool showers are not made if the energy of mcshower or reco shower
-#is below this threshold. This has to be above 0 or else the code may segfault. This is not a "physics cut".
-#Do not change this value unless you know what you are doing.
 Ecut = 20 # in MeV
 
 my_ana = ertool.ERAnaCRTagger()
@@ -55,24 +43,20 @@ my_anaunit = fmwk.ExampleERSelection()
 my_anaunit._mgr.AddAlgo(pid_algo)
 my_anaunit._mgr.AddAlgo(primary_algo)
 my_anaunit._mgr.AddAlgo(secondary_algo)
-#my_anaunit._mgr.AddAlgo(my_algo)
 my_anaunit._mgr.AddAna(my_ana)
 #my_anaunit._mgr.AddCfgFile('new_empart.txt')
-#This cut is applied in helper... ertool showers are not made if the energy of mcshower or reco shower
-#is below this threshold. This has to be above 0 or else the code may segfault. This is not a "physics cut".
-#Do not change this value unless you know what you are doing.
 my_anaunit.SetMinEDep(Ecut)
-my_anaunit._mgr._mc_for_ana = True
+my_anaunit._mgr._mc_for_ana = False
 
 # ***************  Set Producers  ****************
 # First Argument: True = MC, False = Reco
-my_anaunit.SetShowerProducer(True,"mcreco");
-#my_anaunit.SetShowerProducer(False,"showerreco");
+#my_anaunit.SetShowerProducer(True,"")
+my_anaunit.SetShowerProducer(False,"showerrecopandora")
 #my_anaunit.SetShowerProducer(False,"newdefaultreco");
 #my_anaunit.SetShowerProducer(False,"pandoraNuShower");
 #my_anaunit.SetShowerProducer(False,"mergeall");
-my_anaunit.SetTrackProducer(True,"mcreco");
-#my_anaunit.SetTrackProducer(False,"stitchkalmanhit");
+#my_anaunit.SetTrackProducer(True,"mcreco");
+my_anaunit.SetTrackProducer(False,"stitchkalmanhit");
 #my_anaunit.SetTrackProducer(False,"costrk");
 #my_anaunit.SetVtxProducer(True,"generator");
 # ************************************************
