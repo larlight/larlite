1f24e99e7e04b9d057ff79e3b51444b3edbd6b2b
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-09-14 18:09:10 -0500

    viewer now respects cfg file! and you can override if you wish --kazu

--- python/seltool/erviewer.py
+++ python/seltool/erviewer.py
@@ -1,13 +1,24 @@
 from ROOT import ertool
-from basictool import GeoViewer
+from basictool import GeoViewer,fcllite
+import os
 
 class ERViewer (GeoViewer):
-
 
     def __init__(self,name):
         super(ERViewer,self).__init__()
         self._holder = ertool.GeoViewerERToolBackend()
+        self._cfg_mgr = fcllite.ConfigManager()
         self.set_window_title(name)
+        self.setConfigFile(ertool.kDefaultConfigFileName)
+
+    def setConfigFile(self, fname):
+
+        if not os.path.isfile(fname):
+            print 'Error! not a file:',fname
+            raise Exception
+        self._cfg_mgr.Reset()
+        self._cfg_mgr.AddCfgFile(fname);
+        self._holder.AcceptPSet(self._cfg_mgr.Config())
         
     def add(self,arg1,arg2,arg3=False):
         self._holder.Add(arg1,arg2,arg3)
