d789cb97356719c7f4f27dbafc9fca1063bd4227
Joseph Zennamo <jzennamo@uchicago.edu>
2015-09-14 10:53:37 -0500

    Adding a skeleton of a CR tagger Algo to speed things up, also integrated it into CCSingleE code, and added *card files* so that there is a central parameter setting

---
+++ UserDev/SelectionTool/ERTool/Algo/ERAlgoCosmicTagger.cxx
@@ -0,0 +1,104 @@
+#ifndef ERTOOL_ERALGOCOSMICTAGGER_CXX
+#define ERTOOL_ERALGOCOSMICTAGGER_CXX
+
+#include "ERAlgoCosmicTagger.h"
+
+namespace ertool {
+
+  ERAlgoCosmicTagger::ERAlgoCosmicTagger(const std::string& name) 
+  : AlgoBase(name)
+  {
+    
+    
+  }
+
+  void ERAlgoCosmicTagger::Reset()
+  {
+    std::cout<<"\t\t"<<__FUNCTION__<<" Reset " << std::endl;
+
+  }
+
+  void ERAlgoCosmicTagger::AcceptPSet(const ::fcllite::PSet& cfg)
+  { return; }
+
+  void ERAlgoCosmicTagger::ProcessBegin()
+  { return; }
+
+  bool ERAlgoCosmicTagger::Reconstruct(const EventData &data, ParticleGraph& graph)
+  {
+    
+    //Step 1: Find Track
+    //Step 2: Find Showers that are not close to the start or end
+    //Step 3: Assign the Parent as Muon
+    //Step 4: Profit.
+
+    // Iterate through all Tracks
+    
+    for(auto const& t : graph.GetParticleNodes(RecoType_t::kTrack)){
+            
+      auto const& trk = data.Track(graph.GetParticle(t).RecoID());
+
+      if(trk.Length() < 0.3) continue;
+
+      // Vector of Daughters gets reset
+      daughters.clear();
+      
+      geoalgo::Point_t trkStart(3);
+      geoalgo::Point_t trkEnd(3);
+      
+      trkStart = trk.front();
+      trkEnd = trk.back();
+      
+      //Iterate through all the showers
+      for(auto const& s : graph.GetParticleNodes(RecoType_t::kShower)){
+	
+      auto const& show = data.Shower(graph.GetParticle(s).RecoID());	
+	// Find Showers that aren't too far from a muon
+	// Find Showers that aren't near the front
+	// Find Showers that aren't near the back
+	geoalgo::Point_t showStart(3);
+	showStart = show.Start();
+
+	
+	if(_verbose){
+	  
+	  if(_geoAlgo.SqDist(showStart, trk) > (_minDist * _minDist) && _geoAlgo.SqDist(showStart, trk) < 1.1*(_minDist * _minDist)) 
+	    std::cout << "\t\t Shower-Track Dist : "<< sqrt(_geoAlgo.SqDist(showStart, trk)) << std::endl;
+	  if(showStart.SqDist(trkStart) > (_strtDist*_strtDist) && showStart.SqDist(trkStart) < 1.1*(_strtDist*_strtDist))
+	    std::cout << "\t\t Shower-TrkSt Dist : "<< sqrt(showStart.SqDist(trkStart))      << std::endl;
+	  if(showStart.SqDist(trkEnd)   > (_endDist * _endDist) && showStart.SqDist(trkEnd)   < 1.1*(_endDist * _endDist))
+	    std::cout << "\t\t Shower-TrkEd Dist : "<< sqrt(showStart.SqDist(trkEnd))        << std::endl;
+	  
+	}
+	
+	if(_geoAlgo.SqDist(showStart, trk) > (_minDist * _minDist) ) continue;
+	if(showStart.SqDist(trkStart) < (_strtDist*_strtDist) ) continue;
+	if(showStart.SqDist(trkEnd)   < (_endDist * _endDist) ) continue;
+	
+	if(_geoAlgo.SqDist(showStart, trk) < (_deltaDist *_deltaDist) ){	  
+	  daughters.push_back(graph.GetParticle(s).ID());
+	}
+	
+      } // iterate through all Showers
+
+      if(_verbose){
+	//	std::cout << "Number of deltas : " << daughters.size() << " for track " << graph.GetParticle(t).RecoID() << std::endl;        
+      }
+
+      for(auto const& showID : daughters){
+	
+
+	graph.SetParentage(graph.GetParticle(t).ID(), showID);
+
+      }//iterate daughter RecoIDs
+            
+    }//iterate through tracks
+    
+    return true;}
+
+  void ERAlgoCosmicTagger::ProcessEnd(TFile* fout)
+  {}
+
+}
+
+#endif
