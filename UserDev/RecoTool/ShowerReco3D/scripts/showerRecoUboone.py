da50c4ba5d4b224ad490184005db7aec55ee4b72
David <devish.c@gmail.com>
2015-09-16 14:00:15 -0500

    energy now added! -- David Caratelli

--- UserDev/RecoTool/ShowerReco3D/scripts/showerRecoUboone.py
+++ UserDev/RecoTool/ShowerReco3D/scripts/showerRecoUboone.py
@@ -47,7 +47,11 @@ def getShowerRecoAlgModular():
   axis3D.setVerbosity(True)
   axis3D.setSeedVectorErrorCutoff(0.5)
 
-  alg.AddShowerRecoModule(axis3D                           )
+  energy = showerreco.EnergyModule()
+  energy.setVerbosity(True)
+
+  alg.AddShowerRecoModule(axis3D)
+  alg.AddShowerRecoModule(energy)
   # alg.AddShowerRecoModule(showerreco.StartPoint2DModule()  )
   # alg.AddShowerRecoModule(showerreco.StartPoint3DModule()  )
   # alg.AddShowerRecoModule(showerreco.ShowerChargeModule()  )
