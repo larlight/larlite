6be4e8f742d5c3e12f402d407f3fd04d06267e43
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-09-17 10:32:44 -0500

    Added non-cosmic-primary counter. Added FlashMatch in the chain of cosmic_tag.py --kazu

--- UserDev/LArLiteApp/CosmicRejection/ERAnaCRTagger.h
+++ UserDev/LArLiteApp/CosmicRejection/ERAnaCRTagger.h
@@ -66,6 +66,7 @@ namespace ertool {
     unsigned int _ctr_cosmic_w_secondaries;
     unsigned int _ctr_non_cosmic_w_sec;
     unsigned int _ctr_non_cosmic;
+    unsigned int _ctr_non_cosmic_primary;
     unsigned int _ctr_interaction;
     unsigned int _ctr_orphan;
     
