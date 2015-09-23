c33b193b7c668b8becff32cd6ee4516dfa684cd8
David Kaleko <kaleko@nevis.columbia.edu>
2015-09-23 16:16:38 -0500

    implemented more shower quality plots in the python run script

--- UserDev/RecoTool/ShowerReco3D/scripts/gen_ShowerQuality_plots.py
+++ UserDev/RecoTool/ShowerReco3D/scripts/gen_ShowerQuality_plots.py
@@ -21,6 +21,8 @@ if len(sys.argv) != 2:
 
 #File that was output from ShowerQuality code including a ttree
 fname = sys.argv[1] #'ShowerQuality_ana_out.root'
+#Name of folder this script will create (or use, if exists) to put output image files in
+output_directory = 'generated_plots'
 #Name of the once-per-shower ttree from the ShowerQuality output root file
 tshowername = 'fShowerTree'
 #Name of the once-per-event ttree from the ShowerQuality output root file
@@ -29,31 +31,51 @@ teventname  = 'fEventTree'
 if not os.path.isfile(fname):
 	raise IOError('ShowerQuality output file does not exist: %s'%fname)
 
+#Output plots will be saved into a "generated_plots" folder
+if not os.path.isdir(output_directory):
+        os.makedirs(output_directory)
+
 #Turn the ShowerQuality output into a pandas dataframe like a BAMF
 shower_df = pd.DataFrame(root2array(fname,tshowername))
 event_df  = pd.DataFrame(root2array(fname,teventname) )
 
-#Plot of energy asymmetry
-fig = energy_asym_plot(shower_df)
-plt.savefig('energy_asym.pdf')
+#Plot of energy resolution
+fig = energy_res_plot(shower_df)
+plt.savefig('%s/energy_res.png'%output_directory)
 plt.show()
+
+#Plot of energy asymmetry
+fig = energy_res_plot(shower_df)
+plt.savefig('%s/energy_res.png'%output_directory)
+plt.show()
+
+#2D histogram of reco vs true energy
+for myplane in ['U','V','Y']:
+        fig = energy_corr_plot(shower_df,plane='%s'%myplane)
+        plt.savefig('%s/energy_correlation_%splane.png'%(output_directory,myplane))
+        plt.show()
 
 #Plot of start point accuracy
 fig = startpoint_acc_plot(shower_df)
-plt.savefig('startpoint_accuracy.pdf')
+plt.savefig('%s/startpoint_accuracy.png'%output_directory)
 plt.show()
 
 # 3D direction angular resolution
 fig = anglediff_3d_plot(shower_df)
-plt.savefig('3Ddir_accuracy.pdf')
+plt.savefig('%s/3Ddir_accuracy.png'%output_directory)
 plt.show()
 
 # dEdx plots
 fig = dEdx_plot(shower_df)
-plt.savefig('dEdx_resolution.pdf')
+plt.savefig('%s/dEdx_resolution.png'%output_directory)
 plt.show()
 
 # reco efficiency
 fig = reco_efficiency_vsenergy_plot(event_df)
-plt.savefig('reco_efficiency_vsenergy.pdf')
+plt.savefig('%s/reco_efficiency_vsenergy.png'%output_directory)
+plt.show()
+
+# Matched cluster efficiency
+fig = cluster_efficiency_perplane_plot(shower_df)
+plt.savefig('%s/cluster_efficiency_perplane.png'%output_directory)
 plt.show()
