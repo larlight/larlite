5026320fdf75aa1ba04bde45f3f5da8f4b0f60c6
Corey Adams <corey.adams@yale.edu>
2015-09-29 11:13:01 -0500

    skjdhfds

--- UserDev/RecoTool/ShowerReco3D/App/ShowerReco3D.cxx
+++ UserDev/RecoTool/ShowerReco3D/App/ShowerReco3D.cxx
@@ -7,129 +7,132 @@
 #include "DataFormat/DataFormat-TypeDef.h"
 namespace larlite {
 
-  ShowerReco3D::ShowerReco3D() : ana_base()
-  {
-    _name="ShowerReco3D";
-    fInputProducer  = "fuzzycluster";
-    fOutputProducer = "showerreco";
-    fUsePFParticle  = false;
-  }
-  
-  bool ShowerReco3D::initialize() {
+ShowerReco3D::ShowerReco3D() : ana_base()
+{
+  _name = "ShowerReco3D";
+  fInputProducer  = "fuzzycluster";
+  fOutputProducer = "showerreco";
+  fUsePFParticle  = false;
+}
 
-    if (_fout)
-      _fout->cd();
+bool ShowerReco3D::initialize() {
 
-    fManager.Initialize();
+  if (_fout)
+    _fout->cd();
 
-    return true;
-  }
-  
-  bool ShowerReco3D::analyze(storage_manager* storage) {
+  fManager.Initialize();
 
-    // Re-initialize tools
-    fManager.Reset();
+  return true;
+}
 
-    // Retrieve clusters and fed into the algorithm
-    std::vector<std::vector<Hit2D> > local_clusters;
-    fCRUHelper.GenerateHit2D(storage,fInputProducer,local_clusters);
+bool ShowerReco3D::analyze(storage_manager* storage) {
 
-    // Create output data product holder
-    auto shower_v = storage->get_data<event_shower>(fOutputProducer);
-    shower_v->clear();
+  // Re-initialize tools
+  fManager.Reset();
 
-    // set event ID through storage manager
-    storage->set_id(storage->get_data<event_cluster>(fInputProducer)->run(),
-                    storage->get_data<event_cluster>(fInputProducer)->subrun(),
-                    storage->get_data<event_cluster>(fInputProducer)->event_id());
+  // Retrieve clusters and fed into the algorithm
+  std::vector<std::vector<Hit2D> > local_clusters;
+  fCRUHelper.GenerateHit2D(storage, fInputProducer, local_clusters);
 
-    // Result shower holder
-    std::vector< ::showerreco::Shower_t> res_shower_v;
-    
-    // Run matching
-    std::vector<std::vector<unsigned int> > matched_pairs;
-    if(!fUsePFParticle) {
-      matched_pairs = fManager.Reconstruct(local_clusters,res_shower_v);
-    }else{
+  // Create output data product holder
+  auto shower_v = storage->get_data<event_shower>(fOutputProducer);
+  shower_v->clear();
 
-      auto pfpart_v = storage->get_data<event_pfpart>(fInputProducer);
+  // set event ID through storage manager
+  storage->set_id(storage->get_data<event_cluster>(fInputProducer)->run(),
+                  storage->get_data<event_cluster>(fInputProducer)->subrun(),
+                  storage->get_data<event_cluster>(fInputProducer)->event_id());
 
-      if(pfpart_v && pfpart_v->size()) {
-      
-        //auto const& ass_clusters = pfpart_v->association(data::kCluster, fInputProducer);
-        event_cluster* ev_cluster = nullptr;
-        auto const& ass_clusters = storage->find_one_ass(pfpart_v->id(),ev_cluster,pfpart_v->name());
-        // Loop over pfparticles
-        for(size_t pfpart_index=0; pfpart_index<pfpart_v->size(); ++pfpart_index) {
-          
-          auto const& pfp = (*pfpart_v)[pfpart_index];
-          
-          // If this is not pdg=11 pfparticle, ignore
-          if(pfp.PdgCode()!=11) continue;
-          
-          // Otherwise we store the association information in matched_pair
-          matched_pairs.push_back(std::vector<unsigned int>());
-          
-          auto& last_pair = (*matched_pairs.rbegin());
-          
-          last_pair.reserve(ass_clusters[pfpart_index].size());
-          
-          // Loop over associated index for *this* pfparticle
-          for(auto const& ass_index : ass_clusters[pfpart_index]) 
-            last_pair.push_back(ass_index);
-        }
+  // Result shower holder
+  std::vector< ::showerreco::Shower_t> res_shower_v;
+
+
+  // Run matching
+  std::vector<std::vector<unsigned int> > matched_pairs;
+  if (!fUsePFParticle) {
+    matched_pairs = fManager.Reconstruct(local_clusters, res_shower_v);
+  } else {
+
+    auto pfpart_v = storage->get_data<event_pfpart>(fInputProducer);
+
+    if (pfpart_v && pfpart_v->size()) {
+
+      //auto const& ass_clusters = pfpart_v->association(data::kCluster, fInputProducer);
+      event_cluster* ev_cluster = nullptr;
+      auto const& ass_clusters = storage->find_one_ass(pfpart_v->id(), ev_cluster, pfpart_v->name());
+      // Loop over pfparticles
+      for (size_t pfpart_index = 0; pfpart_index < pfpart_v->size(); ++pfpart_index) {
+
+        auto const& pfp = (*pfpart_v)[pfpart_index];
+
+        // If this is not pdg=11 pfparticle, ignore
+        if (pfp.PdgCode() != 11) continue;
+
+        // Otherwise we store the association information in matched_pair
+        matched_pairs.push_back(std::vector<unsigned int>());
+
+        auto& last_pair = (*matched_pairs.rbegin());
+
+        last_pair.reserve(ass_clusters[pfpart_index].size());
+
+        // Loop over associated index for *this* pfparticle
+        for (auto const& ass_index : ass_clusters[pfpart_index])
+          last_pair.push_back(ass_index);
       }
-
-      fManager.Reconstruct(local_clusters,
-                           matched_pairs,
-                           res_shower_v);
     }
 
 
-    for(auto const& res_shower : res_shower_v) {
-
-      shower s;
-      s.set_id ( shower_v->size() );
-
-      s.set_total_energy        ( res_shower.fTotalEnergy         );
-      s.set_total_energy_err    ( res_shower.fSigmaTotalEnergy    );
-      s.set_total_MIPenergy     ( res_shower.fTotalMIPEnergy      );
-      s.set_total_MIPenergy_err ( res_shower.fSigmaTotalMIPEnergy );
-      s.set_total_best_plane    ( res_shower.fBestPlane.Plane     );
-      s.set_direction           ( res_shower.fDCosStart           );
-      s.set_direction_err       ( res_shower.fSigmaDCosStart      );
-      s.set_start_point         ( res_shower.fXYZStart            );
-      s.set_start_point_err     ( res_shower.fSigmaXYZStart       );
-      s.set_dedx                ( res_shower.fdEdx                );
-      s.set_dedx_err            ( res_shower.fSigmadEdx           );
-      s.set_length              ( res_shower.fLength              );
-      s.set_opening_angle       ( res_shower.fOpeningAngle        );
-
-      shower_v->emplace_back(s);
-    }
-        
-    // Make sure result has the same size 
-    if(shower_v->size() != matched_pairs.size())
-      throw ::showerreco::ShowerRecoException("Mismatch in # of showers from algorithm's return!");
-
-    for(size_t index = 0; index < shower_v->size(); ++index)
-
-      (*shower_v)[index].set_id(index);
-
-    // create association for showers
-    auto shower_ass_v = storage->get_data<event_ass>(shower_v->name());    
-    shower_ass_v->set_association(shower_v->id(),product_id(data::kCluster,fInputProducer),matched_pairs);
-    //shower_v->set_association(data::kCluster,fInputProducer,matched_pairs);
-    // std::cout << "Done with this event\n";
-    return true;
+    fManager.Reconstruct(local_clusters,
+                         matched_pairs,
+                         res_shower_v);
   }
-  
-  bool ShowerReco3D::finalize() {    
-    
-    fManager.Finalize(_fout);
 
-    return true;
+
+
+  for (auto const& res_shower : res_shower_v) {
+
+    shower s;
+    s.set_id ( shower_v->size() );
+
+    s.set_total_energy        ( res_shower.fTotalEnergy         );
+    s.set_total_energy_err    ( res_shower.fSigmaTotalEnergy    );
+    s.set_total_MIPenergy     ( res_shower.fTotalMIPEnergy      );
+    s.set_total_MIPenergy_err ( res_shower.fSigmaTotalMIPEnergy );
+    s.set_total_best_plane    ( res_shower.fBestPlane.Plane     );
+    s.set_direction           ( res_shower.fDCosStart           );
+    s.set_direction_err       ( res_shower.fSigmaDCosStart      );
+    s.set_start_point         ( res_shower.fXYZStart            );
+    s.set_start_point_err     ( res_shower.fSigmaXYZStart       );
+    s.set_dedx                ( res_shower.fdEdx                );
+    s.set_dedx_err            ( res_shower.fSigmadEdx           );
+    s.set_length              ( res_shower.fLength              );
+    s.set_opening_angle       ( res_shower.fOpeningAngle        );
+
+    shower_v->emplace_back(s);
   }
-  
+
+  // Make sure result has the same size
+  if (shower_v->size() != matched_pairs.size())
+    throw ::showerreco::ShowerRecoException("Mismatch in # of showers from algorithm's return!");
+
+  for (size_t index = 0; index < shower_v->size(); ++index)
+
+    (*shower_v)[index].set_id(index);
+
+  // create association for showers
+  auto shower_ass_v = storage->get_data<event_ass>(shower_v->name());
+  shower_ass_v->set_association(shower_v->id(), product_id(data::kCluster, fInputProducer), matched_pairs);
+  //shower_v->set_association(data::kCluster,fInputProducer,matched_pairs);
+  // std::cout << "Done with this event\n";
+  return true;
+}
+
+bool ShowerReco3D::finalize() {
+
+  fManager.Finalize(_fout);
+
+  return true;
+}
+
 }
 #endif
--- UserDev/RecoTool/ShowerReco3D/Base/ShowerRecoManager.cxx
+++ UserDev/RecoTool/ShowerReco3D/Base/ShowerRecoManager.cxx
@@ -7,126 +7,130 @@
 
 namespace showerreco {
 
-  ShowerRecoManager::ShowerRecoManager()
-    : fMatchMgr(nullptr)
-  {
-    auto geom = ::larutil::Geometry::GetME();
-    fMatchMgr = new ::cmtool::CMatchManager(geom->Nplanes());
-    _matching_times = 0.;
-    _matching_calls = 0;
-    _CPAN_times = 0.;
-    _CPAN_calls = 0;
+ShowerRecoManager::ShowerRecoManager()
+  : fMatchMgr(nullptr)
+{
+  auto geom = ::larutil::Geometry::GetME();
+  fMatchMgr = new ::cmtool::CMatchManager(geom->Nplanes());
+  _matching_times = 0.;
+  _matching_calls = 0;
+  _CPAN_times = 0.;
+  _CPAN_calls = 0;
+}
+
+void ShowerRecoManager::Initialize()
+{
+  for (auto alg : _alg_v)
+    alg->Initialize();
+
+  return;
+}
+
+void ShowerRecoManager::Reset()
+{
+  for (auto alg : _alg_v) alg->Reset();
+
+  fMatchMgr->Reset();
+}
+
+ClusterAss_t ShowerRecoManager::Reconstruct (const std::vector<std::vector<larutil::PxHit> >& clusters,
+    std::vector<showerreco::Shower_t>& showers)
+{
+  showers.clear();
+  fMatchMgr->SetClusters(clusters);
+
+  ClusterAss_t res_ass;
+  // Run matching & retrieve matched cluster indices
+  try {
+    _watch.Start();
+    fMatchMgr->Process();
+    _matching_times += _watch.RealTime();
+    _matching_calls += 1;
   }
-
-  void ShowerRecoManager::Initialize()
-  {
-    for(auto alg : _alg_v)
-      alg->Initialize();
-    
-    return;
-  }
-
-  void ShowerRecoManager::Reset()
-  {
-    for(auto alg : _alg_v) alg->Reset();
-
-    fMatchMgr->Reset();
-  }
-
-  ClusterAss_t ShowerRecoManager::Reconstruct (const std::vector<std::vector<larutil::PxHit> >& clusters,
-                                               std::vector<showerreco::Shower_t>& showers)
-  {
-    showers.clear();
-    fMatchMgr->SetClusters(clusters);
-
-    ClusterAss_t res_ass;
-    // Run matching & retrieve matched cluster indices
-    try{
-      _watch.Start();
-      fMatchMgr->Process();
-      _matching_times += _watch.RealTime();
-      _matching_calls += 1;
-    }
-    catch( ::cmtool::CMTException &e){
-      e.what();
-      return res_ass;
-    }
-    res_ass = fMatchMgr->GetBookKeeper().GetResult();
-  
-    Process(res_ass, showers);
-
+  catch ( ::cmtool::CMTException &e) {
+    e.what();
     return res_ass;
   }
-  
-  void ShowerRecoManager::Reconstruct (const std::vector<std::vector<larutil::PxHit> >& clusters,
-                                       const ClusterAss_t& ass,
-                                       std::vector<showerreco::Shower_t>& showers)
-  {
-    showers.clear();
-    fMatchMgr->SetClusters(clusters);
-    Process(ass,showers);
-  }
-  
-  void ShowerRecoManager::Process(const ClusterAss_t& ass,
-                                  std::vector<showerreco::Shower_t>& showers)
-  {
+  res_ass = fMatchMgr->GetBookKeeper().GetResult();
 
-    showers.clear();
-    showers.reserve(ass.size());
-    for(auto const& pair : ass) {
 
-      ShowerClusterSet_t in_clusters;
-      in_clusters.reserve(pair.size());
-      
-      for(auto const& index : pair){
-        in_clusters.emplace_back( fMatchMgr->GetInputClusters()[index] );
-        // Make sure to fill the params:
-	_watch.Start();
-        _params_alg.FillParams(in_clusters.back());
-	_CPAN_times += _watch.RealTime();
-	_CPAN_calls += 1;
-      }
+  Process(res_ass, showers);
 
-      for(auto& shower_alg : _alg_v)
 
-        showers.emplace_back(shower_alg->RecoOneShower(in_clusters));
+  return res_ass;
+}
 
-      for(auto& shower_ana : _ana_v)
+void ShowerRecoManager::Reconstruct (const std::vector<std::vector<larutil::PxHit> >& clusters,
+                                     const ClusterAss_t& ass,
+                                     std::vector<showerreco::Shower_t>& showers)
+{
+  showers.clear();
+  fMatchMgr->SetClusters(clusters);
+  Process(ass, showers);
+}
 
-        shower_ana->Analyze(showers.back(),in_clusters);
-        
+void ShowerRecoManager::Process(const ClusterAss_t& ass,
+                                std::vector<showerreco::Shower_t>& showers)
+{
+
+  showers.clear();
+  showers.reserve(ass.size());
+  for (auto const& pair : ass) {
+
+    ShowerClusterSet_t in_clusters;
+    in_clusters.reserve(pair.size());
+
+    for (auto const& index : pair) {
+      in_clusters.emplace_back( fMatchMgr->GetInputClusters()[index] );
+      // Make sure to fill the params:
+      _watch.Start();
+      _params_alg.FillParams(in_clusters.back());
+      _CPAN_times += _watch.RealTime();
+      _CPAN_calls += 1;
     }
 
+
+    for (auto& shower_alg : _alg_v)
+
+      showers.emplace_back(shower_alg->RecoOneShower(in_clusters));
+
+
+    for (auto& shower_ana : _ana_v)
+
+      shower_ana->Analyze(showers.back(), in_clusters);
+
   }
 
-  void ShowerRecoManager::Finalize(TFile* fout){
-    if (!fout)
-      return;
-    fout->cd();
+}
 
-    // print out profiling performance for CPAN and matching
-    double matching_T = _matching_times / (double) _matching_calls;
-    double CPAN_T    = _CPAN_times / (double) _CPAN_calls;
-    std::cout << std::endl
-	      << "=================== Time Report =====================" << std::endl
-	      << "Matching Time: " << std::setw(10) << matching_T * 1.e6 << " [us/cluster]" << std::endl
-	      << "CPAN Time    : " << std::setw(10) << CPAN_T * 1.e6 << " [us/cluster]" << std::endl
-	      << "=====================================================" << std::endl
-	      << std::endl;
-    
-    // loop through algorithms
-    for (auto& alg : _alg_v)
-      alg->Finalize(fout);
-
-    // loop through ana modules
-    for(auto& ana : _ana_v) {
-      auto tree = ana->GetTree();
-      if (tree) tree->Write();
-    }
-
+void ShowerRecoManager::Finalize(TFile* fout) {
+  if (!fout)
     return;
+  fout->cd();
+
+  // print out profiling performance for CPAN and matching
+  double matching_T = _matching_times / (double) _matching_calls;
+  double CPAN_T    = _CPAN_times / (double) _CPAN_calls;
+  std::cout << std::endl
+            << "=================== Time Report =====================" << std::endl
+            << "Matching Time: " << std::setw(10) << matching_T * 1.e6 << " [us/cluster]" << std::endl
+            << "CPAN Time    : " << std::setw(10) << CPAN_T * 1.e6 << " [us/cluster]" << std::endl
+            << "=====================================================" << std::endl
+            << std::endl;
+
+  // loop through algorithms
+  for (auto& alg : _alg_v)
+    alg->Finalize(fout);
+
+  // loop through ana modules
+  for (auto& ana : _ana_v) {
+    auto tree = ana->GetTree();
+    if (tree) tree->Write();
   }
-  
+
+  return;
+}
+
 }
 
 #endif
