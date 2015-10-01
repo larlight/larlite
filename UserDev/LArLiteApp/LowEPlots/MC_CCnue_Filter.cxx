e8afe627e8f44e8cbcf921cef893027461f73ce8
Joseph Zennamo <jzennamo@uchicago.edu>
2015-10-01 09:39:15 -0500

    Fixing all the larlite filters to do what I actually want

--- UserDev/LArLiteApp/LowEPlots/MC_CCnue_Filter.cxx
+++ UserDev/LArLiteApp/LowEPlots/MC_CCnue_Filter.cxx
@@ -27,42 +27,32 @@ namespace larlite {
       return false;
     }
     
-    bool ret = true;
+    bool ret = false;
+    bool CC = false;
+    bool nue = false;
 
     for(auto &ev_mctruth : *ev_mctruth_v){
-      
+     
       //Enforce CC interaction channel
-      if( ev_mctruth.GetNeutrino().CCNC() != 0 )
-	return false;
+      if( ev_mctruth.GetNeutrino().CCNC() == 0 ){
+	CC = true;}
 
       // If there are any detector external interactions veto the event
-      if(!(TPC.Contain(ev_mctruth.GetNeutrino().Nu().Trajectory().back().Position())))
-	return false;       
+      if(!(TPC.Contain(ev_mctruth.GetNeutrino().Nu().Trajectory().back().Position()))){
+	return false;}
       
-            
-      //Enforce that there is exactly 1 electron, above 20MeV kinetic energy
-      //I don't care about neutrons, weird quarks, the neutrino, etc.
-      size_t n_electrons = 0;
-      
-      for(auto const& particle : ev_mctruth.GetParticles()){
-	
-	if( abs(particle.PdgCode()) == 11 && 
-	    particle.StatusCode() == 1)
-	  n_electrons++;
-	
-      }
-      
-      if( n_electrons > 0)
-	ret = false;
-      
-      
-      if(ret == true) continue;
-      
-    }  
+      if(ev_mctruth.GetNeutrino().Nu().PdgCode() == 12){
+	nue = true;}        
+    }
     
     //check the status of the ret variable
+    if(CC && nue){ret = true;} 
+    else{ret = false;}
+
+    
     return ret;  
     
+
   }
 
   bool MC_CCnue_Filter::finalize() {
--- UserDev/LArLiteApp/LowEPlots/MC_NC_Filter.cxx
+++ UserDev/LArLiteApp/LowEPlots/MC_NC_Filter.cxx
@@ -27,25 +27,13 @@ namespace larlite {
       return false;
     }
 
-    //Grab the MCShowers
-    auto ev_mcshower = storage->get_data<event_mcshower>("mcreco");
-    if(!ev_mcshower) {
-      print(larlite::msg::kERROR,__FUNCTION__,Form("Did not find specified data product, mcshower!"));
-      return false;
-    }
-
-    bool ret = true;
-    
-    bool isCharPion = false;
-    bool isPhoton = false;
+    bool ret = false;
     bool NC = false;
     
     for(auto &ev_mctruth : *ev_mctruth_v){      
       //Checks if the neutrino exchanged a W or a Z during the interaction 
-      if(ev_mctruth.GetNeutrino().CCNC() == 1){
-	NC = true;}
-      else{
-	NC = false;}
+      if(ev_mctruth.GetNeutrino().CCNC() == 1){NC = true;}
+      else{NC = false;}
       
       // If there are any detector external interactions veto the event
       if(!(TPC.Contain(ev_mctruth.GetNeutrino().Nu().Trajectory().back().Position()))){
@@ -54,30 +42,11 @@ namespace larlite {
       
     }//loops through MCTruth
     
-    for(auto &mcs : *ev_mcshower){
-      //Checks for prompt photons from delta or neutral pions
-      if(mcs.PdgCode() == 22 && 
-	 (abs(mcs.AncestorPdgCode()) == 2224 || 
-	  abs(mcs.AncestorPdgCode()) == 2214 || 
-	  abs(mcs.AncestorPdgCode()) == 2114 || 
-	  abs(mcs.AncestorPdgCode()) == 111))
-	isPhoton = true;
-      //Checks for electrons coming from charged pion->muon->michel decay
-      if(abs(mcs.PdgCode()) == 11 &&
-	 abs(mcs.MotherPdgCode()) == 13 &&
-	 abs(mcs.AncestorPdgCode()) == 211)
-	isCharPion = true;	      
-    }//loops through MCShowers
-    
-    
     //Enforce CC interaction channel
-    if(NC){
-      ret = true;}
+    if(NC){ret = true;}
     else{ret = false;}
     
     //check the status of the ret variable
-    //   if (_flip)
-    //  return (!ret);
     return ret;  
     
   }
