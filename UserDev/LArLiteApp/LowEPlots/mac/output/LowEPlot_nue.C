8175a52e40f30f95d6203e3edb95b42922d496ca
Joseph Zennamo <jzennamo@uchicago.edu>
2015-10-01 09:40:18 -0500

    Buffing up the Low E Excess drawing code

--- UserDev/LArLiteApp/LowEPlots/mac/output/LowEPlot_nue.C
+++ UserDev/LArLiteApp/LowEPlots/mac/output/LowEPlot_nue.C
@@ -4,25 +4,56 @@ void LowEPlot_nue(){
   gStyle->SetOptFit(0000);
   gStyle->SetPadBorderMode(0);
   gStyle->SetPadBottomMargin(0.15);
-  gStyle->SetPadLeftMargin(0.15);
+  gStyle->SetPadLeftMargin(0.1);
   gStyle->SetPadRightMargin(0.05);
   gStyle->SetFrameBorderMode(0);
   gStyle->SetCanvasBorderMode(0);
   gStyle->SetPalette(1);
   gStyle->SetCanvasColor(0);
   gStyle->SetPadColor(0);
+
+  TCanvas* c = new TCanvas("c","",1100,1700);
+  c->cd();
+  
+  TH1 *hr = new TH2F("hr","",100,.100,3,100,0,725);
+  hr->SetStats(0);
+  hr->GetXaxis()->SetTitle("Reconstructed Neutrino Energy [GeV]");
+  hr->GetXaxis()->SetLabelFont(42);
+  hr->GetXaxis()->SetLabelSize(0.04);
+  hr->GetXaxis()->SetTitleSize(0.06);
+  hr->GetXaxis()->SetTitleOffset(1.1);
+  hr->GetXaxis()->CenterTitle();
+  hr->GetYaxis()->SetTitle("Events");
+  hr->GetYaxis()->SetNdivisions(509);
+  hr->GetXaxis()->SetNdivisions(515);
+  hr->GetYaxis()->SetLabelFont(42);
+  hr->GetYaxis()->SetLabelOffset(0.01);
+  hr->GetYaxis()->SetLabelSize(0.04);
+  hr->GetYaxis()->SetTitleSize(0.07);
+  hr->GetYaxis()->SetTitleOffset(0.7);
+  hr->GetYaxis()->CenterTitle();
+  //  hr->GetYaxis()->SetRangeUser(0,1);
+  //  hr->SetMaximum((200*sin2));
+  hr->Draw();
     
 
-  TH1D *nue    = new TH1D("NuEInt",";Reconstructed Neutrino Energy [GeV];Events",25, 50, 3000);
-  TH1D *numu   = new TH1D("NuMu",  ";Reconstructed Neutrino Energy [GeV];Events",25, 50, 3000);
-  TH1D *nc     = new TH1D("NC",    ";Reconstructed Neutrino Energy [GeV];Events",25, 50, 3000);
-  TH1D *cosmic = new TH1D("Cos",   ";Reconstructed Neutrino Energy [GeV];Events",25, 50, 3000);
-  TH1D *dirt   = new TH1D("Dirt",  ";Reconstructed Neutrino Energy [GeV];Events",25, 50, 3000);
+
+
+  TH1D *nue    = new TH1D("NuEInt",";Reconstructed Neutrino Energy [GeV];Events",39, .1, 3);
+  TH1D *numu   = new TH1D("NuMu",  ";Reconstructed Neutrino Energy [GeV];Events",39, .1, 3);
+  TH1D *nc     = new TH1D("NC",    ";Reconstructed Neutrino Energy [GeV];Events",39, .1, 3);
+  TH1D *cosmic = new TH1D("Cos",   ";Reconstructed Neutrino Energy [GeV];Events",39, .1, 3);
+  TH1D *dirt   = new TH1D("Dirt",  ";Reconstructed Neutrino Energy [GeV];Events",39, .1, 3);
 
 
   THStack *LEE = new THStack("lee", ";Reconstructed Neutrino Energy [GeV];Events");
 
-  TTree *tree;  
+  TTree *tree1;  
+  TTree *tree2;  
+  TTree *tree3;  
+  TTree *tree4;  
+  TTree *tree5;  
+
   Int_t           _numEvts;
   Int_t           _neutrinos;
   Double_t        _e_nuReco;
@@ -34,144 +65,143 @@ void LowEPlot_nue(){
   TBranch        *b_e_dep;   //!
   TBranch        *b_weight;   //!
 
-   TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("singleE_nue_selection.root");
-  if (!f || !f->IsOpen()) {
-    f = new TFile("singleE_nue_selection.root");
+   TFile *f1 = (TFile*)gROOT->GetListOfFiles()->FindObject("singleE_nue_selection.root");
+  if (!f1 || !f1->IsOpen()) {
+    f1 = new TFile("singleE_nue_selection.root");
   }
 
-  f->GetObject("beamNuE",tree);
+  f1->GetObject("beamNuE",tree1);
 
-  tree->SetBranchAddress("_numEvts", &_numEvts, &b_numEvts);
-  tree->SetBranchAddress("_neutrinos", &_neutrinos, &b_neutrinos);
-  tree->SetBranchAddress("_e_nuReco", &_e_nuReco, &b_e_nuReco);
-  tree->SetBranchAddress("_e_dep", &_e_dep, &b_e_dep);
-  tree->SetBranchAddress("_weight", &_weight, &b_weight);
-
-
+  tree1->SetBranchAddress("_numEvts", &_numEvts, &b_numEvts);
+  tree1->SetBranchAddress("_neutrinos", &_neutrinos, &b_neutrinos);
+  tree1->SetBranchAddress("_e_nuReco", &_e_nuReco, &b_e_nuReco);
+  tree1->SetBranchAddress("_e_dep", &_e_dep, &b_e_dep);
+  tree1->SetBranchAddress("_weight", &_weight, &b_weight);
 
 
-
-  Int_t nentries = tree->GetEntries();
+  Int_t nentries = tree1->GetEntries();
   Int_t nbytes = 0;
 
   for(Int_t i = 0; i < nentries; i++){
     
-    nbytes += tree->GetEntry(i);
+    nbytes += tree1->GetEntry(i);
     
 
-    nue->Fill(_e_nuReco, _weight*(6.6e20/(2.701e17*1991)));
+    nue->Fill(_e_nuReco/1000, _weight*(6.6e20/(2.701e17*1991)));
        
   }
 
-  nue->SetFillColor(kRed);
+  std::cout << "Nu E : " << nue->Integral() << std::endl;
+
+  nue->SetFillColor(kGreen-2);
   
-  TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("singleE_mu_selection.root");
-  if (!f || !f->IsOpen()) {
-    f = new TFile("singleE_mu_selection.root");
+  TFile *f2 = (TFile*)gROOT->GetListOfFiles()->FindObject("singleE_mu_selection.root");
+  if (!f2 || !f2->IsOpen()) {
+    f2 = new TFile("singleE_mu_selection.root");
   }
 
-  f->GetObject("beamNuE",tree);
+  f2->GetObject("beamNuE",tree2);
 
-  tree->SetBranchAddress("_numEvts", &_numEvts, &b_numEvts);
-  tree->SetBranchAddress("_neutrinos", &_neutrinos, &b_neutrinos);
-  tree->SetBranchAddress("_e_nuReco", &_e_nuReco, &b_e_nuReco);
-  tree->SetBranchAddress("_e_dep", &_e_dep, &b_e_dep);
-  tree->SetBranchAddress("_weight", &_weight, &b_weight);
+  tree2->SetBranchAddress("_numEvts", &_numEvts, &b_numEvts);
+  tree2->SetBranchAddress("_neutrinos", &_neutrinos, &b_neutrinos);
+  tree2->SetBranchAddress("_e_nuReco", &_e_nuReco, &b_e_nuReco);
+  tree2->SetBranchAddress("_e_dep", &_e_dep, &b_e_dep);
+  tree2->SetBranchAddress("_weight", &_weight, &b_weight);
 
-  Int_t nentries = tree->GetEntries();
+  Int_t nentries = tree2->GetEntries();
   Int_t nbytes = 0;
 
   for(Int_t i = 0; i < nentries; i++){
     
-    nbytes += tree->GetEntry(i);
+    nbytes += tree2->GetEntry(i);
     
 
-    numu->Fill(_e_nuReco, _weight*(6.6e20/(2.701e17*1991)));
+    numu->Fill(_e_nuReco/1000, _weight*(6.6e20/(2.701e17*1991)));
        
   }
 
-  numu->SetFillColor(kBlue);
+  numu->SetFillColor(kBlue-5);
 
-  TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("singleE_nc_selection.root");
-  if (!f || !f->IsOpen()) {
-    f = new TFile("singleE_nc_selection.root");
+  TFile *f3 = (TFile*)gROOT->GetListOfFiles()->FindObject("singleE_nc_selection.root");
+  if (!f3 || !f3->IsOpen()) {
+    f3 = new TFile("singleE_nc_selection.root");
   }
 
-  f->GetObject("beamNuE",tree);
+  f3->GetObject("beamNuE",tree3);
 
-  tree->SetBranchAddress("_numEvts", &_numEvts, &b_numEvts);
-  tree->SetBranchAddress("_neutrinos", &_neutrinos, &b_neutrinos);
-  tree->SetBranchAddress("_e_nuReco", &_e_nuReco, &b_e_nuReco);
-  tree->SetBranchAddress("_e_dep", &_e_dep, &b_e_dep);
-  tree->SetBranchAddress("_weight", &_weight, &b_weight);
+  tree3->SetBranchAddress("_numEvts", &_numEvts, &b_numEvts);
+  tree3->SetBranchAddress("_neutrinos", &_neutrinos, &b_neutrinos);
+  tree3->SetBranchAddress("_e_nuReco", &_e_nuReco, &b_e_nuReco);
+  tree3->SetBranchAddress("_e_dep", &_e_dep, &b_e_dep);
+  tree3->SetBranchAddress("_weight", &_weight, &b_weight);
 
-  Int_t nentries = tree->GetEntries();
+  Int_t nentries = tree3->GetEntries();
   Int_t nbytes = 0;
 
   for(Int_t i = 0; i < nentries; i++){
     
-    nbytes += tree->GetEntry(i);
+    nbytes += tree3->GetEntry(i);
     
 
-    nc->Fill(_e_nuReco, _weight*(6.6e20/(2.701e17*1991)));
+    nc->Fill(_e_nuReco/1000, _weight*(6.6e20/(2.701e17*1991)));
        
   }
 
-  nc->SetFillColor(kGreen);
+  nc->SetFillColor(kBlue-9);
 
-  TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("singleE_cosmic_selection.root");
-  if (!f || !f->IsOpen()) {
-    f = new TFile("singleE_cosmic_selection.root");
+  TFile *f4 = (TFile*)gROOT->GetListOfFiles()->FindObject("singleE_cosmic_selection.root");
+  if (!f4 || !f4->IsOpen()) {
+    f4 = new TFile("singleE_cosmic_selection.root");
   }
 
-  f->GetObject("cosmicShowers",tree);
+  f4->GetObject("cosmicShowers",tree4);
 
-  tree->SetBranchAddress("_numEvts", &_numEvts, &b_numEvts);
-  tree->SetBranchAddress("_neutrinos", &_neutrinos, &b_neutrinos);
-  tree->SetBranchAddress("_e_nuReco", &_e_nuReco, &b_e_nuReco);
-  tree->SetBranchAddress("_e_dep", &_e_dep, &b_e_dep);
-  tree->SetBranchAddress("_weight", &_weight, &b_weight);
+  tree4->SetBranchAddress("_numEvts", &_numEvts, &b_numEvts);
+  tree4->SetBranchAddress("_neutrinos", &_neutrinos, &b_neutrinos);
+  tree4->SetBranchAddress("_e_nuReco", &_e_nuReco, &b_e_nuReco);
+  tree4->SetBranchAddress("_e_dep", &_e_dep, &b_e_dep);
+  tree4->SetBranchAddress("_weight", &_weight, &b_weight);
 
-  Int_t nentries = tree->GetEntries();
+  Int_t nentries = tree4->GetEntries();
   Int_t nbytes = 0;
 
   for(Int_t i = 0; i < nentries; i++){
     
-    nbytes += tree->GetEntry(i);
+    nbytes += tree4->GetEntry(i);
     
 
-    cosmic->Fill(_e_nuReco, 2.5);//21);
+    cosmic->Fill(_e_nuReco/1000, 2.5*1.5);//21);
        
   }
 
   //  cosmic->Draw("same");
-  cosmic->SetFillColor(kBlack);
-
-  TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("singleE_dirt_selection.root");
-  if (!f || !f->IsOpen()) {
-    f = new TFile("singleE_dirt_selection.root");
+  cosmic->SetFillColor(kRed-3);
+  /*
+  TFile *f5 = (TFile*)gROOT->GetListOfFiles()->FindObject("singleE_dirt_selection.root");
+  if (!f5 || !f5->IsOpen()) {
+    f5 = new TFile("singleE_dirt_selection.root");
   }
 
-  f->GetObject("beamNuE",tree);
+  f5->GetObject("beamNuE",tree5);
 
-  tree->SetBranchAddress("_numEvts", &_numEvts, &b_numEvts);
-  tree->SetBranchAddress("_neutrinos", &_neutrinos, &b_neutrinos);
-  tree->SetBranchAddress("_e_nuReco", &_e_nuReco, &b_e_nuReco);
-  tree->SetBranchAddress("_e_dep", &_e_dep, &b_e_dep);
-  tree->SetBranchAddress("_weight", &_weight, &b_weight);
+  tree5->SetBranchAddress("_numEvts", &_numEvts, &b_numEvts);
+  tree5->SetBranchAddress("_neutrinos", &_neutrinos, &b_neutrinos);
+  tree5->SetBranchAddress("_e_nuReco", &_e_nuReco, &b_e_nuReco);
+  tree5->SetBranchAddress("_e_dep", &_e_dep, &b_e_dep);
+  tree5->SetBranchAddress("_weight", &_weight, &b_weight);
 
-  Int_t nentries = tree->GetEntries();
+  Int_t nentries = tree5->GetEntries();
   Int_t nbytes = 0;
 
   for(Int_t i = 0; i < nentries; i++){
     
-    nbytes += tree->GetEntry(i);
+    nbytes += tree5->GetEntry(i);
     
 
-    dirt->Fill(_e_nuReco, _weight);
+    dirt->Fill(_e_nuReco/1000, _weight);
        
   }
-
+  */
   //  dirt->Draw("same");
 
   LEE->Add(nue);
@@ -179,9 +209,12 @@ void LowEPlot_nue(){
   LEE->Add(nc);
   LEE->Add(cosmic);
 
-  LEE->Draw();
+  LEE->Draw("same");
   
-  TLegend *leg = new TLegend(0.5,0.65,0.8,0.9,NULL,"brNDC");
+  c->RedrawAxis();
+  
+  
+  TLegend *leg = new TLegend(0.3,0.65,0.6,0.9,NULL,"brNDC");
   leg->SetBorderSize(0);
   leg->SetTextSize(0.035);
   leg->SetTextFont(62);
@@ -191,10 +224,10 @@ void LowEPlot_nue(){
   leg->SetLineWidth(1);
   leg->SetFillColor(0);
   leg->SetFillStyle(0);
-  leg->AddEntry(nue,"Beam Intrinsic #nu_{e}","f");
-  leg->AddEntry(numu,"Beam Intrinsic #nu_{#mu}","f");
-  leg->AddEntry(nc,"Beam Intrinsic Neutral Current","f");
-  leg->AddEntry(cosmic,"Cosmic, in- and out-time","f");
+  leg->AddEntry(cosmic,TString::Format("Cosmic, in-time : %4.1f Events", cosmic->Integral() ),"f");
+  leg->AddEntry(numu,TString::Format("Beam Intrinsic #nu_{#mu} : %4.1f Events", numu->Integral() ),"f");
+  leg->AddEntry(nc,TString::Format("Beam Intrinsic Neutral Current : %4.1f Events", nc->Integral() ),"f");
+  leg->AddEntry(nue,TString::Format("Beam Intrinsic #nu_{e} : %4.1f Events", nue->Integral() ),"f");
   leg->Draw();
 
 
