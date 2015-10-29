2f753a0b1a0ce2df3cf3f5e0b0180421a7cc2da8
Corey Adams <corey.adams@yale.edu>
2015-10-29 00:13:48 -0500

    Fix various issues with the live display.

--- UserDev/DisplayTool/python/evdmanager/__init__.py
+++ UserDev/DisplayTool/python/evdmanager/__init__.py
@@ -2,3 +2,4 @@ from geometry import argoneut, microboone, lariat
 from event import event, manager
 from larlite_manager import larlite_manager
 from larsoft_manager import larsoft_manager
+from ubdaq_manager import ubdaq_manager
