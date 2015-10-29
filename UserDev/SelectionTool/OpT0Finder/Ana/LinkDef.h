c2d3d4629aad8b7fe596c834045bf92d056cef88
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-10-29 10:02:03 -0500

    Adding ana module for PE --kazu

--- UserDev/SelectionTool/OpT0Finder/Ana/LinkDef.h
+++ UserDev/SelectionTool/OpT0Finder/Ana/LinkDef.h
@@ -10,6 +10,8 @@
 #pragma link off all functions;
 
 #pragma link C++ class larlite::CosmicRateAna+;
+#pragma link C++ class larlite::PEDistAna+;
 //ADD_NEW_CLASS ... do not change this line
 #endif
+
 
