f4bdbc0859541dcf59b40ca0e48d67406134a019
Elena Gramellini <elenag@uboonegpvm03.fnal.gov>
2015-10-29 14:47:09 -0500

    Change in output structure for Cluster Quality check

--- UserDev/RecoTool/CMToolAna/MMQuality.h
+++ UserDev/RecoTool/CMToolAna/MMQuality.h
@@ -17,6 +17,7 @@
 
 #include <TH1D.h>
 #include <TH2D.h>
+#include <TTree.h>
 #include <map>
 #include "Analysis/ana_base.h"
 #include "MCComp/MCMatchAlg.h"
@@ -78,6 +79,29 @@ namespace larlite {
 
   protected:
 
+    int _mcshowerTot ;
+    int _recoClusterTot ;
+
+    std::vector<int> fMCShower;
+    std::vector<int> fRecoHit;
+	        
+    std::vector<double> fRecoEff_0;
+    std::vector<double> fRecoEff_1;
+    std::vector<double> fRecoEff_2;
+	              
+    std::vector<double> fRecoPur_0;
+    std::vector<double> fRecoPur_1;
+    std::vector<double> fRecoPur_2;
+	              
+    std::vector<double> fBestEff_0;
+    std::vector<double> fBestEff_1;
+    std::vector<double> fBestEff_2;
+	              
+    std::vector<double> fBestPur_0;
+    std::vector<double> fBestPur_1;
+    std::vector<double> fBestPur_2;
+
+
     /// minimum energy cut to find a relevant MC Shower
     double _mc_energy_min;
 
@@ -94,14 +118,40 @@ namespace larlite {
     std::string fClusterProducer;
 
     /// Cluster efficiency (entry per MCShower)
-    std::vector<TH1D*> vShowerClusterEff;
+    //  std::vector<TH1D*> vShowerClusterEff;
 
     /// Cluster purity (entry per MCShower)
-    std::vector<TH1D*> vShowerClusterPur;
+    //std::vector<TH1D*> vShowerClusterPur;
+
+    /// Cluster efficieny vs  purity (entry per MCShower)
+    //std::vector<TH2D*> vShowerClusterEffVsPur;
+
+    /// Cluster efficiency (entry per RecoCluster)
+
+    //std::vector<TH1D*> vRecoShowerClusterEff;
+
+    /// Cluster purity (entry per RecoCluster)
+
+    //std::vector<TH1D*> vRecoShowerClusterPur;
+
+    /// Cluster efficieny vs  purity (entry per RecoCluster)
+
+    //std::vector<TH2D*> vRecoShowerClusterEffVsPur;
 
     /// Matching correctness
     TH1D *hMatchCorrectness;
 
+    /// Number of hits
+    //TH1D *hClusterHits;
+
+    // My analysis TTree
+    TTree *fMMQualityTree;
+
+    /// Function to prepare TTree
+    void InitializeAnaTree();
+
+    /// Function to clear TTree
+    void ClearTreeVar();
   };
 }
 #endif
