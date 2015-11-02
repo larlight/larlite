ec9e857bdd3cd64a0cf96051cd0942a69782969f
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-11-02 08:33:36 -0600

    added G4 photons --kazu

--- UserDev/SelectionTool/OpT0Finder/Ana/PEDistAna.h
+++ UserDev/SelectionTool/OpT0Finder/Ana/PEDistAna.h
@@ -50,21 +50,28 @@ namespace larlite {
     void SetTimeRange(double min, double max)
     { _min_time = min; _max_time = max; }
 
-    void SetProducers(std::string ophit   = "",
+    void SetProducers(std::string g4      = "",
+		      std::string ophit   = "",
 		      std::string opflash = "",
 		      std::string opdigit = "",
 		      std::string trigger = ""
 		      )
-    { _opdigit_producer = opdigit; _ophit_producer = ophit; _opflash_producer = opflash; _trigger_producer = trigger; }
+    {
+      _g4_producer = g4;
+      _opdigit_producer = opdigit;
+      _ophit_producer = ophit;
+      _opflash_producer = opflash;
+      _trigger_producer = trigger;
+    }
 
   protected:
 
     TTree *_event_tree, *_run_tree;
     
     unsigned int _run, _subrun, _event;
-    std::vector<double> _wf_event_pe, _hit_event_pe, _flash_event_pe;
-    std::vector< std::vector<double> > _wf_tot_pe, _hit_tot_pe, _flash_tot_pe;
-    std::string _opdigit_producer, _ophit_producer, _opflash_producer, _trigger_producer;
+    std::vector<double> _g4_event_pe,_wf_event_pe, _hit_event_pe, _flash_event_pe;
+    std::vector< std::vector<double> > _g4_tot_pe, _wf_tot_pe, _hit_tot_pe, _flash_tot_pe;
+    std::string _opdigit_producer, _g4_producer, _ophit_producer, _opflash_producer, _trigger_producer;
     double _min_time, _max_time;
     double _spe_area;
   };
