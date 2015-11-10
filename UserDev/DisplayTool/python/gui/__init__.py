78134e22c2c5254f1dac61a2460ec11f7d31867e
Corey Adams <corey.adams@yale.edu>
2015-11-10 16:15:52 -0600

    Adding 3D view windows and basic gui.

--- UserDev/DisplayTool/python/gui/__init__.py
+++ UserDev/DisplayTool/python/gui/__init__.py
@@ -3,3 +3,10 @@ from larlitegui import larlitegui
 from livegui import livegui
 from viewport import viewport
 from larsoftgui import larsoftgui
+try:
+    import pyqtgraph.opengl as gl
+    from gui3D import gui3D
+    from larlitegui3D import larlitegui3D
+except:
+    pass
+    
