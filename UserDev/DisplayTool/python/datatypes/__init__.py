ce5c2bcff443d01e9acadcd9e9487204c5fd9344
Corey Adams <corey.adams@yale.edu>
2015-11-10 16:35:35 -0600

    Add 3D import catch and objects.

--- UserDev/DisplayTool/python/datatypes/__init__.py
+++ UserDev/DisplayTool/python/datatypes/__init__.py
@@ -6,3 +6,9 @@ from track import track
 from wire import wire, rawDigit
 from match import match
 from drawableItems import drawableItems
+try:
+    import pyqtgraph.opengl as gl
+    from drawableItems import drawableItems3D
+    from track import track3D
+except:
+    pass
