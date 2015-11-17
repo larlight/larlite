aa696609524ec6933444ed37e52502f409b11e6e
Yun-Tse Tsai <yuntsebaryon@gmail.com>
2015-11-17 14:28:31 -0800

    modify the structure

--- UserDev/SelectionTool/OpT0Finder/Algorithms/PhotonLibHypothesis.cxx
+++ UserDev/SelectionTool/OpT0Finder/Algorithms/PhotonLibHypothesis.cxx
@@ -2,64 +2,35 @@
 #define PHOTONLIBHYPOTHESIS_CXX
 
 #include "PhotonLibHypothesis.h"
-#include "OpT0Finder/Base/OpT0FinderException.h"
 #include "OpT0Finder/PhotonLibrary/PhotonVisibilityService.h"
 
 namespace flashana {
 
-  PhotonLibHypothesis* PhotonLibHypothesis::_me = nullptr;
-
-  PhotonLibHypothesis::PhotonLibHypothesis()
-    : _pmt_x_v()
-    , _pmt_y_v()
-    , _pmt_z_v()
-  {}
-
-  void PhotonLibHypothesis::SetOpDetPositions( const std::vector<double>& pos_x,
-                                               const std::vector<double>& pos_y,
-                                               const std::vector<double>& pos_z )
-  {
-    if(pos_x.size() != pos_y.size() || pos_x.size() != pos_z.size() )
-      throw OpT0FinderException("Unmatching optical detector position array length!");
-    _pmt_x_v = pos_x;
-    _pmt_y_v = pos_y;
-    _pmt_z_v = pos_z;
-
-  }
-
-  const std::vector<double>& PhotonLibHypothesis::ChargeHypothesis(const QCluster_t& trk,
-                                                                   const double xshift)
+  void PhotonLibHypothesis::FillEstimate(const QCluster_t& trk, Flash_t &flash_t)
   {
 
-    double qmax = 0;
-    if(_pmt_x_v.empty()) Print(msg::kEXCEPTION,__FUNCTION__,"PMT Geometry not set!");
-    if(_qll_hypothesis_v.empty()) _qll_hypothesis_v.resize(_pmt_x_v.size(),0);
-    if(_qll_hypothesis_v.size() != _pmt_x_v.size())
-      Print(msg::kEXCEPTION,__FUNCTION__,"Hypothesis vector length != PMT count");
+    size_t n_pmt = BaseAlgorithm::NOpDets();
 
-    for(auto& v : _qll_hypothesis_v) v=0;
+    for ( auto& v : flash_t ) v = 0;
 
-    for(size_t pmt_index=0; pmt_index < _pmt_x_v.size(); ++pmt_index) {
+    for ( size_t ipmt = 0; ipmt < n_pmt; ++ipmt) {
 
-      for(size_t pt_index=0; pt_index < trk.size(); ++pt_index) {
+      for ( size_t ipt = 0; ipt < trk.size(); ++ipt) {
 
-        auto const& pt = trk[pt_index];
+        auto const& pt = trk[ipt];
 
         double q = pt.q;
-        q *= ::phot::PhotonVisibilityService::GetME().GetVisibility((pt.x+xshift),pt.y,pt.z,pmt_index);
-        _qll_hypothesis_v[pmt_index] += q;
+        q *= ::phot::PhotonVisibilityService::GetME().GetVisibility( pt.x, pt.y, pt.z, ipmt);
+        flash_t[ipmt] += q;
 
       }
-
-      if(_qll_hypothesis_v[pmt_index] > qmax) qmax = _qll_hypothesis_v[pmt_index];
     }
 
-    //for(auto& v : _qll_hypothesis_v) v /= qmax;
-    double qsum =0;
-    for(auto const& v : _qll_hypothesis_v) qsum += v;
-    for(auto& v : _qll_hypothesis_v) v/= qsum;
+    double qsum = 0;
+    for ( auto const& v : flash_t ) qsum += v;
+    for ( auto& v : flash_t ) v/= qsum;
 
-    return _qll_hypothesis_v;
+    return;
   }
 }
 #endif
