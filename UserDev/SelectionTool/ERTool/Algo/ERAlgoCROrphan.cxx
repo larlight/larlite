5b8a57a609fe5a4a5cdf1922bcfb3eba2a784883
David Kaleko <kaleko@nevis.columbia.edu>
2015-11-23 14:08:37 -0600

    actually implementing the new enum value in ERAlgoCROrphan (should have included this in previous commit, whoops)

--- UserDev/SelectionTool/ERTool/Algo/ERAlgoCROrphan.cxx
+++ UserDev/SelectionTool/ERTool/Algo/ERAlgoCROrphan.cxx
@@ -5,108 +5,108 @@
 
 namespace ertool {
 
-  ERAlgoCROrphan::ERAlgoCROrphan(const std::string& name) : AlgoBase(name)
-  {}
+ERAlgoCROrphan::ERAlgoCROrphan(const std::string& name) : AlgoBase(name)
+{}
 
-  void ERAlgoCROrphan::Reset()
-  {}
+void ERAlgoCROrphan::Reset()
+{}
 
-  void ERAlgoCROrphan::AcceptPSet(const ::fcllite::PSet& cfg)
-  {
-    auto p = cfg.get_pset(Name());
-    auto xrange_v = p.get<std::vector<double> >("tpc_aabox_xrange");
-    auto yrange_v = p.get<std::vector<double> >("tpc_aabox_yrange");
-    auto zrange_v = p.get<std::vector<double> >("tpc_aabox_zrange");
-    _tpc_box.Min( xrange_v[0], yrange_v[0], zrange_v[0] );
-    _tpc_box.Max( xrange_v[1], yrange_v[1], zrange_v[1] );
-    
-    _respect_trk_dir      = p.get<bool  >( "respect_track_dir"     );
-    _min_dist_trk_def     = p.get<double>( "min_dist_track_to_be"  );
-    _dist_back_to_ceiling = p.get<double>( "dist_back_to_ceiling"  );
-  }
+void ERAlgoCROrphan::AcceptPSet(const ::fcllite::PSet& cfg)
+{
+  auto p = cfg.get_pset(Name());
+  auto xrange_v = p.get<std::vector<double> >("tpc_aabox_xrange");
+  auto yrange_v = p.get<std::vector<double> >("tpc_aabox_yrange");
+  auto zrange_v = p.get<std::vector<double> >("tpc_aabox_zrange");
+  _tpc_box.Min( xrange_v[0], yrange_v[0], zrange_v[0] );
+  _tpc_box.Max( xrange_v[1], yrange_v[1], zrange_v[1] );
 
-  void ERAlgoCROrphan::ProcessBegin()
-  {}
+  _respect_trk_dir      = p.get<bool  >( "respect_track_dir"     );
+  _min_dist_trk_def     = p.get<double>( "min_dist_track_to_be"  );
+  _dist_back_to_ceiling = p.get<double>( "dist_back_to_ceiling"  );
+}
 
-  bool ERAlgoCROrphan::Reconstruct(const EventData &data, ParticleGraph& graph)
-  {
+void ERAlgoCROrphan::ProcessBegin()
+{}
 
-    ::geoalgo::HalfLine shower;
+bool ERAlgoCROrphan::Reconstruct(const EventData &data, ParticleGraph& graph)
+{
 
-    for(auto const& id : graph.GetParticleNodes()) {
+  ::geoalgo::HalfLine shower;
 
-      auto& part = graph.GetParticle(id);
+  for (auto const& id : graph.GetParticleNodes()) {
 
-      if( part.ProcessType() == kCosmic || part.Descendant() )
-	continue;
+    auto& part = graph.GetParticle(id);
 
-      bool is_cosmic = false;
-      if( part.RecoType() == kTrack ) {
+    if ( part.ProcessType() == kCosmic || part.Descendant() )
+      continue;
 
-	auto const& trk = data.Track(part.RecoID());
-	if(trk.size()<2 || trk.IsLonger(_min_dist_trk_def)) continue;
+    bool is_cosmic = false;
+    if ( part.RecoType() == kTrack ) {
 
-	auto const& pt1 = trk[0];
-	auto const& pt2 = trk[1];
-	auto dir = pt2 - pt1;
-	shower.Start(pt1[0],pt1[1],pt1[2]);
-	shower.Dir(dir);
+      auto const& trk = data.Track(part.RecoID());
+      if (trk.size() < 2 || trk.IsLonger(_min_dist_trk_def)) continue;
 
-	is_cosmic = IsCROrphan(shower);
+      auto const& pt1 = trk[0];
+      auto const& pt2 = trk[1];
+      auto dir = pt2 - pt1;
+      shower.Start(pt1[0], pt1[1], pt1[2]);
+      shower.Dir(dir);
 
-	if(!is_cosmic && !_respect_trk_dir) {
+      is_cosmic = IsCROrphan(shower);
 
-	  auto const& rev_pt1 = trk[trk.size()-1];
-	  auto const& rev_pt2 = trk[trk.size()-2];
-	  auto rev_dir = rev_pt2 - rev_pt1;
-	  shower.Start(rev_pt1[0],rev_pt1[1],rev_pt1[2]);
-	  shower.Dir(rev_dir[0],rev_dir[1],rev_dir[2]);
+      if (!is_cosmic && !_respect_trk_dir) {
 
-	  is_cosmic = IsCROrphan(shower);
-	}
+        auto const& rev_pt1 = trk[trk.size() - 1];
+        auto const& rev_pt2 = trk[trk.size() - 2];
+        auto rev_dir = rev_pt2 - rev_pt1;
+        shower.Start(rev_pt1[0], rev_pt1[1], rev_pt1[2]);
+        shower.Dir(rev_dir[0], rev_dir[1], rev_dir[2]);
 
-      }else if( part.RecoType() == kShower)
-
-	is_cosmic = IsCROrphan(data.Shower(part.RecoID()));
-
-      if(is_cosmic) {
-
-	graph.SetPrimary(id);
-	
-	part.SetParticleInfo( part.PdgCode(),
-			      part.Mass(),
-			      part.Vertex(),
-			      part.Momentum(),
-			      part.RecoScore(),
-			      kCosmic);
+        is_cosmic = IsCROrphan(shower);
       }
+
+    } else if ( part.RecoType() == kShower)
+
+      is_cosmic = IsCROrphan(data.Shower(part.RecoID()));
+
+    if (is_cosmic) {
+
+      graph.SetPrimary(id);
+
+      part.SetParticleInfo( part.PdgCode(),
+                            part.Mass(),
+                            part.Vertex(),
+                            part.Momentum(),
+                            part.RecoScore(),
+                            kCosmicOrphan);
     }
-    return true;
   }
+  return true;
+}
 
-  bool ERAlgoCROrphan::IsCROrphan(const ::geoalgo::HalfLine& shr) const
-  {
+bool ERAlgoCROrphan::IsCROrphan(const ::geoalgo::HalfLine& shr) const
+{
 
-    auto const& start = shr.Start();
-    auto const& dir   = shr.Dir();
+  auto const& start = shr.Start();
+  auto const& dir   = shr.Dir();
 
-    if(dir[1] > 0) return false;
+  if (dir[1] > 0) return false;
 
-    if(!_tpc_box.Contain(start)) return true;
+  if (!_tpc_box.Contain(start)) return true;
 
-    double dist = _tpc_box.Max()[1] - start[1];
+  double dist = _tpc_box.Max()[1] - start[1];
 
-    dist = dist / dir[1] * -1.;
+  dist = dist / dir[1] * -1.;
 
-    //std::cout<<"Dist is  : "<<dist<<std::endl ;
+  //std::cout<<"Dist is  : "<<dist<<std::endl ;
 
-    if(dist > _dist_back_to_ceiling) return false;
+  if (dist > _dist_back_to_ceiling) return false;
 
-    return true;
-  }
+  return true;
+}
 
-  void ERAlgoCROrphan::ProcessEnd(TFile* fout)
-  {}
+void ERAlgoCROrphan::ProcessEnd(TFile* fout)
+{}
 
 }
 
