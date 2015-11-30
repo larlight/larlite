99956c0bd5f5cb7a4df0d86166da16b27b121c6d
David Kaleko <kaleko@nevis.columbia.edu>
2015-11-30 13:06:15 -0600

    Fixed small bug in AlgoFindRelationship, added some wrapper functions to AlgoFindRelationship so they can be used from within const functions (IE they don't fill a vtx variable), added the ability for EROrphan to tag a particle as neutron-induced, and added the kNeutronInduced process flag to ertooltypes

--- UserDev/SelectionTool/ERTool/Algo/AlgoFindRelationship.cxx
+++ UserDev/SelectionTool/ERTool/Algo/AlgoFindRelationship.cxx
@@ -3,43 +3,43 @@
 
 #include "AlgoFindRelationship.h"
 
-namespace ertool{
+namespace ertool {
 
   AlgoFindRelationship::AlgoFindRelationship()
   {
     _debug  = false;
     _maxIP   = 1.;
   }
-  
+
   void AlgoFindRelationship::Reset()
   {}
 
 
   double AlgoFindRelationship::FindClosestApproach(const geoalgo::HalfLine_t& shr1,
-						   const geoalgo::HalfLine_t& shr2,
-						   geoalgo::Point_t& vtx) const
+      const geoalgo::HalfLine_t& shr2,
+      geoalgo::Point_t& vtx) const
   {
     // Find mininum impact parameter between a two showers
     // flip their directions and look backwards
-    
+
     // Create a half-line pointing backwards from the shower
-    geoalgo::HalfLine_t shr1Bkwd(shr1.Start(), shr1.Dir()*(-1));
-    geoalgo::HalfLine_t shr2Bkwd(shr2.Start(), shr2.Dir()*(-1));
-    
+    geoalgo::HalfLine_t shr1Bkwd(shr1.Start(), shr1.Dir() * (-1));
+    geoalgo::HalfLine_t shr2Bkwd(shr2.Start(), shr2.Dir() * (-1));
+
     // coordinates for closest points on the two objects
     geoalgo::Point_t PtShr1(3);
     geoalgo::Point_t PtShr2(3);
     double IP = _geoAlgo.SqDist(shr1Bkwd, shr2Bkwd, PtShr1, PtShr2);
     // build candidate vertex
-    vtx = (PtShr1+PtShr2)/2.;
-    
+    vtx = (PtShr1 + PtShr2) / 2.;
+
     return sqrt(IP);
   }
 
 
   double AlgoFindRelationship::FindClosestApproach(const geoalgo::HalfLine_t& shr,
-						   const geoalgo::Trajectory_t& trk,
-						   geoalgo::Point_t& vtx) const
+      const geoalgo::Trajectory_t& trk,
+      geoalgo::Point_t& vtx) const
   {
     // Find mininum impact parameter between a shower and a track
     // Search for a point of origin, either common to the two objects
@@ -49,10 +49,10 @@ namespace ertool{
     // *** ADD ALONG SHOWER DIR FOR ITS LENGTH - NOT IMPLEMENTED NOW ***
     // and along the entire segment track as well as backwards along the
     // track direction
-    
+
     // Create a half-line pointing backwards from the shower
-    geoalgo::HalfLine_t shrBkwd(shr.Start(), shr.Dir()*(-1));
-    
+    geoalgo::HalfLine_t shrBkwd(shr.Start(), shr.Dir() * (-1));
+
     // 1) Search for whether the shower might originate
     // from the track
     // get IP for this shower and the entire track
@@ -62,27 +62,27 @@ namespace ertool{
     geoalgo::Point_t PtTrk(3);
     double IP = _geoAlgo.SqDist(shrBkwd, trk, PtShr, PtTrk);
     // build candidate vertex
-    vtx = (PtShr+PtTrk)/2.;
-    
+    vtx = (PtShr + PtTrk) / 2.;
+
     return sqrt(IP);
   }
 
 
   double AlgoFindRelationship::FindClosestApproach(const geoalgo::Trajectory_t& trk,
-						   const geoalgo::HalfLine_t& shr,
-						   geoalgo::Point_t& vtx) const
+      const geoalgo::HalfLine_t& shr,
+      geoalgo::Point_t& vtx) const
   {
-    
-    double IP = FindClosestApproach(shr,trk,vtx);
-    
+
+    double IP = FindClosestApproach(shr, trk, vtx);
+
     return IP;
   }
 
 
 
   double AlgoFindRelationship::FindClosestApproach(const geoalgo::Trajectory_t& trk2,
-						   const geoalgo::Trajectory_t& trk1,
-						   geoalgo::Point_t& vtx) const
+      const geoalgo::Trajectory_t& trk1,
+      geoalgo::Point_t& vtx) const
   {
     // Find mininum impact parameter between two tracks
     // Search for a point of origin, either common to the two objects
@@ -94,61 +94,93 @@ namespace ertool{
     // holders for closest approach points
     geoalgo::Point_t pt1(3);
     geoalgo::Point_t pt2(3);
-    
+
     // keep track of smallest quantity
-    double IP = 1036.;
+    //sqrt is returned at the end, and everything compared to this is squared, so square it
+    double IP = 1036.*1036.;
     // and temporary holders
     double IPtmp = 0;
     geoalgo::Point_t vtxtmp(3);
     geoalgo::Point_t pttmp(3);
 
-    //std::cout << "\t\t\t\tTrk A Start: " << trk1.front() << std::endl; 
-    //std::cout << "\t\t\t\tTrk B Start: " << trk2.front() << std::endl; 
+    //std::cout << "\t\t\t\tTrk A Start: " << trk1.front() << std::endl;
+    //std::cout << "\t\t\t\tTrk B Start: " << trk2.front() << std::endl;
 
     // Search for IP looping through segments of one track
     // and using start-point of other track
     pttmp = _geoAlgo.ClosestPt(trk2.front(), trk1);
-    vtxtmp = (trk2.front()+pttmp)/2.;
+    vtxtmp = (trk2.front() + pttmp) / 2.;
     IPtmp = pttmp.SqDist(trk2.front());
-    //std::cout << "\t\t\t\tTrk A body and Trk B start. IP: " << IPtmp << " Vtx: " << vtxtmp << std::endl; 
+    //std::cout << "\t\t\t\tTrk A body and Trk B start. IP: " << IPtmp << " Vtx: " << vtxtmp << std::endl;
     if (IPtmp < IP) { IP = IPtmp; vtx = vtxtmp; }
     // same but flip roles of tracks
     pttmp = _geoAlgo.ClosestPt(trk1.front(), trk2);
-    vtxtmp = (trk1.front()+pttmp)/2.;
+    vtxtmp = (trk1.front() + pttmp) / 2.;
     IPtmp = pttmp.SqDist(trk1.front());
-    //std::cout << "\t\t\t\tTrk B body and Trk A start. IP: " << IPtmp << " Vtx: " << vtxtmp <<  std::endl; 
+    //std::cout << "\t\t\t\tTrk B body and Trk A start. IP: " << IPtmp << " Vtx: " << vtxtmp <<  std::endl;
     if (IPtmp < IP) { IP = IPtmp; vtx = vtxtmp; }
     // Search for IP looping through segments of one track
     // and using end point of other track
     pttmp = _geoAlgo.ClosestPt(trk2.back(), trk1);
-    vtxtmp = (trk2.back()+pttmp)/2.;
+    vtxtmp = (trk2.back() + pttmp) / 2.;
     IPtmp = pttmp.SqDist(trk2.back());
-    //std::cout << "\t\t\t\tTrk A body and Trk B end. IP: " << IPtmp << " Vtx: " << vtxtmp <<  std::endl; 
+    //std::cout << "\t\t\t\tTrk A body and Trk B end. IP: " << IPtmp << " Vtx: " << vtxtmp <<  std::endl;
     if (IPtmp < IP) { IP = IPtmp; vtx = vtxtmp; }
     // same but flip roles of tracks
     pttmp = _geoAlgo.ClosestPt(trk1.back(), trk2);
-    vtxtmp = (trk1.back()+pttmp)/2.;
+    vtxtmp = (trk1.back() + pttmp) / 2.;
     IPtmp = pttmp.SqDist(trk1.back());
-    //std::cout << "\t\t\t\tTrk B body and Trk A end. IP: " << IPtmp << " Vtx: " << vtxtmp <<  std::endl; 
+    //std::cout << "\t\t\t\tTrk B body and Trk A end. IP: " << IPtmp << " Vtx: " << vtxtmp <<  std::endl;
     if (IPtmp < IP) { IP = IPtmp; vtx = vtxtmp; }
 
     return sqrt(IP);
   }
 
+  double AlgoFindRelationship::FindClosestApproach(const geoalgo::HalfLine_t& shr1,
+      const geoalgo::HalfLine_t& shr2) const
+  {
+    geoalgo::Point_t vtx(3);
+    return FindClosestApproach(shr1, shr2, vtx);
+  }
+
+
+  double AlgoFindRelationship::FindClosestApproach(const geoalgo::HalfLine_t& shr,
+      const geoalgo::Trajectory_t& trk) const
+  {
+    geoalgo::Point_t vtx(3);
+    return FindClosestApproach(shr, trk, vtx);
+  }
+
+
+  double AlgoFindRelationship::FindClosestApproach(const geoalgo::Trajectory_t& trk,
+      const geoalgo::HalfLine_t& shr) const
+  {
+    geoalgo::Point_t vtx(3);
+    return FindClosestApproach(shr, trk, vtx);
+  }
+
+
+
+  double AlgoFindRelationship::FindClosestApproach(const geoalgo::Trajectory_t& trk2,
+      const geoalgo::Trajectory_t& trk1) const
+  {
+    geoalgo::Point_t vtx(3);
+    return FindClosestApproach(trk1, trk2, vtx);
+  }
 
   RelationType_t AlgoFindRelationship::FindRelation(const geoalgo::HalfLine_t& s1,
-						    const geoalgo::HalfLine_t& s2,
-						    geoalgo::Point_t& vtx,
-						    double& score) const
+      const geoalgo::HalfLine_t& s2,
+      geoalgo::Point_t& vtx,
+      double& score) const
   {
-    
-    double IP = FindClosestApproach(s1,s2,vtx);
+
+    double IP = FindClosestApproach(s1, s2, vtx);
     if (IP < 1e-10)
-      score = 1./IP;
+      score = 1. / IP;
     else
       score = 1e+10;
 
-    if (IP < _maxIP){
+    if (IP < _maxIP) {
       if (_debug) { std::cout << "\tshowers are siblings" << std::endl; }
       return RelationType_t::kSibling;
     }
@@ -158,49 +190,49 @@ namespace ertool{
 
 
   RelationType_t AlgoFindRelationship::FindRelation(const geoalgo::Trajectory_t& t,
-						    const geoalgo::HalfLine_t& s,
-						    geoalgo::Point_t& vtx,
-						    double& score) const
+      const geoalgo::HalfLine_t& s,
+      geoalgo::Point_t& vtx,
+      double& score) const
   {
-    
-    double IP = FindClosestApproach(t,s,vtx);
+
+    double IP = FindClosestApproach(t, s, vtx);
     if (IP < 1e-10)
-      score = 1./IP;
+      score = 1. / IP;
     else
       score = 1e+10;
 
-    if (IP < _maxIP){
+    if (IP < _maxIP) {
 
       double vtx_to_t_start = vtx.Dist(t.front());
-      double vtx_to_t_body  = sqrt(_geoAlgo.SqDist(vtx,t));
-      
-      if (_debug) { 
-	std::cout << "\tShr Start: " << s.Start() << std::endl
-		  << "\tTrk Start: " << t.front() << "\tEnd: " << t.back() << std::endl
-		  << "\tVtx to Trk Start: " << vtx_to_t_start << "\t Vtx to Trk Body: " << vtx_to_t_body << std::endl;
+      double vtx_to_t_body  = sqrt(_geoAlgo.SqDist(vtx, t));
+
+      if (_debug) {
+        std::cout << "\tShr Start: " << s.Start() << std::endl
+                  << "\tTrk Start: " << t.front() << "\tEnd: " << t.back() << std::endl
+                  << "\tVtx to Trk Start: " << vtx_to_t_start << "\t Vtx to Trk Body: " << vtx_to_t_body << std::endl;
       }
-      
+
       // if vertex closer to track start point => siblings
-      if ( (vtx_to_t_start <= vtx_to_t_body) || (vtx_to_t_start < 0.1) ){
-	if (_debug) { std::cout << "\ttrk-shr are Siblings! HARDCODED NUMBER" << std::endl; }
-	return RelationType_t::kSibling;
+      if ( (vtx_to_t_start <= vtx_to_t_body) || (vtx_to_t_start < 0.1) ) {
+        if (_debug) { std::cout << "\ttrk-shr are Siblings! HARDCODED NUMBER" << std::endl; }
+        return RelationType_t::kSibling;
       }
       // otherwise assume track is parent
       if (_debug) { std::cout << "\tTrack is Shower's parent! HARDCODED NUMBER" << std::endl; }
       return RelationType_t::kParent;
 
     }
-    
+
     return RelationType_t::kUnrelated;
   }
-  
+
   RelationType_t AlgoFindRelationship::FindRelation(const geoalgo::HalfLine_t& s,
-						    const geoalgo::Trajectory_t& t,
-						    geoalgo::Point_t& vtx,
-						    double& score) const
+      const geoalgo::Trajectory_t& t,
+      geoalgo::Point_t& vtx,
+      double& score) const
   {
-  
-    auto const& rel = FindRelation(t,s,vtx,score);
+
+    auto const& rel = FindRelation(t, s, vtx, score);
 
     if (rel == RelationType_t::kSibling)
       return rel;
@@ -215,54 +247,54 @@ namespace ertool{
 
     return RelationType_t::kUnrelated;
   }
-  
-  
+
+
   RelationType_t AlgoFindRelationship::FindRelation(const geoalgo::Trajectory_t& t1,
-						    const geoalgo::Trajectory_t& t2,
-						    geoalgo::Point_t& vtx,
-						    double& score) const
+      const geoalgo::Trajectory_t& t2,
+      geoalgo::Point_t& vtx,
+      double& score) const
   {
 
-    double IP = FindClosestApproach(t1,t2,vtx);
+    double IP = FindClosestApproach(t1, t2, vtx);
     if (IP < 1e-10)
-      score = 1./IP;
+      score = 1. / IP;
     else
       score = 1e+10;
 
-    if (IP < _maxIP){
+    if (IP < _maxIP) {
 
       // if vertex closer to body than to start point
       // of one shower => that shower is parent
       double vtx_to_t1_start = vtx.Dist(t1.front());
-      double vtx_to_t1_body  = sqrt(_geoAlgo.SqDist(vtx,t1));
+      double vtx_to_t1_body  = sqrt(_geoAlgo.SqDist(vtx, t1));
       double vtx_to_t2_start = vtx.Dist(t2.front());
-      double vtx_to_t2_body  = sqrt(_geoAlgo.SqDist(vtx,t2));
-      
-      if (_debug) { 
-	std::cout << "\tTrk1 Start: " << t1.front() << "\tEnd: " << t1.back() << std::endl
-		  << "\tTrk2 Start: " << t2.front() << "\tEnd: " << t2.back() << std::endl
-		  << "\tVtx to Trk1 Start: " << vtx_to_t1_start << "\t Vtx to Trk1 Body: " << vtx_to_t1_body << std::endl
-		  << "\tVtx to Trk2 Start: " << vtx_to_t2_start << "\t Vtx to Trk2 Body: " << vtx_to_t2_body << std::endl;
+      double vtx_to_t2_body  = sqrt(_geoAlgo.SqDist(vtx, t2));
+
+      if (_debug) {
+        std::cout << "\tTrk1 Start: " << t1.front() << "\tEnd: " << t1.back() << std::endl
+                  << "\tTrk2 Start: " << t2.front() << "\tEnd: " << t2.back() << std::endl
+                  << "\tVtx to Trk1 Start: " << vtx_to_t1_start << "\t Vtx to Trk1 Body: " << vtx_to_t1_body << std::endl
+                  << "\tVtx to Trk2 Start: " << vtx_to_t2_start << "\t Vtx to Trk2 Body: " << vtx_to_t2_body << std::endl;
       }
-      
+
       // if vertex very very close to start points
-      if ( (vtx_to_t1_start <= 0.1) and (vtx_to_t2_start <= 0.1) ){
-	if (_debug) { std::cout << "\ttracks are siblings - HARDCODED NUMBER" << std::endl; }
-	return RelationType_t::kSibling;
+      if ( (vtx_to_t1_start <= 0.1) and (vtx_to_t2_start <= 0.1) ) {
+        if (_debug) { std::cout << "\ttracks are siblings - HARDCODED NUMBER" << std::endl; }
+        return RelationType_t::kSibling;
       }
       // if vertex closer to track start point => siblings
-      if ( (vtx_to_t1_start <= vtx_to_t1_body) and (vtx_to_t2_start <= vtx_to_t2_body) ){
-	if (_debug) { std::cout << "\ttracks are siblings" << std::endl; }
-	return RelationType_t::kSibling;
+      if ( (vtx_to_t1_start <= vtx_to_t1_body) and (vtx_to_t2_start <= vtx_to_t2_body) ) {
+        if (_debug) { std::cout << "\ttracks are siblings" << std::endl; }
+        return RelationType_t::kSibling;
       }
-      else if (vtx_to_t1_start < vtx_to_t2_start){
-	if (_debug) { std::cout << "\ttrk1 is child of trk2" << std::endl; }
-	return RelationType_t::kParent; // t1 is parent to t2
+      else if (vtx_to_t1_start < vtx_to_t2_start) {
+        if (_debug) { std::cout << "\ttrk1 is child of trk2" << std::endl; }
+        return RelationType_t::kParent; // t1 is parent to t2
       }
       if (_debug) { std::cout << "\ttrk2 is child of trk1" << std::endl; }
       return RelationType_t::kChild;
     }// if IP passes cut
-      
+
     return RelationType_t::kUnrelated;
   }
 
@@ -270,7 +302,7 @@ namespace ertool{
   double AlgoFindRelationship::GetPDG(const Track &trk)
   {
 
-    switch(trk._pid){
+    switch (trk._pid) {
     case Track::TrackPartID_t::kUnknown : return -1;
     case Track::TrackPartID_t::kPIDA    : return -1;
     case Track::TrackPartID_t::kProton  : return 2212;
@@ -286,7 +318,7 @@ namespace ertool{
   double AlgoFindRelationship::GetMass(const Track &trk)
   {
 
-    switch(trk._pid){
+    switch (trk._pid) {
     case Track::TrackPartID_t::kUnknown : return 0;
     case Track::TrackPartID_t::kPIDA    : return 0;
     case Track::TrackPartID_t::kProton  : return ParticleMass(2212);
@@ -307,7 +339,7 @@ namespace ertool{
   /// Start points are this way grouped
   /// into vertices
   std::vector<::geoalgo::Point_t> AlgoFindRelationship::GetVertices(const ParticleGraph& graph,
-								    const int minObjectsAtVertex) const
+      const int minObjectsAtVertex) const
   {
     // make list to hold vertices
     std::vector<::geoalgo::Point_t> vertices;
@@ -317,35 +349,35 @@ namespace ertool{
 
     // get primaries only
     auto const& primaries = graph.GetPrimaryNodes();
-    
+
     // for each primary, get the start point
     // check against all already found vertices
     // if any one is within the _IP of the
     // current start point -> add object to that vertex
-    for (auto const& nodeID : primaries){
+    for (auto const& nodeID : primaries) {
       bool newvtx = true;
       auto const start = graph.GetParticle(nodeID).Vertex();
-      for (size_t v = 0; v < vertices.size(); v++){
-	if (start.Dist(vertices[v]) < _maxIP){
-	  newvtx = false;
-	  // add to this vertex
-	  numObjects[v] += 1;
-	}
+      for (size_t v = 0; v < vertices.size(); v++) {
+        if (start.Dist(vertices[v]) < _maxIP) {
+          newvtx = false;
+          // add to this vertex
+          numObjects[v] += 1;
+        }
       }
-      if (newvtx){
-	// this vertex is new!
-	// add it
-	vertices.push_back(start);
-	numObjects.push_back(1);
+      if (newvtx) {
+        // this vertex is new!
+        // add it
+        vertices.push_back(start);
+        numObjects.push_back(1);
       }
     }// for all primaries
 
     // we only want vertices with a minimum number of objects
     // starting from this vertex
     std::vector<::geoalgo::Point_t> vertices_afterCut;
-    for (size_t i=0; i < vertices.size(); i++){
+    for (size_t i = 0; i < vertices.size(); i++) {
       if (numObjects[i] >= minObjectsAtVertex)
-	vertices_afterCut.push_back(vertices[i]);
+        vertices_afterCut.push_back(vertices[i]);
     }
 
     return vertices_afterCut;
--- UserDev/SelectionTool/ERTool/Algo/ERAlgoCROrphan.h
+++ UserDev/SelectionTool/ERTool/Algo/ERAlgoCROrphan.h
@@ -2,7 +2,7 @@
  * \file ERAlgoCROrphan.h
  *
  * \ingroup Algo
- * 
+ *
  * \brief Class def header for a class ERAlgoCROrphan
  *
  * @author kazuhiro
@@ -17,6 +17,8 @@
 
 #include "ERTool/Base/AlgoBase.h"
 #include "GeoAlgo/GeoAABox.h"
+#include "ERTool/Algo/AlgoFindRelationship.h"
+
 namespace ertool {
 
   /**
@@ -24,14 +26,14 @@ namespace ertool {
      User custom Algorithm class made by kazuhiro
    */
   class ERAlgoCROrphan : public AlgoBase {
-  
+
   public:
 
     /// Default constructor
-    ERAlgoCROrphan(const std::string& name="CROrphan");
+    ERAlgoCROrphan(const std::string& name = "CROrphan");
 
     /// Default destructor
-    virtual ~ERAlgoCROrphan(){};
+    virtual ~ERAlgoCROrphan() {};
 
     /// Reset function
     void Reset();
@@ -46,19 +48,32 @@ namespace ertool {
     bool Reconstruct(const EventData &data, ParticleGraph& graph);
 
     /// Called after processing the last event sample
-    void ProcessEnd(TFile* fout=nullptr);
+    void ProcessEnd(TFile* fout = nullptr);
 
   private:
 
+    /// Function that takes in a shower (or, a short track that is converted to into a shower)
+    /// and decides if it looks like a cosmic ray orphan (all alone near top of detector, pointing downwards)
     bool IsCROrphan(const ::geoalgo::HalfLine& shr) const;
 
+    /// Function that takes in a particle and decides if it looks like it's neutron-induced from a cosmic
+    /// (all alone, short track length, etc)
+    /// Returns -1 if the particle is not neutron-induced, otherwise returns nodeID of closest cosmic primary
+    int IsNeutronInduced(const ertool::Particle& p, const EventData &data, const ParticleGraph& ps) const;
+
     ::geoalgo::AABox _tpc_box;
-    
+    ertool::AlgoFindRelationship _findRel;
+
     bool   _respect_trk_dir;
     double _min_dist_trk_def;
     double _dist_back_to_ceiling;
+    bool _tag_protons_from_neutrons;
+    //Minimum distance [cm] b/t lone track and cosmic primary for it to be considered neutron-induced
+    double _min_neutron_dist = 5.;
+    //Maximum distance [cm] of lone track for it to be considered neutron-induced
+    double _max_neutron_trklength = 1.;
   };
 }
 #endif
 
-/** @} */ // end of doxygen group 
+/** @} */ // end of doxygen group
