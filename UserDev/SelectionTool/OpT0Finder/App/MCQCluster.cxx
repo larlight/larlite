2302d6fb4d8ac0e4757651be4f326175b5db3979
David Kaleko <kaleko@nevis.columbia.edu>
2016-01-20 14:18:21 -0600

    no change to MCQcluster just indentation i think

--- UserDev/SelectionTool/OpT0Finder/App/MCQCluster.cxx
+++ UserDev/SelectionTool/OpT0Finder/App/MCQCluster.cxx
@@ -25,50 +25,50 @@ namespace flashana {
     _step_size   = pset.get<double>("StepSize");
     _use_light_path = pset.get<bool>("UseLightPath");
   }
-  
+
   const std::vector<flashana::QCluster_t>& MCQCluster::QClusters() const
   { return _qcluster_v; }
-  
+
   MCSource_t MCQCluster::Identify( const unsigned int ancestor_track_id,
-				   const ::larlite::event_mctrack& ev_mct,
-				   const ::larlite::event_mcshower& ev_mcs) const
+                                   const ::larlite::event_mctrack& ev_mct,
+                                   const ::larlite::event_mcshower& ev_mcs) const
   {
     MCSource_t res;
 
-    for (size_t mct_index = 0; mct_index < ev_mct.size(); ++mct_index){
-      
+    for (size_t mct_index = 0; mct_index < ev_mct.size(); ++mct_index) {
+
       auto const& mct = ev_mct[mct_index];
-      
+
       if (mct.TrackID() == ancestor_track_id) {
-	res.index_id = (int)mct_index;
-	res.g4_time  = mct.Start().T();
+        res.index_id = (int)mct_index;
+        res.g4_time  = mct.Start().T();
 
-	//std::cout<<"1) g4 time is: "<<res.g4_time / 1000<<" size : "<<mct.size()<<std::endl ;
-	res.source_type = kMCTrackAncestor;
-	return res;
+        //std::cout<<"1) g4 time is: "<<res.g4_time / 1000<<" size : "<<mct.size()<<std::endl ;
+        res.source_type = kMCTrackAncestor;
+        return res;
       }
-      
+
     }
-    
+
     for (size_t mcs_index = 0; mcs_index < ev_mcs.size(); ++mcs_index) {
-      
+
       auto const& mcs = ev_mcs[mcs_index];
-      
+
       if (mcs.TrackID() == ancestor_track_id) {
-	res.index_id = (int)mcs_index;
-	res.g4_time  = mcs.Start().T();
-	res.source_type = kMCShowerAncestor;
-	return res;
+        res.index_id = (int)mcs_index;
+        res.g4_time  = mcs.Start().T();
+        res.source_type = kMCShowerAncestor;
+        return res;
       }
     }
 
-//	std::cout<<"2) g4 time is: "<<ancestor_track_id<<", "<<res.g4_time<<", "<<res.index_id<<std::endl ;
-    
+//  std::cout<<"2) g4 time is: "<<ancestor_track_id<<", "<<res.g4_time<<", "<<res.index_id<<std::endl ;
+
     return res;
   }
-  
+
   void MCQCluster::Construct( const ::larlite::event_mctrack& ev_mct,
-			      const ::larlite::event_mcshower& ev_mcs )
+                              const ::larlite::event_mcshower& ev_mcs )
   {
 
     //
@@ -77,14 +77,14 @@ namespace flashana {
 
     // Look at tracks on 'per interaction' basis
     std::map<unsigned int, size_t> usedIDs;
-    
+
     // Clear attributes to be filled
     _mctrack_2_qcluster.clear();
     _mcshower_2_qcluster.clear();
     _qcluster_v.clear();
 
     //  std::cout<<"All track ancestors: "<<std::endl;
-    //  for (size_t mct_index = 0; mct_index < ev_mct.size(); mct_index++) 
+    //  for (size_t mct_index = 0; mct_index < ev_mct.size(); mct_index++)
     //    std::cout<<ev_mct[mct_index].AncestorTrackID()<<", ";
     //
     //    std::cout<<"\n\nAll track ids: "<<std::endl;
@@ -101,30 +101,30 @@ namespace flashana {
     _qcluster_v.reserve(ev_mct.size());
     for (size_t mct_index = 0; mct_index < ev_mct.size(); mct_index++) {
       //  std::cout<<"Ancestors: "<<ev_mct.at(mct_index).AncestorTrackID()<<std::endl;
-      
+
       auto const& trk = ev_mct[mct_index];
       if (trk.size() <= 2 || trk[0].T() < -2050000 || trk[0].T() > 2750000 ) continue;
-      
+
       auto usedIDs_iter = usedIDs.find(trk.AncestorTrackID());
-      
+
       size_t qcluster_index = 0;
       if ( usedIDs_iter == usedIDs.end() ) {
-	qcluster_index = _qcluster_v.size();
-	usedIDs.emplace(trk.AncestorTrackID(), qcluster_index);
-	// Search ancestor identity
-	_qcluster_2_mcobject.emplace_back(Identify(trk.AncestorTrackID(),ev_mct,ev_mcs));
-	QCluster_t qcluster;	
-	qcluster.idx = _qcluster_2_mcobject.back().index_id;
-	_qcluster_v.emplace_back(qcluster);
+        qcluster_index = _qcluster_v.size();
+        usedIDs.emplace(trk.AncestorTrackID(), qcluster_index);
+        // Search ancestor identity
+        _qcluster_2_mcobject.emplace_back(Identify(trk.AncestorTrackID(), ev_mct, ev_mcs));
+        QCluster_t qcluster;
+        qcluster.idx = _qcluster_2_mcobject.back().index_id;
+        _qcluster_v.emplace_back(qcluster);
       }
       else
-	qcluster_index = usedIDs_iter->second;
-      
+        qcluster_index = usedIDs_iter->second;
+
       _mctrack_2_qcluster.push_back(qcluster_index);
 
-      
+
 //      std::cout<<" what's our idx: "<<_qcluster_2_mcobject.back().index_id<<", and size of thing: "<<_qcluster_2_mcobject.size()<<", "<<qcluster_index<<std::endl ;
-      
+
       auto& tpc_obj = _qcluster_v[qcluster_index];
       auto& tpc_src = _qcluster_2_mcobject[qcluster_index];
 
@@ -136,137 +136,137 @@ namespace flashana {
       // Some constants needed
       double det_drift_velocity = ::larutil::LArProperties::GetME()->DriftVelocity(); ///< cm/us
       double event_time = trk[0].T(); // ns
-      double shift_x = event_time * det_drift_velocity * pow(10,-3); //cm
+      double shift_x = event_time * det_drift_velocity * pow(10, -3); //cm
       tpc_obj.reserve(tpc_obj.size() + trk.size());
 
 
-      if(_use_light_path){
-         
-	 _lightpath_clustering.SetXOffset(shift_x);
+      if (_use_light_path) {
+
+        _lightpath_clustering.SetXOffset(shift_x);
 
         ::geoalgo::Trajectory mctraj;
-	for (size_t i = 0; i < trk.size(); ++i) 
-	  mctraj.push_back(::geoalgo::Vector(trk.at(i).X(), trk.at(i).Y(), trk.at(i).Z()));
+        for (size_t i = 0; i < trk.size(); ++i)
+          mctraj.push_back(::geoalgo::Vector(trk.at(i).X(), trk.at(i).Y(), trk.at(i).Z()));
 
-	auto qclus = _lightpath_clustering.FlashHypothesis(mctraj);
-	   
-	for( auto const & c : qclus )
-	  tpc_obj.emplace_back(c) ;
+        auto qclus = _lightpath_clustering.FlashHypothesis(mctraj);
 
-           }
+        for ( auto const & c : qclus )
+          tpc_obj.emplace_back(c) ;
+
+      }
 
       else {
 
-      // Now loop over all mctracks that share an ancestor and treat
-      // them as one interaction
-      ::geoalgo::Point_t step_dir(0, 0, 0);
-      
-      for (size_t step_index = 0; (step_index + 1) < trk.size(); ++step_index) {
-	
-	auto const& pt1 = trk[step_index];
-	auto const& pt2 = trk[step_index + 1];
+        // Now loop over all mctracks that share an ancestor and treat
+        // them as one interaction
+        ::geoalgo::Point_t step_dir(0, 0, 0);
 
-	double dx = pt2.X() - pt1.X();
-	double dy = pt2.Y() - pt1.Y();
-	double dz = pt2.Z() - pt1.Z();
-	double e_diff = pt1.E() - pt2.E();
-	tpc_src.energy_deposit += e_diff;
+        for (size_t step_index = 0; (step_index + 1) < trk.size(); ++step_index) {
 
-	//std::cout<<"\nOld X, Y, Z, Q: "<<pt1.X() + dx/2. +shift_x<<", "<<pt1.Y()+dy/2.<<", "<<pt1.Z()+dz/2.<<", "<<(pt1.E() - pt2.E())<<std::endl ;
-	
-	double distance = sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
-	double dedx = e_diff / distance;
-	//dedx = 2.3;
-	step_dir[0] = dx;
-	step_dir[1] = dy;
-	step_dir[2] = dz;
-	step_dir.Normalize();
-	
-	// Create individual points
-	size_t n_segments = size_t( distance / _step_size ) + 1;
-	_n = _n+n_segments;
-	tpc_obj.reserve(n_segments + tpc_obj.size());
-	for (size_t segment_index = 1; segment_index <= n_segments; ++segment_index) {
-	  
-	  QPoint_t pt;
-	  
-	  if (segment_index != n_segments) {
-	    pt.q = _light_yield * dedx * _step_size;
+          auto const& pt1 = trk[step_index];
+          auto const& pt2 = trk[step_index + 1];
 
-	    if( _use_xshift)pt.x = pt1.X() + step_dir[0] * _step_size * (segment_index - 0.5) + shift_x;
-	    if(!_use_xshift)pt.x = pt1.X() + step_dir[0] * _step_size * (segment_index - 0.5);
-	    pt.y = pt1.Y() + step_dir[1] * _step_size * (segment_index - 0.5);
-	    pt.z = pt1.Z() + step_dir[2] * _step_size * (segment_index - 0.5);
-//	    std::cout<<"pt.x in MCQCluster: "<<pt.x<<std::endl ;
-	    tpc_obj.emplace_back(pt);
-	    } 
-	  else if(segment_index == n_segments && (distance - _step_size*(n_segments-1))!=0) {
-	    double offset = _step_size * (segment_index - 1);
-	    double remain = distance - offset;
+          double dx = pt2.X() - pt1.X();
+          double dy = pt2.Y() - pt1.Y();
+          double dz = pt2.Z() - pt1.Z();
+          double e_diff = pt1.E() - pt2.E();
+          tpc_src.energy_deposit += e_diff;
 
-	    pt.q = _light_yield * dedx * remain;
-	    if( _use_xshift)pt.x = pt1.X() + step_dir[0] * (offset + remain / 2.) + shift_x;
-	    if(!_use_xshift)pt.x = pt1.X() + step_dir[0] * (offset + remain / 2.);
-	    pt.y = pt1.Y() + step_dir[1] * (offset + remain / 2.);
-	    pt.z = pt1.Z() + step_dir[2] * (offset + remain / 2.);
-//	    std::cout<<"pt.x in MCQCluster: "<<pt.x<<", "<<distance << std::endl ;
-	    tpc_obj.emplace_back(pt);
-	    break;
-	  }
-//	    std::cout<<"pt.x in MCQCluster: "<<pt.x<<", "<<segment_index<<std::endl ;
+          //std::cout<<"\nOld X, Y, Z, Q: "<<pt1.X() + dx/2. +shift_x<<", "<<pt1.Y()+dy/2.<<", "<<pt1.Z()+dz/2.<<", "<<(pt1.E() - pt2.E())<<std::endl ;
 
-	}// Finish looping over segments
-      }// Finish looping over mctrack trajectory points
-     }// Else if use mcqcluster
+          double distance = sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
+          double dedx = e_diff / distance;
+          //dedx = 2.3;
+          step_dir[0] = dx;
+          step_dir[1] = dy;
+          step_dir[2] = dz;
+          step_dir.Normalize();
+
+          // Create individual points
+          size_t n_segments = size_t( distance / _step_size ) + 1;
+          _n = _n + n_segments;
+          tpc_obj.reserve(n_segments + tpc_obj.size());
+          for (size_t segment_index = 1; segment_index <= n_segments; ++segment_index) {
+
+            QPoint_t pt;
+
+            if (segment_index != n_segments) {
+              pt.q = _light_yield * dedx * _step_size;
+
+              if ( _use_xshift)pt.x = pt1.X() + step_dir[0] * _step_size * (segment_index - 0.5) + shift_x;
+              if (!_use_xshift)pt.x = pt1.X() + step_dir[0] * _step_size * (segment_index - 0.5);
+              pt.y = pt1.Y() + step_dir[1] * _step_size * (segment_index - 0.5);
+              pt.z = pt1.Z() + step_dir[2] * _step_size * (segment_index - 0.5);
+//      std::cout<<"pt.x in MCQCluster: "<<pt.x<<std::endl ;
+              tpc_obj.emplace_back(pt);
+            }
+            else if (segment_index == n_segments && (distance - _step_size * (n_segments - 1)) != 0) {
+              double offset = _step_size * (segment_index - 1);
+              double remain = distance - offset;
+
+              pt.q = _light_yield * dedx * remain;
+              if ( _use_xshift)pt.x = pt1.X() + step_dir[0] * (offset + remain / 2.) + shift_x;
+              if (!_use_xshift)pt.x = pt1.X() + step_dir[0] * (offset + remain / 2.);
+              pt.y = pt1.Y() + step_dir[1] * (offset + remain / 2.);
+              pt.z = pt1.Z() + step_dir[2] * (offset + remain / 2.);
+//      std::cout<<"pt.x in MCQCluster: "<<pt.x<<", "<<distance << std::endl ;
+              tpc_obj.emplace_back(pt);
+              break;
+            }
+//      std::cout<<"pt.x in MCQCluster: "<<pt.x<<", "<<segment_index<<std::endl ;
+
+          }// Finish looping over segments
+        }// Finish looping over mctrack trajectory points
+      }// Else if use mcqcluster
     } // Finish looping over mctracks
     //std::cout<<"size in MCQCluster output is "<<_n<<std::endl;
-    _n=0;
+    _n = 0;
     //
     // MCShower treatment to be added
     //
   }// EOF
 
   void MCQCluster::Swap(std::vector<flashana::QCluster_t>&& qcluster_v,
-			std::vector<flashana::MCSource_t>&& source_v)
+                        std::vector<flashana::MCSource_t>&& source_v)
   {
-    std::swap(_qcluster_v,qcluster_v);
-    std::swap(_qcluster_2_mcobject,source_v);
+    std::swap(_qcluster_v, qcluster_v);
+    std::swap(_qcluster_2_mcobject, source_v);
   }
-  
+
   const flashana::QCluster_t& MCQCluster::QCluster(size_t id) const
   {
     if (id < _qcluster_v.size())
-      
+
       return _qcluster_v[id];
-    
+
     std::cerr << "Invalid QCluster index: " << id << std::endl;
     throw std::exception();
   }
-  
+
   size_t MCQCluster::MCTrack2QCluster(size_t id) const
   {
     if (id < _mctrack_2_qcluster.size()) return _mctrack_2_qcluster[id];
-    
+
     std::cerr << "Invalid MCTrack index: " << id << std::endl;
     throw std::exception();
   }
-  
+
   size_t MCQCluster::MCShower2QCluster(size_t id) const
   {
     if (id < _mcshower_2_qcluster.size()) return _mcshower_2_qcluster[id];
-    
+
     std::cerr << "Invalid MCShower index: " << id << std::endl;
     throw std::exception();
   }
-  
+
   const MCSource_t& MCQCluster::MCObjectID(size_t id) const
   {
     if (id < _qcluster_2_mcobject.size() ) return _qcluster_2_mcobject[id];
-    
+
     std::cerr << "Invalid QCluster index: " << id << std::endl;
     throw std::exception();
   }
-  
+
 }
 
 #endif
