3d69b13c3fe10ae96bd2a389247f1915abb54179
David Caratelli <devish.c@gmail.com>
2016-01-25 14:54:16 -0600

    MCClusterere based on back-tracker to simulate perfect clustering for multi-particle events -- David Caratelli

---
+++ UserDev/RecoTool/MCComp/MCClusterer.h
@@ -0,0 +1,72 @@
+/**
+ * \file MCClusterer.h
+ *
+ * \ingroup MCComp
+ * 
+ * \brief Class def header for a class MCClusterer
+ *
+ * @author david caratelli
+ */
+
+/** \addtogroup MCComp
+
+    @{*/
+
+#ifndef LARLITE_MCCLUSTERER_H
+#define LARLITE_MCCLUSTERER_H
+
+#include "Analysis/ana_base.h"
+#include "MCBTAlg.h"
+
+namespace larlite {
+  /**
+     \class MCClusterer
+     User custom analysis class made by david caratelli
+   */
+  class MCClusterer : public ana_base{
+  
+  public:
+
+    /// Default constructor
+    MCClusterer();
+
+    /// Default destructor
+    virtual ~MCClusterer(){}
+
+    virtual bool initialize();
+
+    virtual bool analyze(storage_manager* storage);
+
+    virtual bool finalize();
+
+    void setHitProducer(std::string s) { _hit_producer = s; }
+    
+    void setMinEnergy(double e) { _mc_energy_min = e; }
+
+  protected:
+
+    // hit brack-tracking tool
+    ::btutil::MCBTAlg _bt_algo;
+
+    // hit producer to be used
+    std::string _hit_producer;
+
+    // minimum energy for a particle to be added to the map [MeV]
+    double _mc_energy_min;
+    
+
+    
+    
+  };
+}
+#endif
+
+//**************************************************************************
+// 
+// For Analysis framework documentation, read Manual.pdf here:
+//
+// http://microboone-docdb.fnal.gov:8080/cgi-bin/ShowDocument?docid=3183
+//
+//**************************************************************************
+
+/** @} */ // end of doxygen group 
