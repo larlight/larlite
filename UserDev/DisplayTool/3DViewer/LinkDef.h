30a6d3a91f2f4e0ab82dc3f62f0bb47774091d09
Corey Adams <corey.adams@yale.edu>
2016-01-28 10:23:55 -0600

    Adding cxx side of shower drawing.

--- UserDev/DisplayTool/3DViewer/LinkDef.h
+++ UserDev/DisplayTool/3DViewer/LinkDef.h
@@ -9,27 +9,8 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-// #pragma link C++ class evd::RecoBase3D+;
-// Link all the classes that are "interactive" - for use in evd
-// #pragma link C++ class evd::Shower2d+;
-// #pragma link C++ class std::vector<::evd::Shower2d>+;
-// #pragma link C++ class evd::RecoBase<evd::Shower2d>+;
-// #pragma link C++ class evd::DrawShower+;
-
-// #pragma link C++ class evd::Hit+;
-// #pragma link C++ class std::vector<::evd::Hit>+;
-// #pragma link C++ class evd::RecoBase<evd::Hit>+;
-// #pragma link C++ class evd::DrawHit+;
-
-// #pragma link C++ class evd::Cluster2d+;
-// #pragma link C++ class std::vector<::evd::Cluster2d>+;
-// #pragma link C++ class evd::RecoBase<evd::Cluster2d>+;
-// #pragma link C++ class evd::DrawCluster+;
-
-// #pragma link C++ class evd::DrawMatch+;
 
 #pragma link C++ class evd::Track3D+;
-// #pragma link C++ class std::vector<std::pair<float,float> >+;
 #pragma link C++ class std::vector<evd::Track3D>+;
 #pragma link C++ class evd::RecoBase3D<evd::Track3D>+;
 #pragma link C++ class evd::DrawTrack3D+;
@@ -39,11 +20,13 @@
 #pragma link C++ class evd::RecoBase3D<evd::MCTrack3D>+;
 #pragma link C++ class evd::DrawMCTrack3D+;
 
+#pragma link C++ class evd::Shower3D+;
+#pragma link C++ class std::vector<evd::Shower3D>+;
+#pragma link C++ class evd::RecoBase3D<evd::Shower3D>+;
+#pragma link C++ class evd::DrawShower3D+;
 
 // Link the classes that are processors
-// #pragma link C++ class larlite::DrawVertex+;
-// #pragma link C++ class larlite::DrawEndpoint2d+;
-// #pragma link C++ class std::vector<std::vector<std::pair<float,float> > >+;
+
 //ADD_NEW_CLASS ... do not change this line
 #endif
 
