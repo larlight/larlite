445273ee20db892ddaf00c90ac8ca55ae70ca9b1
David Caratelli <devish.c@gmail.com>
2016-01-29 09:34:29 -0600

    adding a GeoCylinder class which currently can be used to check if a point is in the cylinder volume or not -- David Caratelli

---
+++ UserDev/BasicTool/GeoAlgo/GeoCylinder.h
@@ -0,0 +1,62 @@
+/**
+ * \file GeoCylinder.h
+ *
+ * \ingroup GeoAlgo
+ * 
+ * \brief Class def header for a class Cylinder
+ *
+ * @author david caratelli
+ */
+
+/** \addtogroup GeoAlgo
+
+    @{*/
+#ifndef BASICTOOL_GEOCYLINDER_H
+#define BASICTOOL_GEOCYLINDER_H
+
+#include "GeoVector.h"
+#include "GeoAlgo.h"
+#include "GeoLine.h"
+
+namespace geoalgo {
+  /**
+     \class Cylinder
+     @brief Representation of a 3D Cylinder volume.
+  */
+  class Cylinder {
+    
+  public:
+    
+    /// Default constructor
+    Cylinder();
+
+    /// Default destructor
+    virtual ~Cylinder(){};
+    
+    /// Alternative ctor (0)
+    Cylinder(const double x_min, const double y_min, const double z_min,
+	       const double x_max, const double y_max, const double z_max,
+	       const double radius);
+    
+    /// Altenartive ctor (1)
+    Cylinder(const Point_t& min, const Vector_t& max, const double radius);
+    
+    /// Containment evaluation
+    bool Contain(const Point_t &pt) const; ///< Test if a point is contained within the box
+    
+  protected:
+    
+    Point_t _min; ///< Minimum point
+    Point_t _max; ///< Maximum point
+    double  _radius; ///< Radius of the cylinder
+
+    // geoalgo utility
+    GeoAlgo _geoAlgo;
+
+  };
+
+  typedef Cylinder Cylinder_t;
+}
+#endif
+/** @} */ // end of doxygen group 
+
