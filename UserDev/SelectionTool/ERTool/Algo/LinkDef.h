fb045831afe21824a7a3e3045742d137150fb0ef
Rui <ran4@hawk.iit.edu>
2016-03-15 11:43:01 -0500

    remove more scrach files

--- UserDev/SelectionTool/ERTool/Algo/LinkDef.h
+++ UserDev/SelectionTool/ERTool/Algo/LinkDef.h
@@ -33,10 +33,6 @@
 #pragma link C++ class ertool::ERAlgoTrackID+;
 #pragma link C++ class ertool::ERAlgoOpT0Tender+;
 #pragma link C++ class ertool::ERAlgoNueSharedFlashMerger+;
-#pragma link C++ class ertool::ERAlgoNCPi0+;
-#pragma link C++ class ertool::AlgoNCPi0Finder+;
-#pragma link C++ class ertool::ERAlgoDumbPi0+;
-
 
 #endif
 
