c1ab84026aed7fb4ba4a9bfce0cca71ab3d86782
Yun-Tse Tsai <yuntsebaryon@gmail.com>
2016-04-12 14:46:04 -0700

    changed it back

--- config/setup_reco.sh
+++ config/setup_reco.sh
@@ -8,8 +8,6 @@ export LARLITE_BASEDIR="$( cd "$( dirname "$me" )" && pwd )"
 source $LARLITE_BASEDIR/config/setup.sh
 
 # Set up the event display if possible:
-export PYTHONPATH="/Library/Python/2.7/site-packages/PyQt4:/System/Library/Frameworks/Python.framework/Versions/2.7/bin:${PYTHONPATH}"
-export DYLD_FRAMEWORK_PATH="/Users/yuntse/Sources/qt-everywhere-opensource-src-5.5.1/qtbase/lib"
 source $LARLITE_USERDEVDIR/DisplayTool/setup_evd.sh
 if [ $BUILD_LARLITE_EVD ]; then
   USER_MODULE="${USER_MODULE} DisplayTool "
