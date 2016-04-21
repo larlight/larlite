4850164a0299f66e083824f83b0090d6caafc08e
Corey Adams <corey.adams@yale.edu>
2016-04-21 12:38:49 -0500

    Update Base to match protoshower namespace

--- UserDev/RecoTool/ShowerReco3D/Base/ShowerRecoException.h
+++ UserDev/RecoTool/ShowerReco3D/Base/ShowerRecoException.h
@@ -2,7 +2,7 @@
  * \file ShowerRecoException.h
  *
  * \ingroup ShowerReco3D
- * 
+ *
  * \brief Class def header for exception classes in ShowerReco3D package
  *
  * @author kazuhiro
@@ -18,31 +18,31 @@
 #include <exception>
 
 namespace showerreco {
-  /**
-     \class ShowerRecoException
-     Generic (base) exception class
-  */
-  class ShowerRecoException : public std::exception{
+/**
+   \class ShowerRecoException
+   Generic (base) exception class
+*/
+class ShowerRecoException : public std::exception {
 
-  public:
+public:
 
-    ShowerRecoException(std::string msg="") : std::exception()
-    {
-      _msg = "\033[93m";
-      _msg += msg;
-      _msg += "\033[00m";
-    }
+  ShowerRecoException(std::string msg = "") : std::exception()
+  {
+    _msg = "\033[93m";
+    _msg += msg;
+    _msg += "\033[00m";
+  }
 
-    virtual ~ShowerRecoException() throw(){};
-    virtual const char* what() const throw() 
-    { return _msg.c_str(); }
+  virtual ~ShowerRecoException() throw() {};
+  virtual const char* what() const throw()
+  { return _msg.c_str(); }
 
-  private:
+private:
 
-    std::string _msg;
-  };
+  std::string _msg;
+};
 
 }
 #endif
-/** @} */ // end of doxygen group 
+/** @} */ // end of doxygen group
 
