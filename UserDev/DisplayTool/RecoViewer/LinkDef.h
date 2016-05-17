a42197b553db07160cffaad1b8e066aa84dff3fd
Corey Adams <corey.adams@yale.edu>
2016-05-17 14:45:28 -0500

    This should fix the build issues on mac.

--- UserDev/DisplayTool/RecoViewer/LinkDef.h
+++ UserDev/DisplayTool/RecoViewer/LinkDef.h
@@ -11,9 +11,9 @@
 
 // #pragma link C++ class evd::RecoBase+;
 // Link all the classes that are "interactive" - for use in evd
-#pragma link C++ class evd::Shower2d+;
-#pragma link C++ class std::vector<::evd::Shower2d>+;
-#pragma link C++ class evd::RecoBase<evd::Shower2d>+;
+#pragma link C++ class evd::Shower2D+;
+#pragma link C++ class std::vector<::evd::Shower2D>+;
+#pragma link C++ class evd::RecoBase<evd::Shower2D>+;
 #pragma link C++ class evd::DrawShower+;
 
 #pragma link C++ class evd::Hit+;
@@ -21,23 +21,23 @@
 #pragma link C++ class evd::RecoBase<evd::Hit>+;
 #pragma link C++ class evd::DrawHit+;
 
-#pragma link C++ class evd::Cluster2d+;
-#pragma link C++ class std::vector<::evd::Cluster2d>+;
-#pragma link C++ class evd::RecoBase<evd::Cluster2d>+;
+#pragma link C++ class evd::Cluster2D+;
+#pragma link C++ class std::vector<::evd::Cluster2D>+;
+#pragma link C++ class evd::RecoBase<evd::Cluster2D>+;
 #pragma link C++ class evd::DrawCluster+;
 
 #pragma link C++ class evd::DrawMatch+;
 
-#pragma link C++ class evd::Track2d+;
+#pragma link C++ class evd::Track2D+;
 #pragma link C++ class std::vector<std::pair<float,float> >+;
-#pragma link C++ class std::vector<::evd::Track2d>+;
-#pragma link C++ class evd::RecoBase<evd::Track2d>+;
+#pragma link C++ class std::vector<::evd::Track2D>+;
+#pragma link C++ class evd::RecoBase<evd::Track2D>+;
 #pragma link C++ class evd::DrawTrack+;
 
 #pragma link C++ class evd::Endpoint2D+;
 #pragma link C++ class std::vector<::evd::Endpoint2D>+;
 #pragma link C++ class evd::RecoBase<::evd::Endpoint2D>+;
-#pragma link C++ class evd::DrawEndpoint2d+;
+#pragma link C++ class evd::DrawEndpoint+;
 
 
 // Link the classes that are processors
