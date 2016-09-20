9bdc6ee94b81153b7e1e528c4478dcc739e16bf5
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-09-20 12:39:52 -0500

    typo fix --kazu

--- UserDev/RecoTool/CMTool/CMTAlgMatch/LinkDef.h
+++ UserDev/RecoTool/CMTool/CMTAlgMatch/LinkDef.h
@@ -23,7 +23,7 @@
 #pragma link C++ class cmtool::CFAlgoChargeDistrib+;
 #pragma link C++ class cmtool::CFAlgoShowerCompat+;
 #pragma link C++ class cmtool::CFAlgoProhibitPlane+;
-#pragma link C++ class cmtool::CFAlgoI0U+;
+#pragma link C++ class cmtool::CFAlgoIoU+;
 //ADD_CFALGO_CLASS ... do not change this line
 //ADD_NEW_CLASS ... do not change this line
 #endif
