75c2d588ee5bc970ce43ce392e066b2d7a263f97
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-10-14 17:18:13 -0400

    typo motherfcuker

--- core/Base/UtilFunc.cxx
+++ core/Base/UtilFunc.cxx
@@ -4,7 +4,7 @@
 #include "UtilFunc.h"
 #include "DataFormatConstants.h"
 
-namespace lalrite {
+namespace larlite {
   const std::string& GetProductName(const larlite::data::DataType_t t)
   {
     return larlite::data::kDATA_TREE_NAME[t];
