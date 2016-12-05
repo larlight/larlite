d99cb83ff71f314e42eb8669534095f186a7d21d
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-12-05 14:27:01 -0600

    v6 by default --kazu

--- UserDev/SelectionTool/OpT0Finder/PhotonLibrary/PhotonLibrary.cxx
+++ UserDev/SelectionTool/OpT0Finder/PhotonLibrary/PhotonLibrary.cxx
@@ -107,7 +107,8 @@ namespace phot{
 	if(!f) {
 	  std::cerr<<"\033[95m<<"<<__FUNCTION__<<">>\033[00m " << "Failed to open a ROOT file: " << LibraryFile.c_str()<<std::endl;
 	  std::cerr<<"If you don't have photon library data file, download from below URL..."<<std::endl;
-	  std::cerr<<"/grid/fermiapp/products/uboone/uboone_data/v01_04_00/PhotonPropagation/LibraryData/uboone_photon_library_v5.root"<<std::endl;
+	  //std::cerr<<"/grid/fermiapp/products/uboone/uboone_data/v01_04_00/PhotonPropagation/LibraryData/uboone_photon_library_v6.root"<<std::endl;
+	  std::cerr<<"/pnfs/uboone/persistent/users/jaz8600/PhotonLibraries/v6/EField/uboone_photon_library_v6_efield.root"<<std::endl;
 	  throw std::exception();
 	}
 	tt =  (TTree*)f->Get("PhotonLibraryData");
