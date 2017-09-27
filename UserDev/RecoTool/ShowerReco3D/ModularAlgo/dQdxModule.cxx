e74a3e3806a7589aa8cd08b7cb01f2782471cbbd
david <devish.c@gmail.com>
2017-09-27 09:05:21 -0400

    dqdx xyz corrections --DC

--- UserDev/RecoTool/ShowerReco3D/ModularAlgo/dQdxModule.cxx
+++ UserDev/RecoTool/ShowerReco3D/ModularAlgo/dQdxModule.cxx
@@ -108,7 +108,10 @@ namespace showerreco {
 
 	if (d3D >= _dtrunk) continue;
 
-	_dqdx_v[ d3D * 3 ] += h.charge;
+	double qcorr = ChargeCorrection(h.charge,resultShower.fXYZStart);
+	
+
+	_dqdx_v[ d3D * 3 ] += qcorr;
 	
 	_nhits += 1;
 	
@@ -143,6 +146,137 @@ namespace showerreco {
     
     return;
   }
+
+  void dQdxModule::CreateResponseMap(const double& stepsize) {
+    
+    _responseMap.clear();
+    _responseStep = stepsize;
+
+    auto geom = larutil::Geometry::GetME();
+    
+    int nx = int((geom->DetHalfWidth() * 2)  / _responseStep) + 1;
+    int ny = int((geom->DetHalfHeight() * 2) / _responseStep) + 1;
+    int nz = int((geom->DetLength())         / _responseStep) + 1;
+    
+    _responseMap = std::vector< std::vector< std::vector<double> > >(nx,std::vector< std::vector<double> >(ny, std::vector<double>(nz,-1) ) );
+    
+    return;
+  }
+
+  double dQdxModule::ChargeCorrection(const double& q, const TVector3& vtx){
+
+    auto geom = larutil::Geometry::GetME();
+
+    // find cells in map
+    size_t i = vtx.X() / _responseStep;
+    size_t j = (geom->DetHalfHeight() + vtx.Y()) / _responseStep;
+    size_t k = vtx.Z() / _responseStep;
+
+    // make sure not out of bounds:
+    if ( (i < 0) or (j < 0) or (k < 0) or
+	 (i >= _responseMap.size()) or
+	 (j >= _responseMap[0].size()) or
+	 (k >= _responseMap[0][0].size()) ) {
+      return q;
+    }
+
+    double corr = _responseMap[i][j][k];
+
+    if (corr <= 0) {
+      //std::cout << "No correction available..." << std::endl;
+      // find nearest non-empty box
+      corr = NearestCell(i,j,k);
+    }
+
+    return q * corr;
+  }
+  
+  
+  void dQdxModule::SetResponse(const double& x, const double& y, const double& z, const double& q)
+    
+  {
+
+    auto geom = larutil::Geometry::GetME();
+    
+    if ( (x < 0) || (x > 2 * geom->DetHalfWidth()) ) {
+      std::cout << "X value out of bounds." << std::endl;
+      return;
+    }
+
+    if ( (y < -geom->DetHalfHeight()) || (y > geom->DetHalfHeight()) ) {
+      std::cout << "y value out of bounds." << std::endl;
+      return;
+    }
+
+    if ( (z < 0) || (z > geom->DetLength()) ) {
+      std::cout << "X value out of bounds." << std::endl;
+      return;
+    }
+
+    // find vector position
+    size_t i = x / _responseStep;
+    size_t j = (geom->DetHalfHeight() + y) / _responseStep;
+    size_t k = z / _responseStep;
+
+    //std::cout << "x, y, z " << x << ", " << y << ", " << z << "\t i, j, k " << i << ", " << j << ", "  << k << std::endl;
+    
+    _responseMap[i][j][k] = q;
+
+    return;
+  }
+
+  double dQdxModule::NearestCell(const size_t& i, const size_t& j, const size_t& k) {
+
+    double val;
+    int    ctr;
+
+    std::vector<int> cellspan;
+
+    for (int cellsize = 1; cellsize < 4; cellsize++) {
+
+      val = 0;
+      ctr = 0;
+
+      cellspan.clear();
+      for (int n=1; n < cellsize; n++){
+	cellspan.push_back(-(n+1));
+	cellspan.push_back(n+1);
+      }
+      
+      for (auto const& xd : cellspan) {
+	for (auto const& yd : cellspan) {
+	  for (auto const& zd : cellspan) {
+	    
+	    if ( (i+xd >= 0) and (j+yd > 0) and (k+zd > 0) and
+		 (i+xd < _responseMap.size()) and
+		 (j+yd < _responseMap[0].size()) and
+		 (k+zd < _responseMap[0][0].size()) ) {
+	      
+	      if ( _responseMap[i+xd][j+yd][k+zd] > 0){
+		val += _responseMap[i+xd][j+yd][k+zd];
+		ctr += 1;
+	      }
+	      
+	    }// if not out of bounds
+	    
+	  }// z loop
+	}// y loop
+      }// x loop
+
+      if (val != 0) {
+	val /= ctr;
+	//std::cout << "val is " << val << std::endl;
+	return val;
+      }
+      
+    }// for all cell-sizes
+
+    // if we never found non-zero correction, return 1
+    //std::cout << "no neighboring correction found..return 1" << std::endl;
+    return 1;
+  }
+
+  
 } //showerreco
 
 #endif
