09178bf2697a4e339e8128d2c4232e413befd2b5
Victor Genty <vgenty@nevis.columbia.edu>
2018-07-24 10:05:22 -0400

    add mcinfo dump

--- UserDev/RecoTool/ShowerReco3D/App/LinkDef.h
+++ UserDev/RecoTool/ShowerReco3D/App/LinkDef.h
@@ -1,30 +1,12 @@
-//
-// cint script to generate libraries
-// Declaire namespace & classes you defined
-// #pragma statement: order matters! Google it ;)
-//
-
 #ifdef __CINT__
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
 #pragma link C++ class larlite::ShowerReco3D+;
-#pragma link C++ class larlite::ComputePi0Mass+;
-#pragma link C++ class larlite::Pi0Mass+;
-#pragma link C++ class larlite::Pi0Quality+;
-#pragma link C++ class larlite::ShowerQuality_singleshowers+;
-#pragma link C++ class larlite::ShowerQuality_nueshowers+;
-#pragma link C++ class larlite::ShowerQuality_multishowers+;
 #pragma link C++ class larlite::ShowerQuality_DL+;
-#pragma link C++ class larlite::HitCalibration+;
-#pragma link C++ class larlite::ToyShowerGenerator+;
-#pragma link C++ class larlite::ShowerAna+;
-#pragma link C++ class showerreco::ShowerClusterFinder+;
-#pragma link C++ class showerreco::ColtonPandoraShower+;
-#pragma link C++ class showerreco::ClusterInfo+;
-//ADD_NEW_CLASS ... do not change this line
-//ADD_NEW_ALG_CLASS ... do not change this line
+#pragma link C++ class larlite::SegmentDump+;
+
 #endif
 
 
--- UserDev/RecoTool/ShowerReco3D/App/SegmentDump.h
+++ UserDev/RecoTool/ShowerReco3D/App/SegmentDump.h
@@ -1,43 +1,22 @@
-#ifndef LARLITE_SHOWERQUALITY_DL_H
-#define LARLITE_SHOWERQUALITY_DL_H
+#ifndef __SEGMENT_DUMP_H__
+#define __SEGMENT_DUMP_H__
 
 #include "Analysis/ana_base.h"
-#include "ShowerReco3D/Base/ShowerRecoException.h"
-#include "MCSingleShowerCompAlg.h"
 #include "LArUtil/SpaceChargeMicroBooNE.h"
 
 namespace larlite {
 
-  class ShowerQuality_DL : public ana_base {
-
+  class SegmentDump : public ana_base {
   public:
+    SegmentDump();
+    ~SegmentDump() {};
 
-    ShowerQuality_DL();
-    ~ShowerQuality_DL() {};
-
-
-    void SetVertexProducer(const std::string name)
-    { fVertexProducer = name; }
-
-    void SetShowerProducer(const std::string name)
-    { fShowerProducer = name; }
 
     bool initialize();
     bool analyze(storage_manager* storage);
     bool finalize();
     
-  private:
-
-
-    /// Vertex Producer's Name
-    std::string fVertexProducer;
-
-    /// Shower Producers name
-    std::string fShowerProducer;
-    
-    /// Analysis TTree. Filled once per reconstructed shower.
     TTree *fEventTree;
-    TTree *fShowerTree;
     TTree *fAndyTree;
 
     int _event;
@@ -46,91 +25,6 @@ namespace larlite {
     int _entry;
 
     bool _has_mc;
-    
-    //
-    // reco
-    //
-    int _vtx_id;
-    int _nshowers;
-    std::vector<double> _reco_x_v;
-    std::vector<double> _reco_y_v;
-    std::vector<double> _reco_z_v;
-    std::vector<double> _reco_dcosx_v;
-    std::vector<double> _reco_dcosy_v;
-    std::vector<double> _reco_dcosz_v;
-    std::vector<double> _reco_phi_v;
-    std::vector<double> _reco_theta_v;
-    std::vector<double> _reco_energy_U_v;
-    std::vector<double> _reco_energy_V_v;
-    std::vector<double> _reco_energy_Y_v;
-    std::vector<double> _reco_dedx_v;
-    std::vector<double> _reco_dedx_U_v;
-    std::vector<double> _reco_dedx_V_v;
-    std::vector<double> _reco_dedx_Y_v;
-    std::vector<double> _reco_dqdx_v;
-    std::vector<double> _reco_dqdx_U_v;
-    std::vector<double> _reco_dqdx_V_v;
-    std::vector<double> _reco_dqdx_Y_v;
-    std::vector<double> _reco_length_v;
-    std::vector<double> _reco_width1_v;
-    std::vector<double> _reco_width2_v;
-
-    std::vector<double> _mc_reco_anglediff_v;
-    std::vector<double> _mc_reco_dist_v;
-    std::vector<double> _cluster_eff_U_v;
-    std::vector<double> _cluster_eff_V_v;
-    std::vector<double> _cluster_eff_Y_v;
-    std::vector<double> _cluster_pur_U_v;
-    std::vector<double> _cluster_pur_V_v;
-    std::vector<double> _cluster_pur_Y_v;
-    
-    //
-    // mc
-    //
-    int _mcinfoInteractionType;
-    int _mcinfoMode;
-    int _mc_pdg_code;
-
-    double _mc_x;
-    double _mc_y;
-    double _mc_z;
-    double _mc_dcosx;
-    double _mc_dcosy;
-    double _mc_dcosz;
-    double _mc_energy;
-
-    double _mc_dedx;
-    double _mc_dqdx_U;
-    double _mc_dqdx_V;
-    double _mc_dqdx_Y;
-    double _mc_charge_U;
-    double _mc_charge_V;
-    double _mc_charge_Y;
-
-    double _mcs_E;
-    double _mc_containment;
-    double _mc_length;
-    double _mc_wildlength;
-    
-    //
-    // eff & purity alg
-    //
-    MCSingleShowerCompAlg _alg;
-
-    //
-    // clear
-    //
-    void ResizeVectors(int nshowers);
-    void ClearEvent();
-    void ClearVertex();
-    void AttachAndy(TTree* tree);
-    void ResetAndy();
-    void FillSegment(storage_manager *sto);
-    void FillAndy(storage_manager *sto);
-
-    //
-    // segment stand-in
-    //
 
     struct aparticle{
       int pdg;
@@ -290,6 +184,18 @@ namespace larlite {
     
     ::larutil::SpaceChargeMicroBooNE _sce;    
 
+
+    void FillAndy(storage_manager *sto);
+    void FillSegment(storage_manager *sto);
+    
+    void AttachEvent(TTree* tree);
+    void AttachAndy(TTree* tree);
+
+    void ClearEvent();
+    void ResetAndy();
+
   };
+
 }
+
 #endif
