fcbd8ddad0bac51a8e7ca800c6bed6d823fc05f9
twongjirad <taritree.wongjirad@gmail.com>
2018-12-17 17:25:11 -0500

    add pixelmask to storage_manager

--- core/DataFormat/storage_manager.cxx
+++ core/DataFormat/storage_manager.cxx
@@ -48,6 +48,7 @@
 #include "swtrigger.h"
 #include "larflow3dhit.h"
 #include "larflowcluster.h"
+#include "pixelmask.h"
 
 namespace larlite {
 
@@ -1204,6 +1205,9 @@ namespace larlite {
       break;
     case data::kLArFlowCluster:
       _ptr_data_array[type][name]=new event_larflowcluster(name);
+      break;
+    case data::kPixelMask:
+      _ptr_data_array[type][name]=new event_pixelmask(name);
       break;
     default:
       print(msg::kERROR,__FUNCTION__,Form("Event-data identifier not supported: %d",(int)type));
