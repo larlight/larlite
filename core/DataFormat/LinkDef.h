6c42e0d1138c0afd5618cbcf4902944fc09a5eaa
twongjirad <taritree.wongjirad@gmail.com>
2019-05-15 06:45:11 -0400

    added vector of vector int to dict

--- core/DataFormat/LinkDef.h
+++ core/DataFormat/LinkDef.h
@@ -9,6 +9,7 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class std::vector<std::vector<int> >+;
 #pragma link C++ class std::vector<std::vector<float> >+;
 #pragma link C++ class std::vector<std::vector<double> >+;
 
